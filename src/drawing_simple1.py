96dfd0f325dcc30660e3dded274bb20a7f1e77e7
Rickard Lindberg <ricli85@gmail.com>
2008-12-04 21:55:23 +0000

    Basic drawing algorithm for events implemented.

--- src/drawing_simple1.py
+++ src/drawing_simple1.py
@@ -1,6 +1,7 @@
 """
-Contains algorithms for drawing a timeline.
+Implements a prototype algorithm for drawing a timeline.
 """
+
 
 import logging
 
@@ -9,19 +10,81 @@ import wx
 from drawing import DrawingAlgorithm
 from drawing import Metrics
 
+
+INNER_PADDING = 3
+OUTER_PADDING = 5
+THRESHOLD_PIX = 20
+
+
 class SimpleDrawingAlgorithm1(DrawingAlgorithm):
 
+    def __init__(self):
+        self.event_font = wx.Font(8, wx.FONTFAMILY_DEFAULT,
+                                  wx.FONTSTYLE_NORMAL,
+                                  wx.FONTWEIGHT_NORMAL)
+
     def draw(self, dc, time_period, events):
-        logging.debug("Draw in SimpleDrawingAlgorithm")
+        # Store data so that it can be reused in functions
         self.dc = dc
         self.time_period = time_period
-        self.events = events
         self.metrics = Metrics(dc, time_period)
+        # Calc stuff
+        self.data = self._calc_data(events)
+        # Perform drawing
         self._draw_bg()
-        self._calc_event_positions()
         self._draw_events()
+        # Make sure to delete this
         del self.dc
-        del self.metrics
+
+    def _calc_data(self, events):
+        """
+        Calculate rectangles for all events.
+        
+        The rectangles define the areas in which the events can draw themselves.
+        """
+        data = [] # A list of tuples (event, rect)
+        def overlap(r):
+            """Calculate first intersection."""
+            for (event, rect) in data:
+                temp = wx.Rect(*r)
+                intersection = temp.Intersect(rect)
+                if not intersection.IsEmpty():
+                    return intersection
+            return None
+        self.dc.SetFont(self.event_font)
+        for event in events:
+            (tw, th) = self.dc.GetTextExtent(event.text)
+            ew = self.metrics.get_width(event.time_period)
+            logging.debug("th = %s", th)
+            if ew > THRESHOLD_PIX:
+                # Tread as period
+                rw = ew + 2 * OUTER_PADDING
+                rh = th + 2 * INNER_PADDING + 2 * OUTER_PADDING
+                rx = self.metrics.get_x(event.mean_time()) - rw / 2
+                ry = self.metrics.half_height() + OUTER_PADDING
+                movedir = 1
+            else:
+                # Tread as event
+                rw = tw + 2 * INNER_PADDING + 2 * OUTER_PADDING
+                rh = th + 2 * INNER_PADDING + 2 * OUTER_PADDING
+                rx = self.metrics.get_x(event.mean_time()) - rw / 2
+                ry = self.metrics.half_height() - rh - OUTER_PADDING
+                movedir = -1
+            rect = wx.Rect(rx, ry, rw, rh)
+            # Move rect until there is no overlap
+            while True:
+                o = overlap(rect)
+                if not o:
+                    break
+                rect.OffsetXY(0, movedir * o.Height)
+            data.append((event, rect))
+        # Remove outer padding
+        for (event, rect) in data:
+            logging.debug("Before deflate, %s", rect)
+            rect.Deflate(OUTER_PADDING, OUTER_PADDING)
+            logging.debug("After deflate, %s", rect)
+            pass
+        return data
 
     def _draw_bg(self):
         self.dc.SetPen(wx.Pen(wx.Color(0, 0, 0), 1, wx.SOLID))
@@ -33,33 +96,14 @@ class SimpleDrawingAlgorithm1(DrawingAlgorithm):
         self.dc.DrawText(str(self.time_period.start_time), 5, self.metrics.half_height() - 15)
         self.dc.DrawText(str(self.time_period.end_time), self.metrics.width - tw - 5, self.metrics.half_height() - 15)
 
-    def _calc_event_positions(self):
-        logging.debug("_calc_event_positions")
-        self.eventspos = {}
-        for event in self.events:
-            start_time = event.time_period.start_time
-            y = self.metrics.half_height()
-            if event.is_period():
-                y += 20
-            else:
-                y -= 20
-            self.eventspos[event] = (self.metrics.get_x(start_time), y)
-
     def _draw_events(self):
-        logging.debug("_draw_events")
-        self.dc.SetFont(wx.Font(8, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL,
-                   wx.FONTWEIGHT_NORMAL))
-        for (event, pos) in self.eventspos.iteritems():
-            (x, y) = pos
-            (tw, th) = self.dc.GetTextExtent(event.text)
+        for (event, rect) in self.data:
             self.dc.SetBrush(wx.Brush(wx.Color(200, 200, 0), wx.SOLID))
-            if event.is_period():
-                width = (self.metrics.get_x(event.time_period.end_time) - x)
-            else:
-                y -= th
-                self.dc.SetPen(wx.Pen(wx.Color(0, 0, 0), 1, wx.SOLID))
-                self.dc.DrawLine(x, self.metrics.half_height(), x, y)
-                width = tw
-                x -= tw / 2
-            self.dc.DrawRectangle(x, y, width, th)
-            self.dc.DrawText(event.text, x, y)
+            # Ensure that we can't draw outside rectangle
+            self.dc.DestroyClippingRegion()
+            self.dc.SetClippingRect(rect)
+            self.dc.DrawRectangleRect(rect)
+            self.dc.SetFont(self.event_font)
+            self.dc.DrawText(event.text,
+                             rect.X + INNER_PADDING,
+                             rect.Y + INNER_PADDING)
