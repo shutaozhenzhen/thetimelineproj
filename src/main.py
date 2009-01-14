726d6c0b428aec82a7b9226ce396e4af20098bc2
Rickard Lindberg <ricli85@gmail.com>
2009-01-14 13:12:20 +0000

    Refactored.

--- src/main.py
+++ src/main.py
@@ -14,49 +14,48 @@ from logging import info as loginfo
 import wx
 
 from gui import MainFrame
-import drawing
 
 
 def parse_options():
-    op = OptionParser(usage="%prog [options] [filename]", version="%prog 0.1")
+    op = OptionParser(usage="%prog [options] [filename]", version="%prog 0.1.0")
     op.add_option("-l", "--log-level", type="int", default=logging.ERROR,
                   help="specify log level (0 to log everything)")
     op.add_option("-f", "--log-file", default=None,
                   help="specify a file to send log messages to")
-    op.add_option("-a", "--drawing-algorithm", default="simple1",
-                  help="specify a drawing algorithm")
-    # We skip first command line argument since it is the name of the program
+    # Skip first command line argument since it is the name of the program
     return op.parse_args(argv[1:])
 
 
 def setup_logging(level, file):
     format= "%(asctime)s [%(levelname)s] %(message)s"
     # Setup logging to stderror
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setFormatter(logging.Formatter(format))
-    consoleHandler.setLevel(level)
-    logging.getLogger().addHandler(consoleHandler)
+    console_hanler = logging.StreamHandler()
+    console_hanler.setFormatter(logging.Formatter(format))
+    console_hanler.setLevel(level)
+    logging.getLogger().addHandler(console_hanler)
     # Setup logging to file
     if file:
-        fileHandler = logging.FileHandler(file, "w")
-        fileHandler.setFormatter(logging.Formatter(format))
-        fileHandler.setLevel(level)
-        logging.getLogger().addHandler(fileHandler)
+        file_handler = logging.FileHandler(file, "w")
+        file_handler.setFormatter(logging.Formatter(format))
+        file_handler.setLevel(level)
+        logging.getLogger().addHandler(file_handler)
     logging.getLogger().setLevel(level)
-    logging.info("Logging set up with level=%s, file=%s", level, file)
+    loginfo("Logging set up with level=%s, file=%s", level, file)
+
+
+def create_wx_app(input_files):
+    app = wx.PySimpleApp()
+    main_frame = MainFrame()
+    main_frame.Show()
+    for input_file in input_files:
+        main_frame.open_timeline(input_file)
+    return app
 
 
 def main():
     (options, input_files) = parse_options()
     setup_logging(options.log_level, options.log_file)
-    drawing.setup_drawing_algorithm(options.drawing_algorithm)
-    app = wx.PySimpleApp()
-    main_frame = MainFrame()
-    main_frame.Show()
-    # Must be called after Show() because window size is not set before that
-    loginfo("Input files = %s" % input_files)
-    for input_file in input_files:
-        main_frame.open_timeline(input_file)
+    app = create_wx_app(input_files)
     app.MainLoop()
 
 
