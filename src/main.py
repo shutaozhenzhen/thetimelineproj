ad65d524480859372c66a069d57d918ea22e3dbb
Rickard Lindberg <ricli85@gmail.com>
2009-01-27 21:04:02 +0000

    Updated after comments in code review.

--- src/main.py
+++ src/main.py
@@ -18,34 +18,48 @@ from version import get_version
 
 
 def parse_options():
-    version_str = "%prog " + get_version()
-    op = OptionParser(usage="%prog [options] [filename]", version=version_str)
-    op.add_option("-l", "--log-level", type="int", default=logging.ERROR,
-                  help="specify log level (0 to log everything)")
-    op.add_option("-f", "--log-file", default=None,
-                  help="specify a file to send log messages to")
+    """Parse command line options using the optparse module."""
+    version_string = "%prog " + get_version()
+    option_parser = OptionParser(usage="%prog [options] [filename]",
+                                 version=version_string)
+    option_parser.add_option("-l", "--log-level",
+                             type="int", default=logging.ERROR,
+                             help="specify log level (0 to log everything)")
+    option_parser.add_option("-f", "--log-file",
+                             default=None,
+                             help="specify a file to send log messages to")
     # Skip first command line argument since it is the name of the program
-    return op.parse_args(argv[1:])
+    return option_parser.parse_args(argv[1:])
 
 
-def setup_logging(level, file):
+def setup_logging(log_level, filename):
+    """
+    Setup default logger to log to stderror and possible also to a file.
+    
+    The default logger is used like this:
+
+        import logging
+        logging.error(...)
+    """
     format= "%(asctime)s [%(levelname)s] %(message)s"
     # Setup logging to stderror
-    console_hanler = logging.StreamHandler()
-    console_hanler.setFormatter(logging.Formatter(format))
-    console_hanler.setLevel(level)
-    logging.getLogger().addHandler(console_hanler)
-    # Setup logging to file
-    if file:
-        file_handler = logging.FileHandler(file, "w")
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(logging.Formatter(format))
+    console_handler.setLevel(log_level)
+    logging.getLogger().addHandler(console_handler)
+    # Setup logging to file if filename is specified
+    if filename:
+        file_handler = logging.FileHandler(filename, "w")
         file_handler.setFormatter(logging.Formatter(format))
-        file_handler.setLevel(level)
+        file_handler.setLevel(log_level)
         logging.getLogger().addHandler(file_handler)
-    logging.getLogger().setLevel(level)
-    loginfo("Logging set up with level=%s, file=%s", level, file)
+    logging.getLogger().setLevel(log_level)
+    loginfo("Logging set up with log level=%s, filename=%s", log_level,
+            filename)
 
 
 def create_wx_app(input_files):
+    """Initialize wx and create the main frame."""
     app = wx.PySimpleApp()
     main_frame = MainFrame()
     main_frame.Show()
@@ -55,6 +69,7 @@ def create_wx_app(input_files):
 
 
 def main():
+    """Main entry point."""
     (options, input_files) = parse_options()
     setup_logging(options.log_level, options.log_file)
     app = create_wx_app(input_files)
