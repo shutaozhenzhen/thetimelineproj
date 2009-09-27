88504ae79f2c1b4df1e58803e3ccf518bdc95ac2
Rickard Lindberg <ricli85@gmail.com>
2009-09-27 15:31:36 +0200

    Updated help pages.

--- src/help_pages.py
+++ src/help_pages.py
@@ -86,17 +86,17 @@ Hoovering an event will display the full description in the status bar.
         id="where_is_save",
         header=_("Where is the save button?"),
         body=_("""
-There is no save button. Timeline will automatically save your data whenever you modify the events.
+There is no save button. Timeline will automatically save your data whenever needed.
 """))
 
     help_system.install_page(
         id="timeline",
         header=_("Timeline"),
-        related_pages=["events", "contact"],
+        related_pages=["events", "categories"],
         body=_("""
-The timeline can represent dates between year 10 and year 9999. The time represented on the timeline is according to normal dates.
+The timeline shows dates according to the Gregorian calendar on the x-axis. Currently the dates are limited to dates between year 10 and year 9999.  
 
-Future versions might support various kinds of timelines so that you for example can specify a time in terms of number of minutes since a start time. If you can interested in such a feature, please get in touch.
+Future versions might support various kinds of timelines so that you for example can specify a time in terms of number of minutes since a start time. If you are interested in such a feature, please get in touch.
 """))
 
     help_system.install_page(
@@ -110,7 +110,7 @@ An event is the basic data type for representing information on the timeline.  I
     help_system.install_page(
         id="categories",
         header=_("Categories"),
-        related_pages=["categories_delete"],
+        related_pages=["categories_delete", "events"],
         body=_("""
 Categories are used to group events. An event can only belong to one category. All events that belong to the same category are displayed with the same background color.
 """))
@@ -122,9 +122,9 @@ Categories are used to group events. An event can only belong to one category. A
         body=_("""
 Timeline tries its best to ensure that the data you save in a timeline never gets lost. One step in that process is to create a backup of your timeline every time before new data is saved. The backup is saved in a file with the same name as the original but with an "~" appended to the name.
 
-If you get an error saying that there was a problem reading timeline data, your backup might save you if the original has been corrupted.
+If you get an error saying that there was a problem reading timeline data there is a chance that your data is corrupt. In that case you can recover the data from the backup.
 
-If you would like to be extra certain to not loose your data, we recommend that you take regular backups of your timeline file. It can be as simple as copying the original file to another directory.
+If you would like to be extra certain to not loose your data, we recommend that you take regular backups of your timeline file.
 """))
 
     help_system.install_page(
@@ -169,14 +169,14 @@ HelpFigure(selected_event)
     help_system.install_page(
         id="delete_event",
         header=_("Delete event"),
-        related_pages=["select_events"],
+        related_pages=["select_events", "events"],
         body=_("""
 To delete an event, select it and press the *Del* key. Multiple events can be deleted at the same time.
 """))
 
     help_system.install_page(
         id="categories_delete",
-        header=_("Will related events be deleted when I delete a category?"),
+        header=_("Will associated events be deleted when I delete a category?"),
         body=_("""
 No. The events will still be there but they will not belong to a category.
 """))
@@ -184,6 +184,7 @@ No. The events will still be there but they will not belong to a category.
     help_system.install_page(
         id="edit_categories",
         header=_("Edit categories"),
+        related_pages=["categories"],
         body=_("""
 Categories can be managed in the *Edit Categories* dialog (*Timeline* > *Edit Categories*). To edit an existing category, double click on it.
 
@@ -192,7 +193,7 @@ The visibility of categories can also be edited in the sidebar (*View* > *Sideba
 
     help_system.install_page(
         id="contact",
-        header=_("Contact us"),
+        header=_("Contact"),
         body=_("""
 If you have more questions about Timeline, or if you want to get in contact with users and developers of Timeline, send an email to the user mailing list: thetimelineproj-user@lists.sourceforge.net. (Please use English.)
 """))
