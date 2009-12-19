eb5fcf5ba82383fe59ef1688cb31c175319f61d0
Roger Lindberg <roger@rolidata.se>
2009-12-19 11:14:56 +0100

    Disabling Del-button in Categories editor when no category is selected

--- timelinelib/gui/dialogs/categorieseditor.py
+++ timelinelib/gui/dialogs/categorieseditor.py
@@ -56,12 +56,15 @@ class CategoriesEditor(wx.Dialog):
                                          style=wx.LB_SINGLE)
         self.Bind(wx.EVT_LISTBOX_DCLICK, self._lst_categories_on_dclick,
                   self.lst_categories)
+        self.Bind(wx.EVT_LISTBOX, self._lst_categories_on_change,
+                  self.lst_categories)
         # The Add button
         btn_add = wx.Button(self, wx.ID_ADD)
         self.Bind(wx.EVT_BUTTON, self._btn_add_on_click, btn_add)
         # The Delete button
-        btn_del = wx.Button(self, wx.ID_DELETE)
-        self.Bind(wx.EVT_BUTTON, self._btn_del_on_click, btn_del)
+        self.btn_del = wx.Button(self, wx.ID_DELETE)
+        self.btn_del.Disable()
+        self.Bind(wx.EVT_BUTTON, self._btn_del_on_click, self.btn_del)
         # The close button
         btn_close = wx.Button(self, wx.ID_CLOSE)
         btn_close.SetDefault()
@@ -74,7 +77,7 @@ class CategoriesEditor(wx.Dialog):
         vbox.Add(self.lst_categories, flag=wx.ALL|wx.EXPAND, border=BORDER)
         button_box = wx.BoxSizer(wx.HORIZONTAL)
         button_box.Add(btn_add, flag=wx.RIGHT, border=BORDER)
-        button_box.Add(btn_del, flag=wx.RIGHT, border=BORDER)
+        button_box.Add(self.btn_del, flag=wx.RIGHT, border=BORDER)
         button_box.AddStretchSpacer()
         button_box.Add(btn_close, flag=wx.LEFT, border=BORDER)
         vbox.Add(button_box, flag=wx.ALL|wx.EXPAND, border=BORDER)
@@ -102,6 +105,11 @@ class CategoriesEditor(wx.Dialog):
                 self.EndModal(ID_ERROR)
             dialog.Destroy()
 
+    def _lst_categories_on_change(self, e):
+        cat = self.lst_categories.GetSelection()
+        if cat > -1:
+            self.btn_del.Enable()
+
     def _btn_add_on_click(self, e):
         try:
             dialog = CategoryEditor(self, _("Add Category"), self.timeline,
@@ -115,7 +123,7 @@ class CategoriesEditor(wx.Dialog):
                 self.error = dialog.error
                 self.EndModal(ID_ERROR)
             dialog.Destroy()
-
+        
     def _btn_del_on_click(self, e):
         try:
             self._delete_selected_category()
@@ -159,3 +167,5 @@ class CategoriesEditor(wx.Dialog):
         self.lst_categories.Clear()
         for category in sort_categories(self.timeline.get_categories()):
             self.lst_categories.Append(category.name, category)
+        self.btn_del.Disable()
+        
