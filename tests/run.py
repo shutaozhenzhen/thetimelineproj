679633505f44a15c37d97d00e0daec90ad5b79bc
Rickard Lindberg <ricli85@gmail.com>
2010-03-27 14:47:33 +0100

    Refactored script that runs automated tests.

--- tests/run.py
+++ tests/run.py
@@ -16,7 +16,10 @@
 # along with Timeline.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-Script that runs all unit tests.
+Script that runs all automated tests.
+
+All automated tests include the unit tests in this directory tree and the
+doctests written in the source code.
 """
 
 import sys
@@ -24,30 +27,60 @@ import os.path
 import unittest
 import gettext
 
-root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.insert(0, root_dir)
+def run_all_tests():
+    setup_paths()
+    enable_gettext()
+    suite = create_test_suite()
+    verbosity = get_verbosity_level()
+    all_pass = run_suite(suite, verbosity)
+    return all_pass
 
-import time_period
-import file_timeline
-import wildcard_helper
-import category_editor
-import config
-import memorydb
-from timelinelib.about import APPLICATION_NAME
-from timelinelib.paths import LOCALE_DIR
+def setup_paths():
+    # So that the we can write 'import timelinelib.xxx' and 'import tests.xxx'
+    root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    sys.path.insert(0, root_dir)
 
-if __name__ == '__main__':
+def enable_gettext():
+    # So that the _ function is available
+    from timelinelib.about import APPLICATION_NAME
+    from timelinelib.paths import LOCALE_DIR
+    gettext.install(APPLICATION_NAME.lower(), LOCALE_DIR, unicode=True)
+
+def create_test_suite():
+    suite = unittest.TestSuite()
+    add_unittests(suite)
+    add_doctests(suite)
+    return suite
+
+def add_unittests(suite):
+    def add_tests_from_module(module_name):
+        __import__(module_name)
+        module = sys.modules[module_name]
+        module_suite = unittest.defaultTestLoader.loadTestsFromModule(module)
+        suite.addTest(module_suite)
+    add_tests_from_module("tests.time_period")
+    add_tests_from_module("tests.file_timeline")
+    add_tests_from_module("tests.wildcard_helper")
+    add_tests_from_module("tests.category_editor")
+    add_tests_from_module("tests.config")
+    add_tests_from_module("tests.memorydb")
+
+def add_doctests(suite):
+    pass
+
+def get_verbosity_level():
     verbosity = 2
     if len(sys.argv) == 2 and sys.argv[1] == "quiet":
         verbosity = 0
-    gettext.install(APPLICATION_NAME.lower(), LOCALE_DIR, unicode=True)
-    loader = unittest.TestLoader()
-    test_modules = [time_period, file_timeline, wildcard_helper,
-                    category_editor, config, memorydb]
-    test_suites = [loader.loadTestsFromModule(x) for x in test_modules]
-    the_suite = unittest.TestSuite(test_suites)
-    res = unittest.TextTestRunner(verbosity=verbosity).run(the_suite)
-    if res.wasSuccessful():
+    return verbosity
+
+def run_suite(suite, verbosity):
+    res = unittest.TextTestRunner(verbosity=verbosity).run(suite)
+    return res.wasSuccessful()
+
+if __name__ == '__main__':
+    all_pass = run_all_tests()
+    if all_pass:
         sys.exit(0)
     else:
         sys.exit(1)
