afa626086e2978a0bb45111d2210f7504d67fcf7
Rickard Lindberg <ricli85@gmail.com>
2010-04-04 09:39:58 +0200

    Refactored file timeline tests to put files in temporary directory.

--- tests/file_timeline.py
+++ tests/file_timeline.py
@@ -16,6 +16,9 @@
 # along with Timeline.  If not, see <http://www.gnu.org/licenses/>.
 
 
+import tempfile
+import shutil
+import os.path
 import unittest
 import os
 import stat
@@ -32,32 +35,21 @@ from timelinelib.drawing.interface import ViewProperties
 
 class TestFileTimeline(unittest.TestCase):
     
-    def _silent_remove(self, path):
-        if os.path.exists(path):
-            os.remove(path)
-
-    def _write_timeline(self, path, lines):
-        f = file(path, "w")
-        f.write("\n".join(lines))
-        f.close()
-
-    def _set_read_only(self, path):
-        os.chmod(path, stat.S_IREAD)
-
-    def _set_write_only(self, path):
-        os.chmod(path, stat.S_IWRITE)
-
     def setUp(self):
+        # Create temporary dir and names
+        self.tmp_dir = tempfile.mkdtemp(prefix="timeline-test")
+        self.corrupt_file = os.path.join(self.tmp_dir, "corrupt.timeline")
+        self.missingeof_file = os.path.join(self.tmp_dir, "missingeof.timeline")
+        self._021_file = os.path.join(self.tmp_dir, "021.timeline")
+        self.invalid_time_period_file = os.path.join(self.tmp_dir, "invalid_time_period.timeline")
+        self.valid_file = os.path.join(self.tmp_dir, "valid.timeline")
+        # Write content to files
         HEADER_030 = "# Written by Timeline 0.3.0 on 2009-7-23 9:40:33"
         HEADER_030_DEV = "# Written by Timeline 0.3.0dev on 2009-7-23 9:40:33"
         HEADER_021 = "# Written by Timeline 0.2.1 on 2009-7-23 9:40:33"
-        self._write_timeline("readonly.timeline", [HEADER_030, "# END"])
-        self._write_timeline("writeonly.timeline", [HEADER_030, "# END"])
-        self._write_timeline("corrupt.timeline", ["corrupt data here"])
-        self._write_timeline("missingeof.timeline", ["# valid data"])
-        self._write_timeline("021.timeline", [HEADER_021])
-        self._set_read_only("readonly.timeline")
-        self._set_write_only("writeonly.timeline")
+        write_timeline(self.corrupt_file, ["corrupt data here"])
+        write_timeline(self.missingeof_file, ["# valid data"])
+        write_timeline(self._021_file, [HEADER_021])
         invalid_time_period = [
             "# Written by Timeline 0.5.0dev785606221dc2 on 2009-9-22 19:1:10",
             "PREFERRED-PERIOD:2008-12-9 11:32:26;2008-12-9 11:32:26",
@@ -68,35 +60,15 @@ class TestFileTimeline(unittest.TestCase):
             "EVENT:2009-7-20 0:0:0;2009-7-27 0:0:0;Vacation;Private",
             "# END",
         ]
-        self._write_timeline("invalid_time_period.timeline",
-                             invalid_time_period)
+        write_timeline(self.invalid_time_period_file, invalid_time_period)
         valid = [
             "# Written by Timeline 0.5.0 on 2009-9-22 19:1:10",
             "# END",
         ]
-        self._write_timeline("valid.timeline", valid)
+        write_timeline(self.valid_file, valid)
 
     def tearDown(self):
-        self._silent_remove("readonly.timeline")
-        self._silent_remove("writeonly.timeline")
-        self._silent_remove("corrupt.timeline")
-        self._silent_remove("missingeof.timeline")
-        self._silent_remove("021.timeline")
-        self._silent_remove("invalid_time_period.timeline")
-        self._silent_remove("valid.timeline")
-
-    # TODO: How to test write error?
-
-    def testReadError(self):
-        """
-        Scenario: You open a timeline and the application fails to read from
-        the file.
-
-        Expected result: You get an exception and you can not use the timeline.
-
-        The read error is simulated with a write-only file.
-        """
-        self.assertRaises(TimelineIOError, FileTimeline, "writeonly.timeline")
+        shutil.rmtree(self.tmp_dir)
 
     def testCorruptData(self):
         """
@@ -104,7 +76,7 @@ class TestFileTimeline(unittest.TestCase):
 
         Expected result: You get an exception and you can not use the timeline.
         """
-        self.assertRaises(TimelineIOError, FileTimeline, "corrupt.timeline")
+        self.assertRaises(TimelineIOError, FileTimeline, self.corrupt_file)
 
     def testMissingEOF(self):
         """
@@ -113,7 +85,7 @@ class TestFileTimeline(unittest.TestCase):
 
         Expected result: The timeline should be treated as corrupt.
         """
-        self.assertRaises(TimelineIOError, FileTimeline, "missingeof.timeline")
+        self.assertRaises(TimelineIOError, FileTimeline, self.missingeof_file)
 
     def testAddingEOF(self):
         """
@@ -122,7 +94,7 @@ class TestFileTimeline(unittest.TestCase):
         Expected result: The timeline does not contain the EOF marker but since
         it is an old file, no exception should be raised.
         """
-        FileTimeline("021.timeline")
+        FileTimeline(self._021_file)
 
     def testInvalidTimePeriod(self):
         """
@@ -133,7 +105,7 @@ class TestFileTimeline(unittest.TestCase):
         should get an error when trying to read this.
         """
         self.assertRaises(TimelineIOError, FileTimeline,
-                          "invalid_time_period.timeline")
+                          self.invalid_time_period_file)
 
     def testSettingInvalidPreferredPeriod(self):
         """
@@ -141,7 +113,7 @@ class TestFileTimeline(unittest.TestCase):
 
         Expected result: You should get an error.
         """
-        timeline = FileTimeline("valid.timeline")
+        timeline = FileTimeline(self.valid_file)
         now = datetime.datetime.now()
         zero_tp = TimePeriod(now, now)
         vp = ViewProperties()
@@ -175,3 +147,9 @@ class TestHelperFunctions(unittest.TestCase):
     def testSplit(self):
         self.assertEqual(split_on_semicolon("one;two\\;;three"),
                          ["one", "two\\;", "three"])
+
+
+def write_timeline(path, lines):
+    f = file(path, "w")
+    f.write("\n".join(lines))
+    f.close()
