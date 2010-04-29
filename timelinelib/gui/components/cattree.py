308ee9ada29e7f340693c7b477e1a54f2827e4a5
Rickard Lindberg <ricli85@gmail.com>
2010-04-29 14:28:33 +0200

    Modify cattree control to also work in categories editor.
    The cattree control was also rewritten using humble dialog pattern.

--- timelinelib/gui/components/cattree.py
+++ timelinelib/gui/components/cattree.py
@@ -19,17 +19,27 @@
 import wx
 import wx.lib.agw.customtreectrl as customtreectrl
 
+import timelinelib.gui.utils as gui_utils
 from timelinelib.gui.utils import category_tree
+from timelinelib.gui.utils import ID_ERROR
+from timelinelib.gui.utils import _ask_question
+from timelinelib.gui.dialogs.categoryeditor import CategoryEditor
 from timelinelib.db.interface import TimelineIOError
 from timelinelib.db.interface import STATE_CHANGE_CATEGORY
 
 
+NO_CHECKBOX_TYPE = 0
 CHECKBOX_TYPE = 1
 
 
 class CategoriesTree(customtreectrl.CustomTreeCtrl):
     """
-    Display categories as tree and let user change visibility with check boxes.
+    Display categories in tree and provide editing functions.
+    
+    If this control is initialized with initialize_from_timeline_view then the
+    visibility of categories can also be altered using check boxes. A context
+    menu is always available that can be used to edit, add, and delete
+    categories.
     """
 
     def __init__(self, parent, fn_handle_db_error):
@@ -39,61 +49,196 @@ class CategoriesTree(customtreectrl.CustomTreeCtrl):
                  customtreectrl.TR_LINES_AT_ROOT |
                  customtreectrl.TR_HAS_BUTTONS)
         customtreectrl.CustomTreeCtrl.__init__(self, parent, style=style)
-        self.fn_handle_db_error = fn_handle_db_error
-        self.view = None
+        self._create_gui()
+        self.controller = CategoriesTreeController(self, fn_handle_db_error)
+
+    def initialize_from_db(self, db):
+        self.controller.initialize_from_db(db)
+
+    def initialize_from_timeline_view(self, view):
+        self.controller.initialize_from_timeline_view(view)
+
+    def set_category_tree(self, tree, view_properties):
+        self.DeleteAllItems()
+        self.root = self.AddRoot("") # Hidden because of TR_HIDE_ROOT
+        self._update_categories_from_tree(tree, self.root, view_properties)
+        self.ExpandAll()
+
+    def get_selected_category(self):
+        item = self.GetSelection()
+        if item is None:
+            return None
+        category = self.GetPyData(item)
+        return category
+
+    def destroy(self):
+        self.controller.destroy()
+
+    def _create_gui(self):
+        # Context menu
+        self.mnu = wx.Menu()
+        self.mnu_edit = wx.MenuItem(self.mnu, wx.ID_ANY, _("Edit..."))
+        self.Bind(wx.EVT_MENU, self._mnu_edit_on_click, self.mnu_edit)
+        self.mnu.AppendItem(self.mnu_edit)
+        self.mnu_add = wx.MenuItem(self.mnu, wx.ID_ANY, _("Add..."))
+        self.Bind(wx.EVT_MENU, self._mnu_add_on_click, self.mnu_add)
+        self.mnu.AppendItem(self.mnu_add)
+        self.mnu_delete = wx.MenuItem(self.mnu, wx.ID_ANY, _("Delete"))
+        self.Bind(wx.EVT_MENU, self._mnu_delete_on_click, self.mnu_delete)
+        self.mnu.AppendItem(self.mnu_delete)
+        # Events on control
         self.Bind(customtreectrl.EVT_TREE_ITEM_CHECKED,
                   self._on_tree_item_checked, self)
+        self.Bind(wx.EVT_RIGHT_DOWN, self._on_right_down)
+        self.Bind(wx.EVT_KEY_DOWN, self._on_key_down)
 
-    def set_view(self, view):
-        if self.view is not None:
-            self.view.timeline.unregister(self._timeline_changed)
-        self.view = view
-        if self.view:
-            self.view.timeline.register(self._timeline_changed)
-            self._update_categories()
-        else:
-            self.DeleteAllItems()
+    def _update_menu_enableness(self):
+        enable_if_item = [self.mnu_edit, self.mnu_delete]
+        enable = self.get_selected_category() is not None
+        for menu_item in enable_if_item:
+            menu_item.Enable(enable)
+
+    def _update_categories_from_tree(self, tree, root_item, view_properties):
+        for (cat, subtree) in tree:
+            legend_panel = wx.Panel(self, size=(10, 10))
+            legend_panel.SetBackgroundColour(cat.color)
+            if view_properties:
+                ct_type = CHECKBOX_TYPE
+            else:
+                ct_type = NO_CHECKBOX_TYPE
+            item = self.AppendItem(root_item, cat.name, ct_type=ct_type,
+                                   wnd=legend_panel, data=cat)
+            if view_properties:
+                visible = view_properties.category_visible(cat)
+                self.CheckItem2(item, visible)
+            self._update_categories_from_tree(subtree, item, view_properties)
         
+    def _on_right_down(self, e):
+        (item, flags) = self.HitTest(e.GetPosition())
+        if item is not None:
+            self.SelectItem(item, True)
+        self._update_menu_enableness()
+        self.PopupMenu(self.mnu)
+
+    def _on_key_down(self, e):
+        keycode = e.GetKeyCode()
+        if keycode == wx.WXK_DELETE:
+            self.controller.delete_selected_category()
+        e.Skip()
+
+    def _mnu_add_on_click(self, e):
+        self.controller.add_category()
+
+    def _mnu_edit_on_click(self, e):
+        self.controller.edit_selected_category()
+
+    def _mnu_delete_on_click(self, e):
+        self.controller.delete_selected_category()
+
     def _on_tree_item_checked(self, e):
         tree_item = e.GetItem()
         cat = tree_item.GetData()
         tree_item_checked = tree_item.IsChecked()
-        self.view.view_properties.set_category_visible(cat, tree_item_checked)
-        self.view.redraw_timeline()
-        self._update_enableness()
+        self.controller.check_category(cat, tree_item_checked)
 
-    def _timeline_changed(self, state_change):
+
+class CategoriesTreeController(object):
+
+    def __init__(self, view, fn_handle_db_error):
+        self.view = view
+        self.db = None
+        self.timeline_view = None
+        self.fn_handle_db_error = fn_handle_db_error
+
+    def initialize_from_db(self, db):
+        self._change_active_db(db)
+        self.timeline_view = None
+        self._update_category_tree()
+
+    def initialize_from_timeline_view(self, view):
+        self._change_active_db(view.timeline)
+        self.timeline_view = view
+        self._update_category_tree()
+
+    def add_category(self):
+        add_category(self.view, self.db, self.fn_handle_db_error)
+
+    def edit_selected_category(self):
+        cat = self.view.get_selected_category()
+        if cat:
+            edit_category(self.view, self.db, cat, self.fn_handle_db_error)
+
+    def delete_selected_category(self):
+        cat = self.view.get_selected_category()
+        if cat:
+            delete_category(self.view, self.db, cat, self.fn_handle_db_error)
+
+    def check_category(self, cat, checked):
+        if self.timeline_view is None:
+            raise Exception("Checking not allowed when there is no "
+                            "timeline view.")
+        self.timeline_view.view_properties.set_category_visible(cat, checked)
+        self.timeline_view.redraw_timeline()
+
+    def destroy(self):
+        if self.db:
+            self.db.unregister(self._db_on_changed)
+
+    def _change_active_db(self, new_db):
+        if self.db:
+            self.db.unregister(self._db_on_changed)
+        self.db = new_db
+        if self.db:
+            self.db.register(self._db_on_changed)
+
+    def _db_on_changed(self, state_change):
         if state_change == STATE_CHANGE_CATEGORY:
-            self._update_categories()
+            self._update_category_tree()
 
-    def _update_categories(self):
+    def _update_category_tree(self):
         try:
-            categories = self.view.timeline.get_categories()
+            if self.db:
+                categories = self.db.get_categories()
+            else:
+                categories = []
         except TimelineIOError, e:
             self.fn_handle_db_error(e)
         else:
             tree = category_tree(categories)
-            self.DeleteAllItems()
-            self.root = self.AddRoot("") # This will be hidden
-            self._update_categories_from_tree(tree, self.root)
-            self.ExpandAll()
+            if self.timeline_view:
+                vp = self.timeline_view.view_properties
+            else:
+                vp = None
+            self.view.set_category_tree(tree, vp)
 
-    def _update_categories_from_tree(self, tree, root_item):
-        for (cat, subtree) in tree:
-            legend_panel = wx.Panel(self, size=(10, 10))
-            legend_panel.SetBackgroundColour(cat.color)
-            item = self.AppendItem(root_item, cat.name, ct_type=CHECKBOX_TYPE,
-                                   wnd=legend_panel, data=cat)
-            visible = self.view.view_properties.category_visible(cat)
-            self.CheckItem2(item, visible)
-            self._update_categories_from_tree(subtree, item)
-            self._update_enableness()
 
-    def _update_enableness(self):
-        def update(parent_visible, tree_items):
-            for item in tree_items:
-                self.EnableItem(item, parent_visible)
-                cat = item.GetData()
-                visible = self.view.view_properties.category_visible(cat)
-                update(visible, item.GetChildren())
-        update(True, self.root.GetChildren())
+def edit_category(parent_ctrl, db, cat, fn_handle_db_error):
+    """Open category editor to edit the given category."""
+    def create_category_editor():
+        return CategoryEditor(parent_ctrl, _("Edit Category"), db, cat)
+    gui_utils.show_modal(create_category_editor, fn_handle_db_error)
+
+
+def add_category(parent_ctrl, db, fn_handle_db_error):
+    """Open category editor to create a new category."""
+    def create_category_editor():
+        return CategoryEditor(parent_ctrl, _("Add Category"), db, None)
+    gui_utils.show_modal(create_category_editor, fn_handle_db_error)
+
+
+def delete_category(parent_ctrl, db, cat, fn_handle_db_error):
+    """Delete the given category after given confirmation from user."""
+    delete_warning = _("Are you sure you want to "
+                       "delete category '%s'?") % cat.name
+    if cat.parent is None:
+        update_warning = _("Events belonging to '%s' will no longer "
+                           "belong to a category.") % cat.name
+    else:
+        update_warning = _("Events belonging to '%s' will now belong "
+                           "to '%s'.") % (cat.name, cat.parent.name)
+    question = "%s\n\n%s" % (delete_warning, update_warning)
+    if _ask_question(question, parent_ctrl) == wx.YES:
+        try:
+            db.delete_category(cat)
+        except TimelineIOError, e:
+            fn_handle_db_error(e)
--- timelinelib/gui/dialogs/categorieseditor.py
+++ timelinelib/gui/dialogs/categorieseditor.py
@@ -27,6 +27,10 @@ from timelinelib.gui.utils import _ask_question
 from timelinelib.gui.utils import BORDER
 from timelinelib.gui.utils import ID_ERROR
 from timelinelib.gui.dialogs.categoryeditor import CategoryEditor
+from timelinelib.gui.components.cattree import CategoriesTree
+from timelinelib.gui.components.cattree import add_category
+from timelinelib.gui.components.cattree import edit_category
+from timelinelib.gui.components.cattree import delete_category
 from timelinelib.utils import ex_msg
 
 
@@ -41,23 +45,16 @@ class CategoriesEditor(wx.Dialog):
     def __init__(self, parent, timeline):
         wx.Dialog.__init__(self, parent, title=_("Edit Categories"))
         self._create_gui()
-        self.timeline = timeline
-        # Note: We must unregister before we close this dialog. When we close
-        # this dialog it will be disposed and self._timeline_changed will no
-        # longer exist. The next time the timeline gets updated it will try to
-        # call a method that does not exist.
-        self.timeline.register(self._timeline_changed)
-        self._update_categories()
+        self.db = timeline
+        self.cat_tree.initialize_from_db(self.db)
 
     def _create_gui(self):
         self.Bind(wx.EVT_CLOSE, self._window_on_close)
-        # The list box
-        self.lst_categories = wx.ListBox(self, size=(200, 180),
-                                         style=wx.LB_SINGLE)
-        self.Bind(wx.EVT_LISTBOX_DCLICK, self._lst_categories_on_dclick,
-                  self.lst_categories)
-        self.Bind(wx.EVT_LISTBOX, self._lst_categories_on_change,
-                  self.lst_categories)
+        # The tree
+        self.cat_tree = CategoriesTree(self, self.handle_db_error)
+        self.cat_tree.SetMinSize((-1, 200))
+        self.Bind(wx.EVT_TREE_SEL_CHANGED, self._cat_tree_on_sel_changed,
+                  self.cat_tree)
         # The Add button
         btn_add = wx.Button(self, wx.ID_ADD)
         self.Bind(wx.EVT_BUTTON, self._btn_add_on_click, btn_add)
@@ -71,10 +68,9 @@ class CategoriesEditor(wx.Dialog):
         btn_close.SetFocus()
         self.SetAffirmativeId(wx.ID_CLOSE)
         self.Bind(wx.EVT_BUTTON, self._btn_close_on_click, btn_close)
-        self.lst_categories.Bind(wx.EVT_KEY_DOWN, self._lst_categories_on_key_down)
         # Setup layout
         vbox = wx.BoxSizer(wx.VERTICAL)
-        vbox.Add(self.lst_categories, flag=wx.ALL|wx.EXPAND, border=BORDER)
+        vbox.Add(self.cat_tree, flag=wx.ALL|wx.EXPAND, border=BORDER)
         button_box = wx.BoxSizer(wx.HORIZONTAL)
         button_box.Add(btn_add, flag=wx.RIGHT, border=BORDER)
         button_box.Add(self.btn_del, flag=wx.RIGHT, border=BORDER)
@@ -82,77 +78,28 @@ class CategoriesEditor(wx.Dialog):
         button_box.Add(btn_close, flag=wx.LEFT, border=BORDER)
         vbox.Add(button_box, flag=wx.ALL|wx.EXPAND, border=BORDER)
         self.SetSizerAndFit(vbox)
-        self.lst_categories.SetFocus()
+
+    def handle_db_error(self, e):
+        gui_utils.handle_db_error_in_dialog(self, e)
+
+    def _cat_tree_on_sel_changed(self, e):
+        self._update_del_button()
 
     def _window_on_close(self, e):
-        # This will always be called before the dialog closes so we can do the
-        # unregister here.
-        self.timeline.unregister(self._timeline_changed)
+        self.cat_tree.destroy()
         self.EndModal(wx.ID_CLOSE)
 
-    def _lst_categories_on_dclick(self, e):
-        def create_category_editor():
-            return CategoryEditor(self, _("Edit Category"), self.timeline,
-                                  e.GetClientData())
-        gui_utils.show_modal(create_category_editor,
-                             gui_utils.create_dialog_db_error_handler(self))
-
-    def _lst_categories_on_change(self, e):
-        cat = self.lst_categories.GetSelection()
-        if cat > -1:
-            self.btn_del.Enable()
-
     def _btn_add_on_click(self, e):
-        def create_category_editor():
-            return CategoryEditor(self, _("Add Category"), self.timeline, None)
-        gui_utils.show_modal(create_category_editor,
-                             gui_utils.create_dialog_db_error_handler(self))
+        add_category(self, self.db, self.handle_db_error)
         
     def _btn_del_on_click(self, e):
-        try:
-            self._delete_selected_category()
-        except TimelineIOError, e:
-            gui_utils.handle_db_error_in_dialog(self, e)
+        cat = self.cat_tree.get_selected_category()
+        if cat:
+            delete_category(self, self.db, cat, self.handle_db_error)
+        self._update_del_button()
+
+    def _update_del_button(self):
+        self.btn_del.Enable(self.cat_tree.get_selected_category() is not None)
 
     def _btn_close_on_click(self, e):
         self.Close()
-
-    def _lst_categories_on_key_down(self, e):
-        try:
-            keycode = e.GetKeyCode()
-            if keycode == wx.WXK_DELETE:
-                self._delete_selected_category()
-            e.Skip()
-        except TimelineIOError, e:
-            handle_db_error_in_dialog(self, e)
-
-    def _timeline_changed(self, state_change):
-        if state_change == STATE_CHANGE_CATEGORY:
-            self._update_categories()
-
-    def _delete_selected_category(self):
-        selection = self.lst_categories.GetSelection()
-        if selection != wx.NOT_FOUND:
-            cat = self.lst_categories.GetClientData(selection)
-            delete_warning = _("Are you sure you want to "
-                               "delete category '%s'?") % cat.name
-            if cat.parent is None:
-                update_warning = _("Events belonging to '%s' will no longer "
-                                   "belong to a category.") % cat.name
-            else:
-                update_warning = _("Events belonging to '%s' will now belong "
-                                   "to '%s'.") % (cat.name, cat.parent.name)
-            question = "%s\n\n%s" % (delete_warning, update_warning)
-            if _ask_question(question, self) == wx.YES:
-                self.timeline.delete_category(cat)
-
-    def _update_categories(self):
-        try:
-            categories = self.timeline.get_categories()
-        except TimelineIOError, e:
-            handle_db_error_in_dialog(self, e)
-        else:
-            self.lst_categories.Clear()
-            for category in sort_categories(categories):
-                self.lst_categories.Append(category.name, category)
-            self.btn_del.Disable()
