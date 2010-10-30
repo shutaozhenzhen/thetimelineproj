ac292a68b800e0ed521b9c6f8de2477340232363
Rickard Lindberg <ricli85@gmail.com>
2010-10-30 12:38:04 +0200

    Create time type for py time.

---
+++ specs/PyTimeType.py
@@ -0,0 +1,50 @@
+# Copyright (C) 2009, 2010  Rickard Lindberg, Roger Lindberg
+#
+# This file is part of Timeline.
+#
+# Timeline is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Timeline is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Timeline.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import unittest
+import datetime
+
+from mock import Mock
+
+from timelinelib.db.time import PyTimeType
+
+
+class PyTimeTypeSpec(unittest.TestCase):
+
+    def setUp(self):
+        self.time_type = PyTimeType()
+
+    def testConvertsTimeToString(self):
+        self.assertEquals(
+            "2010-8-31 13:44:0",
+            self.time_type.time_string(datetime.datetime(2010, 8, 31, 13, 44)))
+
+    def testParsesTimeFromString(self):
+        self.assertEquals(
+            datetime.datetime(2010, 8, 31, 13, 44),
+            self.time_type.parse_time("2010-8-31 13:44:0"))
+
+    def testRaisesValueErrorWhenParsingInvalidTime(self):
+        self.assertRaises(
+            ValueError,
+            self.time_type.parse_time, "2010-31-31 0:0:0")
+
+    def testRaisesValueErrorWhenParsingBadlyFormattedTime(self):
+        self.assertRaises(
+            ValueError,
+            self.time_type.parse_time, "2010-31-hello 0:0:0")
---
+++ timelinelib/db/time.py
@@ -0,0 +1,52 @@
+# Copyright (C) 2009, 2010  Rickard Lindberg, Roger Lindberg
+#
+# This file is part of Timeline.
+#
+# Timeline is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Timeline is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Timeline.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import re
+import datetime
+
+
+class TimeType(object):
+
+    def time_string(self, time):
+        raise NotImplementedError("time_string not implemented.")
+
+    def parse_time(self, time_string):
+        raise NotImplementedError("parse_time not implemented.")
+
+
+class PyTimeType(TimeType):
+
+    def time_string(self, time):
+        return "%s-%s-%s %s:%s:%s" % (time.year, time.month, time.day,
+                                      time.hour, time.minute, time.second)
+
+    def parse_time(self, time_string):
+        match = re.search(r"^(\d+)-(\d+)-(\d+) (\d+):(\d+):(\d+)$", time_string)
+        if match:
+            year = int(match.group(1))
+            month = int(match.group(2))
+            day = int(match.group(3))
+            hour = int(match.group(4))
+            minute = int(match.group(5))
+            second = int(match.group(6))
+            try:
+                return datetime.datetime(year, month, day, hour, minute, second)
+            except ValueError:
+                raise ValueError("Invalid time, time string = '%s'" % time_string)
+        else:
+            raise ValueError("Time not on correct format = '%s'" % time_string)
