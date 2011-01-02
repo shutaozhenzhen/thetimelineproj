d4e45ca6f39f2a006756fbd7422869fc42c45a8d
Roger Lindberg <roger@rolidata.se>
2011-01-02 11:31:13 +0100

    Added 'use wide date range' checkbox to the preferences dialog

---
+++ specs/PreferencesDialog.py
@@ -0,0 +1,47 @@
+# Copyright (C) 2009, 2010  Rickard Lindberg, Roger Lindberg
+#
+# This file is part of Timeline.
+#
+# Timeline is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Timeline is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Timeline.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import unittest
+
+from mock import Mock
+
+from timelinelib.gui.dialogs.preferences import PreferencesDialog
+from timelinelib.gui.dialogs.preferences import PreferencesDialogController
+from timelinelib.config import Config
+
+
+class PreferencesDialogSpec(unittest.TestCase):
+    
+    def setUp(self):
+        self.preferences_dialog = Mock(PreferencesDialog)
+        self.config = Mock(Config)
+        self.controller = PreferencesDialogController(self.preferences_dialog, self.config)
+        
+    def testOpensWithWideDateRangeIfSetInConfig(self):
+        self.config.get_use_wide_date_range.return_value = True
+        self.controller.initialize_controls()
+        self.preferences_dialog.set_checkbox_enable_wide_date_range.assert_called_with(True)
+
+    def testOpensWithNonwideDateRangeIfNotSetInConfig(self):
+        self.config.get_use_wide_date_range.return_value = False
+        self.controller.initialize_controls()
+        self.preferences_dialog.set_checkbox_enable_wide_date_range.assert_called_with(False)
+
+    def testConfigChangesWhenWideDateRangeChanges(self):
+        self.controller.on_use_wide_date_range_changed(False)
+        self.config.set_use_wide_date_range.assert_called_with(False)
--- timelinelib/config.py
+++ timelinelib/config.py
@@ -224,3 +224,9 @@ class Config(object):
             raise ValueError("Invalid week start.")
         self.config_parser.set(DEFAULTSECT, WEEK_START, week_start)
     week_start = property(get_week_start, set_week_start)
+    
+    def get_use_wide_date_range(self):
+        return True
+
+    def set_use_wide_date_range(self, value):
+        pass
