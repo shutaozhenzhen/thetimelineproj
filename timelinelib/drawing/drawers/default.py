6c39c2cc44b5497bbfca6404a059a92772bb722e
Rickard Lindberg <ricli85@gmail.com>
2011-01-08 17:42:33 +0100

    Move layout functionality from DefaultDrawingAlgorithm to TimelineScene class.

--- timelinelib/drawing/drawers/default.py
+++ timelinelib/drawing/drawers/default.py
@@ -30,6 +30,7 @@ from timelinelib.drawing.interface import Drawer
 from timelinelib.drawing.utils import get_default_font
 from timelinelib.drawing.utils import Metrics
 from timelinelib.drawing.utils import darken_color
+from timelinelib.drawing.scene import TimelineScene
 from timelinelib.gui.utils import sort_categories
 from timelinelib.db.objects import TimePeriod
 from timelinelib.paths import ICONS_DIR
@@ -37,7 +38,6 @@ from timelinelib.paths import ICONS_DIR
 
 OUTER_PADDING = 5      # Space between event boxes (pixels)
 INNER_PADDING = 3      # Space inside event box to text (pixels)
-BASELINE_PADDING = 15  # Extra space to move events away from baseline (pixels)
 PERIOD_THRESHOLD = 20  # Periods smaller than this are drawn as events (pixels)
 BALLOON_RADIUS = 12
 DATA_INDICATOR_SIZE = 10
@@ -65,7 +65,7 @@ class DefaultDrawingAlgorithm(Drawer):
         self.black_dashed_pen.SetCap(wx.CAP_BUTT)
         self.grey_solid_pen = wx.Pen(wx.Color(200, 200, 200), 1, wx.SOLID)
         self.red_solid_pen = wx.Pen(wx.Color(255, 0, 0), 1, wx.SOLID)
-        
+
     def _create_brushes(self):
         self.white_solid_brush = wx.Brush(wx.Color(255, 255, 255), wx.SOLID)
         self.black_solid_brush = wx.Brush(wx.Color(0, 0, 0), wx.SOLID)
@@ -77,6 +77,8 @@ class DefaultDrawingAlgorithm(Drawer):
         return period_width_in_pixels > PERIOD_THRESHOLD
 
     def draw(self, dc, timeline, view_properties):
+        self.scene = TimelineScene(dc, timeline, view_properties)
+        self.scene.create()
         self.dc = dc
         self.time_period = view_properties.displayed_period
         self.db = timeline
@@ -84,8 +86,6 @@ class DefaultDrawingAlgorithm(Drawer):
         self.metrics = Metrics(dc.GetSizeTuple(), self.time_type, 
                                self.time_period, 
                                view_properties.divider_position)
-        self._calc_event_positions(view_properties)
-        self._calc_strips()
         self._perform_drawing(view_properties)
         del self.dc # Program crashes if we don't delete the dc reference.
 
@@ -95,12 +95,7 @@ class DefaultDrawingAlgorithm(Drawer):
         self._draw_events(view_properties)
         self._draw_legend(view_properties, self._extract_categories())
         self._draw_ballons(view_properties)
-        
-    def _calc_event_positions(self, view_properties):
-        events_from_db = self.db.get_events(self.time_period)
-        visible_events = view_properties.filter_events(events_from_db)
-        self._calc_rects(visible_events)
-        
+
     def snap(self, time, snap_region=10):
         if self._distance_to_left_border(time) < snap_region:
             return self._get_time_at_left_border(time)
@@ -120,42 +115,42 @@ class DefaultDrawingAlgorithm(Drawer):
     def _get_time_at_left_border(self, time):
         left_strip_time, right_strip_time = self._snap_region(time)
         return left_strip_time
-        
+
     def _get_time_at_right_border(self, time):
         left_strip_time, right_strip_time = self._snap_region(time)
         return right_strip_time
-        
+
     def _distance_between_times(self, time1, time2):
         time1_x = self.metrics.calc_exact_x(time1)
         time2_x = self.metrics.calc_exact_x(time2)
         distance = abs(time1_x - time2_x)
         return distance
-        
+
     def _snap_region(self, time): 
         major_strip, minor_strip = self._choose_strip()
         time_x = self.metrics.calc_exact_x(time)
         left_strip_time = minor_strip.start(time)
         right_strip_time = minor_strip.increment(left_strip_time)
         return (left_strip_time, right_strip_time)
-    
+
     def snap_selection(self, period_selection):
         start, end = period_selection
         return (self.snap(start), self.snap(end))
 
     def event_at(self, x, y):
-        for (event, rect) in self.event_data:
+        for (event, rect) in self.scene.event_data:
             if rect.Contains(wx.Point(x, y)):
                 return event
         return None
 
     def event_with_rect_at(self, x, y):
-        for (event, rect) in self.event_data:
+        for (event, rect) in self.scene.event_data:
             if rect.Contains(wx.Point(x, y)):
                 return (event, rect)
         return None
 
     def event_rect(self, evt):
-        for (event, rect) in self.event_data:
+        for (event, rect) in self.scene.event_data:
             if evt == event:
                 return rect
         return None
@@ -166,114 +161,6 @@ class DefaultDrawingAlgorithm(Drawer):
             if rect.Contains(wx.Point(x, y)):
                 event = event_in_list
         return event
-
-    def _calc_rects(self, events):
-        self.event_data = []
-        for event in events:
-            rect = self._create_rectangle_for_event(event)
-            self.event_data.append((event, rect))
-        for (event, rect) in self.event_data:
-            rect.Deflate(OUTER_PADDING, OUTER_PADDING)
-
-    def _create_rectangle_for_event(self, event):
-        rect = self._create_ideal_rect_for_event(event)
-        self._ensure_rect_is_not_far_outisde_screen(rect)
-        self._prevent_overlap(rect, self._get_y_move_direction(event))
-        return rect
-        
-    def _get_y_move_direction(self, event):
-        if self._display_as_period(event):
-            return 1
-        else:
-            return -1
-
-    def _create_ideal_rect_for_event(self, event):
-        if self._display_as_period(event):
-            return self._create_ideal_rect_for_period_event(event)
-        else:
-            return self._create_ideal_rect_for_non_period_event(event)
-
-    def _display_as_period(self, event):
-        event_width = self.metrics.calc_width(event.time_period)
-        return event_width > PERIOD_THRESHOLD
-
-    def _create_ideal_rect_for_period_event(self, event):
-        self.dc.SetFont(self.small_text_font)
-        tw, th = self.dc.GetTextExtent(event.text)
-        ew = self.metrics.calc_width(event.time_period)
-        rw = ew + 2 * OUTER_PADDING
-        rh = th + 2 * INNER_PADDING + 2 * OUTER_PADDING
-        rx = (self.metrics.calc_x(event.time_period.start_time) -
-              OUTER_PADDING)
-        ry = self.metrics.half_height + BASELINE_PADDING
-        rect = wx.Rect(rx, ry, rw, rh)
-        return rect
-
-    def _create_ideal_rect_for_non_period_event(self, event):
-        self.dc.SetFont(self.small_text_font)
-        tw, th = self.dc.GetTextExtent(event.text)
-        rw = tw + 2 * INNER_PADDING + 2 * OUTER_PADDING
-        rh = th + 2 * INNER_PADDING + 2 * OUTER_PADDING
-        if event.has_data():
-            rw += DATA_INDICATOR_SIZE / 3
-        rx = self.metrics.calc_x(event.mean_time()) - rw / 2
-        ry = self.metrics.half_height - rh - BASELINE_PADDING
-        rect = wx.Rect(rx, ry, rw, rh)
-        return rect
-
-    def _ensure_rect_is_not_far_outisde_screen(self, rect):
-        # Drawing stuff on huge x-coordinates causes drawing to fail.
-        # MARGIN must be big enough to hide outer padding, borders, and
-        # selection markers.
-        rx = rect.GetX()
-        rw = rect.GetWidth()
-        MARGIN = 50
-        if rx < -MARGIN:
-            distance_beyond_left_margin = -rx - MARGIN
-            rx += distance_beyond_left_margin
-            rw -= distance_beyond_left_margin
-        right_edge_x = rx + rw
-        if right_edge_x > self.metrics.width + MARGIN:
-            rw -= right_edge_x - self.metrics.width - MARGIN
-        rect.SetX(rx)
-        rect.SetWidth(rw)
-
-    def _prevent_overlap(self, rect, y_move_direction):
-        while True:
-            intersection_height = self._intersection_height(rect)
-            rect.Y += y_move_direction * intersection_height
-            if intersection_height == 0:
-                break
-            if self._rect_above_or_below_screen(rect):
-                # Optimization: Don't prevent overlap if rect is pushed
-                # outside screen
-                break
-
-    def _rect_above_or_below_screen(self, rect):
-        return rect.Y > self.metrics.height or (rect.Y + rect.Height) < 0
-
-    def _intersection_height(self, rect):
-        for (event, r) in self.event_data:
-            if rect.Intersects(r):
-                r_copy = wx.Rect(*r) # Because `Intersect` modifies rect
-                intersection = r_copy.Intersect(rect)
-                return intersection.Height
-        return 0
-
-    def _calc_strips(self):
-        """Fill the two arrays `minor_strip_data` and `major_strip_data`."""
-        def fill(list, strip):
-            """Fill the given list with the given strip."""
-            current_start = strip.start(self.time_period.start_time)
-            while current_start < self.time_period.end_time:
-                next_start = strip.increment(current_start)
-                list.append(TimePeriod(self.db.get_time_type(), current_start, next_start))
-                current_start = next_start
-        self.major_strip_data = [] # List of time_period
-        self.minor_strip_data = [] # List of time_period
-        major_strip, minor_strip = self.time_type.choose_strip(self.metrics)
-        fill(self.major_strip_data, major_strip)
-        fill(self.minor_strip_data, minor_strip)
 
     def _choose_strip(self):
         """
@@ -301,7 +188,7 @@ class DefaultDrawingAlgorithm(Drawer):
         self._draw_now_line()
 
     def _draw_minor_strips(self):
-        for strip_period in self.minor_strip_data:
+        for strip_period in self.scene.minor_strip_data:
             self._draw_minor_strip_divider_line_at(strip_period.end_time)
             self._draw_minor_strip_label(strip_period)
 
@@ -318,12 +205,12 @@ class DefaultDrawingAlgorithm(Drawer):
         middle = self.metrics.calc_x(strip_period.mean_time())
         middley = self.metrics.half_height
         self.dc.DrawText(label, middle - tw / 2, middley - th)
-        
+
     def _draw_major_strips(self):
         major_strip, minor_strip = self._choose_strip()
         self.dc.SetFont(self.header_font)
         self.dc.SetPen(self.grey_solid_pen)
-        for tp in self.major_strip_data:
+        for tp in self.scene.major_strip_data:
             # Divider line
             x = self.metrics.calc_x(tp.end_time)
             self.dc.DrawLine(x, 0, x, self.metrics.height)
@@ -345,15 +232,15 @@ class DefaultDrawingAlgorithm(Drawer):
                 if x < left:
                     x = left + INNER_PADDING
             self.dc.DrawText(label, x, INNER_PADDING)
-        
+
     def _draw_divider_line(self):
         self.dc.SetPen(self.black_solid_pen)
         self.dc.DrawLine(0, self.metrics.half_height, self.metrics.width,
                          self.metrics.half_height)
-        
+
     def _draw_lines_to_non_period_events(self, view_properties):
         self.dc.SetBrush(self.black_solid_brush)
-        for (event, rect) in self.event_data:
+        for (event, rect) in self.scene.event_data:
             if rect.Y < self.metrics.half_height:
                 x = self.metrics.calc_x(event.mean_time())
                 y = rect.Y + rect.Height / 2
@@ -365,17 +252,17 @@ class DefaultDrawingAlgorithm(Drawer):
                     self.dc.SetPen(self.black_solid_pen)
                 self.dc.DrawLine(x, y, x, self.metrics.half_height)
                 self.dc.DrawCircle(x, self.metrics.half_height, 2)
-        
+
     def _draw_now_line(self):
         now_time = self.time_type.now()
         if self.time_period.inside(now_time):
             self.dc.SetPen(self.darkred_solid_pen)
             x = self.metrics.calc_x(now_time)
             self.dc.DrawLine(x, 0, x, self.metrics.height)
-                
+
     def _extract_categories(self):
         categories = []
-        for (event, rect) in self.event_data:
+        for (event, rect) in self.scene.event_data:
             cat = event.category
             if cat and not cat in categories:
                 categories.append(cat)
@@ -436,7 +323,7 @@ class DefaultDrawingAlgorithm(Drawer):
         """Draw all event boxes and the text inside them."""
         self.dc.SetFont(self.small_text_font)
         self.dc.SetTextForeground((0, 0, 0))
-        for (event, rect) in self.event_data:
+        for (event, rect) in self.scene.event_data:
             # Ensure that we can't draw outside rectangle
             self.dc.DestroyClippingRegion()
             self.dc.SetClippingRect(rect)
@@ -546,7 +433,7 @@ class DefaultDrawingAlgorithm(Drawer):
         self.balloon_data = []     # List of (event, rect)
         top_event = None
         top_rect = None
-        for (event, rect) in self.event_data:
+        for (event, rect) in self.scene.event_data:
             if (event.get_data("description") != None or
                 event.get_data("icon") != None):
                 sticky = view_properties.event_has_sticky_balloon(event)
