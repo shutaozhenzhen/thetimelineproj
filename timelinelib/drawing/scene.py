eb84c51d2c519909d55a808f9c54ec323098c224
Roger Lindberg <roger@rolidata.se>
2011-05-13 14:07:15 +0200

    Implemented drawing of locked event edges

--- timelinelib/drawing/scene.py
+++ timelinelib/drawing/scene.py
@@ -197,6 +197,8 @@ class TimelineScene(object):
         rh = th + 2 * self._inner_padding + 2 * self._outer_padding
         if event.has_data():
             rw += self._data_indicator_size / 3
+        if event.fuzzy or event.locked:
+            rw += th + 2 * self._inner_padding
         rx = self._metrics.calc_x(event.mean_time()) - rw / 2
         ry = self._metrics.half_height - rh - self._baseline_padding
         rect = wx.Rect(rx, ry, rw, rh)
