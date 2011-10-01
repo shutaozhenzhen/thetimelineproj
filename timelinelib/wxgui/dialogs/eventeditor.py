b4cf897e8f0592f82a75bf2bd4c99bf8dc1ae0f0
Rickard Lindberg <ricli85@gmail.com>
2011-10-01 14:26:04 +0200

    Restructure methods in event editor dialog.

--- timelinelib/wxgui/dialogs/eventeditor.py
+++ timelinelib/wxgui/dialogs/eventeditor.py
@@ -38,16 +38,6 @@ class EventEditorDialog(wx.Dialog):
 
     def __init__(self, parent, config, title, timeline,
                  start=None, end=None, event=None):
-        """
-        The 'event' argument is optional. If it is given the dialog is used
-        to edit this event and the controls are filled with data from
-        the event and the arguments 'start' and 'end' are ignored.
-
-        If the 'event' argument isn't given the dialog is used to create a
-        new event, and the controls for start and end time are initially
-        filled with data from the arguments 'start' and 'end' if they are
-        given. Otherwise they will default to today.
-        """
         wx.Dialog.__init__(self, parent, title=title, name="event_editor",
                            style=wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER)
         self.timeline = timeline
@@ -57,111 +47,6 @@ class EventEditorDialog(wx.Dialog):
         self.controller.edit(
             timeline.get_time_type(), DbWrapperEventRepository(timeline),
             start, end, event)
-
-    def set_focus(self, control_name):
-        controls = {"start" : self.dtp_start, "text" : self.txt_text}
-        if controls.has_key(control_name):
-            controls[control_name].SetFocus()
-        else:
-            self.dtp_start.SetFocus()
-
-    def set_start(self, start):
-        self.dtp_start.set_value(start)
-
-    def get_start(self):
-        return self.dtp_start.get_value()
-
-    def set_end(self, start):
-        self.dtp_end.set_value(start)
-
-    def get_end(self):
-        return self.dtp_end.get_value()
-
-    def get_show_period(self):
-        return self.chb_period.IsChecked()
-
-    def set_show_period(self, show):
-        self.chb_period.SetValue(show)
-        self._show_to_time(show)
-
-    def set_show_time(self, checked):
-        self.chb_show_time.SetValue(checked)
-        self.dtp_start.show_time(checked)
-        self.dtp_end.show_time(checked)
-
-    def set_show_add_more(self, visible):
-        self.chb_add_more.Show(visible)
-        self.chb_add_more.SetValue(False)
-
-    def set_fuzzy(self, fuzzy):
-        self.chb_fuzzy.SetValue(fuzzy)
-
-    def get_fuzzy(self):
-        return self.chb_fuzzy.GetValue()
-
-    def get_show_add_more(self):
-        return self.chb_add_more.GetValue()
-
-    def set_locked(self, locked):
-        self.chb_locked.SetValue(locked)
-        self.chb_ends_today.Enable(not self.chb_locked.GetValue())
-
-    def get_locked(self):
-        return self.chb_locked.GetValue()
-
-    def set_ends_today(self, value):
-        self.chb_ends_today.SetValue(value)
-
-    def get_ends_today(self):
-        return self.chb_ends_today.GetValue()
-
-    def set_name(self, name):
-        self.txt_text.SetValue(name)
-        
-    def get_name(self):
-        return self.txt_text.GetValue().strip()
-    
-    def set_category(self, category):
-        self._fill_categories_listbox(category)
-        
-    def get_category(self):
-        selection = self.lst_category.GetSelection()
-        category = self.lst_category.GetClientData(selection)
-        return category
-
-    def set_event_data(self, event_data):
-        for data_id, editor in self.event_data:
-            if event_data.has_key(data_id):
-                data = event_data[data_id]
-                if data is not None:
-                    editor.set_data(data)
-                
-    def get_event_data(self):
-        event_data = {}
-        for data_id, editor in self.event_data:
-            data = editor.get_data()
-            if data != None:
-                event_data[data_id] = editor.get_data()
-        return event_data
-
-    def display_invalid_start(self, message):
-        self._display_invalid_input(message, self.dtp_start)
-
-    def display_invalid_end(self, message):
-        self._display_invalid_input(message, self.dtp_end)
-
-    def display_invalid_name(self, message):
-        self._display_invalid_input(message, self.txt_text)
-
-    def display_db_exception(self, e):
-        gui_utils.handle_db_error_in_dialog(self, e)
-
-    def _display_invalid_input(self, message, control):
-        _display_error_message(message, self)
-        _set_focus_and_select(control)  
-
-    def display_error_message(self, message):
-        _display_error_message(message, self)
             
     def _create_gui(self):
         properties_box = self._create_properties_box()
@@ -174,16 +59,6 @@ class EventEditorDialog(wx.Dialog):
         self._create_main_box_content(properties_box)
         return properties_box
 
-    def _create_checkbox_add_more(self, properties_box):
-        label = _("Add more events after this one")
-        self.chb_add_more = wx.CheckBox(self, label=label)
-        properties_box.Add(self.chb_add_more, flag=wx.ALL, border=BORDER)
-
-    def _create_buttons(self, properties_box):
-        button_box = self.CreateStdDialogButtonSizer(wx.OK|wx.CANCEL)
-        self.Bind(wx.EVT_BUTTON, self._btn_ok_on_click, id=wx.ID_OK)
-        properties_box.Add(button_box, flag=wx.EXPAND|wx.ALL, border=BORDER)
-    
     def _create_main_box_content(self, properties_box):
         groupbox = wx.StaticBox(self, wx.ID_ANY, _("Event Properties"))
         main_box_content = wx.StaticBoxSizer(groupbox, wx.VERTICAL)
@@ -191,17 +66,12 @@ class EventEditorDialog(wx.Dialog):
         self._create_notebook_content(main_box_content)
         properties_box.Add(main_box_content, flag=wx.EXPAND|wx.ALL, 
                            border=BORDER, proportion=1)
-         
+
     def _create_detail_content(self, properties_box_content):
         details = self._create_details()
         properties_box_content.Add(details, flag=wx.ALL|wx.EXPAND, 
                                    border=BORDER)
-    
-    def _create_notebook_content(self, properties_box_content):
-        notebook = self._create_notebook()
-        properties_box_content.Add(notebook, border=BORDER, 
-                                   flag=wx.ALL|wx.EXPAND, proportion=1)
-    
+
     def _create_details(self):
         grid = wx.FlexGridSizer(4, 2, BORDER, BORDER)
         grid.AddGrowableCol(1)
@@ -210,7 +80,7 @@ class EventEditorDialog(wx.Dialog):
         self._create_text_field(grid)
         self._create_categories_listbox(grid)
         return grid    
-    
+
     def _create_time_details(self, grid):
         grid.Add(wx.StaticText(self, label=_("When:")),
                  flag=wx.ALIGN_CENTER_VERTICAL)
@@ -230,7 +100,7 @@ class EventEditorDialog(wx.Dialog):
     def _create_time_picker(self):
         time_type = self.timeline.get_time_type()
         return time_picker_for(time_type)(self, config=self.config)
-
+    
     def _create_checkboxes(self, grid):
         grid.AddStretchSpacer()
         when_box = wx.BoxSizer(wx.HORIZONTAL)
@@ -246,9 +116,16 @@ class EventEditorDialog(wx.Dialog):
         handler = self._chb_period_on_checkbox
         return self._create_chb(box, _("Period"), handler)
 
+    def _chb_period_on_checkbox(self, e):
+        self._show_to_time(e.IsChecked())
+
     def _create_show_time_checkbox(self, box):
         handler = self._chb_show_time_on_checkbox
         return self._create_chb(box, _("Show time"), handler)
+
+    def _chb_show_time_on_checkbox(self, e):
+        self.dtp_start.show_time(e.IsChecked())
+        self.dtp_end.show_time(e.IsChecked())
 
     def _create_fuzzy_checkbox(self, box):
         handler = None
@@ -258,10 +135,13 @@ class EventEditorDialog(wx.Dialog):
         handler = self._chb_show_time_on_locked
         return self._create_chb(box, _("Locked"), handler)
 
+    def _chb_show_time_on_locked(self, e):
+        self.chb_ends_today.Enable(not self.chb_locked.GetValue())
+
     def _create_ends_today_checkbox(self, box):
         handler = None
         return self._create_chb(box, _("Ends today"), handler)
-        
+
     def _create_chb(self, box, label, handler):
         chb = wx.CheckBox(self, label=label)
         if handler is not None:
@@ -282,56 +162,6 @@ class EventEditorDialog(wx.Dialog):
         grid.Add(self.lst_category)
         self.Bind(wx.EVT_CHOICE, self._lst_category_on_choice, 
                   self.lst_category)
-        
-    def _create_notebook(self):
-        self.event_data = []
-        notebook = wx.Notebook(self, style=wx.BK_DEFAULT)
-        for data_id in self.timeline.supported_event_data():
-            self._add_editor(notebook, data_id)
-        return notebook
-     
-    def _add_editor(self, notebook, data_id):
-        editor_class_decription = self._get_editor_class_description(data_id)
-        if editor_class_decription is None:
-            return
-        editor = self._create_editor(notebook, editor_class_decription)
-        self.event_data.append((data_id, editor))
-
-    def _get_editor_class_description(self, editor_class_id):
-        editors = {"description" : (_("Description"), DescriptionEditor),
-                   "icon" : (_("Icon"), IconEditor) }
-        if editors.has_key(editor_class_id):
-            return editors[editor_class_id]  
-        else:
-            return None
-
-    def _create_editor(self, notebook, editor_class_decription):
-        name, editor_class = editor_class_decription
-        panel = wx.Panel(notebook)
-        editor = editor_class(panel)
-        notebook.AddPage(panel, name)
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(editor, flag=wx.EXPAND, proportion=1)
-        panel.SetSizer(sizer)
-        return editor
-        
-    def _btn_ok_on_click(self, evt):
-        self.controller.create_or_update_event()
-
-    def _clear_dialog(self):
-        self.controller.clear()
-        for data_id, editor in self.event_data:
-            editor.clear_data()
-        
-    def _chb_period_on_checkbox(self, e):
-        self._show_to_time(e.IsChecked())
-
-    def _chb_show_time_on_checkbox(self, e):
-        self.dtp_start.show_time(e.IsChecked())
-        self.dtp_end.show_time(e.IsChecked())
-
-    def _chb_show_time_on_locked(self, e):
-        self.chb_ends_today.Enable(not self.chb_locked.GetValue())
 
     def _lst_category_on_choice(self, e):
         new_selection_index = e.GetSelection()
@@ -371,6 +201,61 @@ class EventEditorDialog(wx.Dialog):
                              gui_utils.create_dialog_db_error_handler(self),
                              handle_success)
 
+    def _create_notebook_content(self, properties_box_content):
+        notebook = self._create_notebook()
+        properties_box_content.Add(notebook, border=BORDER, 
+                                   flag=wx.ALL|wx.EXPAND, proportion=1)
+
+    def _create_notebook(self):
+        self.event_data = []
+        notebook = wx.Notebook(self, style=wx.BK_DEFAULT)
+        for data_id in self.timeline.supported_event_data():
+            self._add_editor(notebook, data_id)
+        return notebook
+     
+    def _add_editor(self, notebook, data_id):
+        editor_class_decription = self._get_editor_class_description(data_id)
+        if editor_class_decription is None:
+            return
+        editor = self._create_editor(notebook, editor_class_decription)
+        self.event_data.append((data_id, editor))
+
+    def _get_editor_class_description(self, editor_class_id):
+        editors = {"description" : (_("Description"), DescriptionEditor),
+                   "icon" : (_("Icon"), IconEditor) }
+        if editors.has_key(editor_class_id):
+            return editors[editor_class_id]  
+        else:
+            return None
+
+    def _create_editor(self, notebook, editor_class_decription):
+        name, editor_class = editor_class_decription
+        panel = wx.Panel(notebook)
+        editor = editor_class(panel)
+        notebook.AddPage(panel, name)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(editor, flag=wx.EXPAND, proportion=1)
+        panel.SetSizer(sizer)
+        return editor
+    
+    def _create_checkbox_add_more(self, properties_box):
+        label = _("Add more events after this one")
+        self.chb_add_more = wx.CheckBox(self, label=label)
+        properties_box.Add(self.chb_add_more, flag=wx.ALL, border=BORDER)
+
+    def _create_buttons(self, properties_box):
+        button_box = self.CreateStdDialogButtonSizer(wx.OK|wx.CANCEL)
+        self.Bind(wx.EVT_BUTTON, self._btn_ok_on_click, id=wx.ID_OK)
+        properties_box.Add(button_box, flag=wx.EXPAND|wx.ALL, border=BORDER)
+        
+    def _btn_ok_on_click(self, evt):
+        self.controller.create_or_update_event()
+
+    def _clear_dialog(self):
+        self.controller.clear()
+        for data_id, editor in self.event_data:
+            editor.clear_data()
+        
     def _show_to_time(self, show=True):
         self.lbl_to.Show(show)
         self.dtp_end.Show(show)
@@ -412,6 +297,101 @@ class EventEditorDialog(wx.Dialog):
     def _close(self):
         # TODO: Replace with EventRuntimeData
         self.EndModal(wx.ID_OK)
+
+    def set_start(self, start):
+        self.dtp_start.set_value(start)
+    def get_start(self):
+        return self.dtp_start.get_value()
+
+    def set_end(self, start):
+        self.dtp_end.set_value(start)
+    def get_end(self):
+        return self.dtp_end.get_value()
+
+    def set_show_period(self, show):
+        self.chb_period.SetValue(show)
+        self._show_to_time(show)
+    def get_show_period(self):
+        return self.chb_period.IsChecked()
+
+    def set_show_time(self, checked):
+        self.chb_show_time.SetValue(checked)
+        self.dtp_start.show_time(checked)
+        self.dtp_end.show_time(checked)
+
+    def get_fuzzy(self):
+        return self.chb_fuzzy.GetValue()
+    def set_fuzzy(self, fuzzy):
+        self.chb_fuzzy.SetValue(fuzzy)
+
+    def get_locked(self):
+        return self.chb_locked.GetValue()
+    def set_locked(self, locked):
+        self.chb_locked.SetValue(locked)
+        self.chb_ends_today.Enable(not self.chb_locked.GetValue())
+
+    def get_ends_today(self):
+        return self.chb_ends_today.GetValue()
+    def set_ends_today(self, value):
+        self.chb_ends_today.SetValue(value)
+
+    def set_name(self, name):
+        self.txt_text.SetValue(name)
+    def get_name(self):
+        return self.txt_text.GetValue().strip()
+    
+    def set_category(self, category):
+        self._fill_categories_listbox(category)
+    def get_category(self):
+        selection = self.lst_category.GetSelection()
+        category = self.lst_category.GetClientData(selection)
+        return category
+
+    def set_event_data(self, event_data):
+        for data_id, editor in self.event_data:
+            if event_data.has_key(data_id):
+                data = event_data[data_id]
+                if data is not None:
+                    editor.set_data(data)
+    def get_event_data(self):
+        event_data = {}
+        for data_id, editor in self.event_data:
+            data = editor.get_data()
+            if data != None:
+                event_data[data_id] = editor.get_data()
+        return event_data
+
+    def set_show_add_more(self, visible):
+        self.chb_add_more.Show(visible)
+        self.chb_add_more.SetValue(False)
+    def get_show_add_more(self):
+        return self.chb_add_more.GetValue()
+
+    def set_focus(self, control_name):
+        controls = {"start" : self.dtp_start, "text" : self.txt_text}
+        if controls.has_key(control_name):
+            controls[control_name].SetFocus()
+        else:
+            self.dtp_start.SetFocus()
+
+    def display_invalid_start(self, message):
+        self._display_invalid_input(message, self.dtp_start)
+
+    def display_invalid_end(self, message):
+        self._display_invalid_input(message, self.dtp_end)
+
+    def display_invalid_name(self, message):
+        self._display_invalid_input(message, self.txt_text)
+
+    def _display_invalid_input(self, message, control):
+        _display_error_message(message, self)
+        _set_focus_and_select(control)  
+
+    def display_db_exception(self, e):
+        gui_utils.handle_db_error_in_dialog(self, e)
+
+    def display_error_message(self, message):
+        _display_error_message(message, self)
 
 
 class DescriptionEditor(wx.TextCtrl):
