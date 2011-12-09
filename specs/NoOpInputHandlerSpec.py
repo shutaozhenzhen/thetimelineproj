64a7afb715fbb89f7943f93e35c96522e9e40211
Rickard Lindberg <ricli85@gmail.com>
2011-12-09 18:17:20 +0100

    Prevent events with ends_today=True from being moved by drag.

--- specs/NoOpInputHandlerSpec.py
+++ specs/NoOpInputHandlerSpec.py
@@ -21,7 +21,7 @@ import unittest
 from mock import Mock
 import wx
 
-from specs.utils import an_event, human_time_to_py
+from specs.utils import an_event, an_event_with, human_time_to_py
 from timelinelib.view.drawingarea import DrawingArea
 from timelinelib.view.noop import NoOpInputHandler
 from timelinelib.wxgui.components.timelineview import DrawingAreaPanel
@@ -37,6 +37,15 @@ class NoOpInputHandlerSpec(unittest.TestCase):
         self.given_event_selected(event)
         self.handler.left_mouse_down(10, 10, False, False)
         self.drawing_area.change_input_handler_to_move_by_drag.assert_called_once_with(event, time)
+
+    def test_disables_move_handler_when_event_ends_today(self):
+        event = an_event_with(ends_today=True)
+        time = human_time_to_py("1 Jan 2011")
+        self.given_time_at_x_is(10, time)
+        self.given_event_with_rect_at(10, 10, event, wx.Rect(0, 0, 20, 20))
+        self.given_event_selected(event)
+        self.handler.left_mouse_down(10, 10, False, False)
+        self.assertEquals(0, self.drawing_area.change_input_handler_to_move_by_drag.call_count)
 
     def setUp(self):
         self.setup_drawing_area_mock()
