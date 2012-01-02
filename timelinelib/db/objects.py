c59f6e2f8a67d5d556a9dfc4e6a474256f9893a2
Roger Lindberg <roger@rolidata.se>
2012-01-02 19:39:08 +0100

    Event object 'interface functions'

--- timelinelib/db/objects.py
+++ timelinelib/db/objects.py
@@ -138,7 +138,13 @@ class Event(object):
         new_event.set_data("icon", self.get_data("icon"))    
         return new_event
 
-
+    def is_container(self):
+        return False
+    
+    def is_subevent(self):
+        return False
+    
+    
 class Category(object):
     """
     Store persistent data about a category.
