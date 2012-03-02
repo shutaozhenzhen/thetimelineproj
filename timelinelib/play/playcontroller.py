5c8814b579729e837663ee877f9fbb3b5b6b470e
Rickard Lindberg <ricli85@gmail.com>
2012-03-02 18:23:13 +0100

    Experiment more with play.

--- timelinelib/play/playcontroller.py
+++ timelinelib/play/playcontroller.py
@@ -17,6 +17,7 @@
 
 
 import datetime
+import time
 
 from timelinelib.db.objects import TimePeriod
 from timelinelib.time.pytime import PyTimeType
@@ -37,15 +38,37 @@ class PlayController(object):
 
     def start_movie(self):
         period_length = self.play_frame.get_view_period_length()
+
         first_event_time = self.timeline.get_first_event().time_period.start_time
-        self.last_event_time = self.timeline.get_last_event().time_period.end_time + period_length / 2
         start_time = first_event_time - period_length / 2
         end_time = first_event_time + period_length / 2
-        self.current_period = TimePeriod(self.timeline.get_time_type(), start_time, end_time)
-        self.play_frame.redraw_drawing_area(self.draw_fn)
-        self.play_frame.start_timer(10)
+        self.start_period = TimePeriod(self.timeline.get_time_type(), start_time, end_time)
+
+        s1 = start_time
+
+        self.animation_length = 5.5
+
+        self.last_event_time = self.timeline.get_last_event().time_period.end_time
+        start_time = self.last_event_time - period_length / 2
+        end_time = self.last_event_time + period_length / 2
+        self.end_period = TimePeriod(self.timeline.get_time_type(), start_time, end_time)
+
+        s2 = start_time
+
+        self.total_animation_delta = TimePeriod(self.timeline.get_time_type(), s1, s2).delta()
+
+        self.current_period = self.start_period
+
+        self.last_time = time.time()
+        self.total_animation_time = 0
+
+        self.play_frame.start_timer(50)
 
     def tick(self):
+        new_time = time.time()
+        self.delta = new_time - self.last_time
+        self.last_time = new_time
+        print self.delta
         self.play_frame.redraw_drawing_area(self.draw_fn)
 
     def draw_fn(self, dc):
@@ -55,8 +78,14 @@ class PlayController(object):
             dc, self.timeline, view_properties, self.config)
 
     def get_period(self):
-        if self.current_period.end_time > self.last_event_time:
+        if self.current_period.end_time >= self.end_period.end_time:
             self.play_frame.stop_timer()
-        delta_to_move = self.current_period.delta() / 100
-        self.current_period = self.current_period.move_delta(delta_to_move)
+
+        self.total_animation_time += self.delta
+
+        delta_to_move = self.timeline.get_time_type().mult_timedelta(
+            self.total_animation_delta,
+            min(1, self.total_animation_time/self.animation_length))
+
+        self.current_period = self.start_period.move_delta(delta_to_move)
         return self.current_period
