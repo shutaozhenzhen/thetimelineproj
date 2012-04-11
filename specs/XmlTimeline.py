ab21a42d6899722c63cad57e322b38f55a0edd9a
Roger Lindberg <roger@rolidata.se>
2012-04-11 19:40:49 +0200

    Updated tests for encodind/decoding alert data

--- specs/XmlTimeline.py
+++ specs/XmlTimeline.py
@@ -23,8 +23,10 @@ import os.path
 import shutil
 from datetime import datetime
 import unittest
+import wx
 
 from timelinelib.db.backends.xmlfile import XmlTimeline
+from timelinelib.db.backends.xmlfile import alert_string
 from timelinelib.db import db_open
 from timelinelib.db.objects import Category
 from timelinelib.db.objects import Event
@@ -42,6 +44,22 @@ class XmlTimelineSpec(unittest.TestCase):
         timeline = XmlTimeline(None, load=False, use_wide_date_range=True)
         self.assertTrue(isinstance(timeline.time_type, WxTimeType))
 
+    def testAlertStringParsingGivesAlertData(self):
+        timeline = XmlTimeline(None, load=False, use_wide_date_range=True)
+        time, text = timeline._parse_alert_string("2012-11-11 00:00:00;Now is the time")
+        self.assertEqual("Now is the time", text)
+        self.assertEqual("2012-11-11 00:00:00", "%s" % time)
+
+    def testAlertDataConversionGivesAlertString(self):
+        alert = (datetime(2010, 8, 31, 0, 0, 0), "Hoho")
+        alert_text = alert_string(alert)
+        self.assertEqual("2010-08-31 00:00:00;Hoho", alert_text)
+        
+    def testWxTimeAlertDataConversionGivesAlertString(self):
+        alert = (wx.DateTimeFromDMY(30, 8, 2010, 0, 0, 0), "Hoho")
+        alert_text = alert_string(alert)
+        self.assertEqual("2010-09-30 00:00:00;Hoho", alert_text)
+        
     def testDisplayedPeriodTagNotWrittenIfNotSet(self):
         # Create a new db and add one event
         db = db_open(self.tmp_path)
@@ -93,7 +111,7 @@ class XmlTimelineSpec(unittest.TestCase):
         ev1 = Event(db.get_time_type(), datetime(2010, 3, 3), datetime(2010, 3, 6),
                     "Event 1", cat1)
         ev1.set_data("description", u"The <b>first</b> event åäö.")
-        ev1.set_data("alert", u"2012-12-31 12.00.00;Time to go")
+        ev1.set_data("alert", (datetime(2012, 12, 31), "Time to go"))
         db.save_event(ev1)
         # Create view properties
         vp = ViewProperties()
@@ -113,7 +131,7 @@ class XmlTimelineSpec(unittest.TestCase):
         self.assertEquals(event.time_period.end_time, datetime(2010, 3, 6))
         self.assertEquals(event.category.name, "Category 1")
         self.assertEquals(event.get_data("description"), u"The <b>first</b> event åäö.")
-        self.assertEquals(event.get_data("alert"), u"2012-12-31 12.00.00;Time to go")
+        self.assertEquals(event.get_data("alert"), (datetime(2012, 12, 31), "Time to go"))
         self.assertEquals(event.get_data("icon"), None)
         # Assert that correct view properties are loaded (category visibility
         # checked later)
