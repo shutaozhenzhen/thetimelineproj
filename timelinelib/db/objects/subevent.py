023bfa21dc35938c3832eb4905de415235d99f12
Rickard Lindberg <ricli85@gmail.com>
2012-06-30 17:42:57 +0200

    Fix import.
    Import inside a package should not use package "shortcuts".

--- timelinelib/db/objects/subevent.py
+++ timelinelib/db/objects/subevent.py
@@ -16,7 +16,7 @@
 # along with Timeline.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from timelinelib.db.objects import Event
+from timelinelib.db.objects.event import Event
 
 
 class Subevent(Event):
