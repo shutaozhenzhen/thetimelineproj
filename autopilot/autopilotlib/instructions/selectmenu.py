6439ecc79bfe918f2df1f20bc8dace31bf91f357
Roger Lindberg <roger@rolidata.se>
2013-03-06 20:35:47 +0100

    ap: refactoring of SelectMenu Instruction

--- autopilot/autopilotlib/instructions/selectmenu.py
+++ autopilot/autopilotlib/instructions/selectmenu.py
@@ -20,8 +20,6 @@ import wx
 
 from autopilotlib.instructions.instruction import Instruction
 from autopilotlib.app.logger import Logger
-from autopilotlib.app.constants import TIME_TO_WAIT_FOR_DIALOG_TO_SHOW_IN_MILLISECONDS
-import autopilotlib.manuscript.scanner as scanner
 
 
 class MenuNotFoundException():
@@ -43,38 +41,18 @@ class SelectMenuInstruction(Instruction):
         At least 2 targets must be present.
     """    
         
-    def __init__(self, tokens):
-        Instruction.__init__(self, tokens)
-        
     def execute(self, manuscript, win=None):
+        Instruction.execute(self, manuscript, win)
         try:
-            item_id = self._find_menu_item_id()
-            self._continue_instruction_loop(manuscript)
+            item_id = self._find_menu_item_id(win)
+            win.click_menu_item(item_id)   
             Logger.add_result("Menu selected")
-            self._click_menu_item(manuscript, item_id)    
         except MenuNotFoundException:
             Logger.add_error("Menu not found")
-            manuscript.execute_next_instruction()
             
-    def _labels(self):
-        labels = []
-        for token in self.tokens:
-            if token.id == scanner.ID:
-                labels.append(token.lexeme)
-            elif token.id == scanner.STRING:
-                labels.append(token.lexeme[1:-1])
-        return labels
-    
-    def _continue_instruction_loop(self, manuscript):
-        wx.CallLater(TIME_TO_WAIT_FOR_DIALOG_TO_SHOW_IN_MILLISECONDS,
-                     manuscript.execute_next_instruction)
-        
-    def _click_menu_item(self, manuscript, item_id):
-        wx.GetActiveWindow().ProcessCommand(item_id)
-
-    def _find_menu_item_id(self):
-        labels = self._labels()
-        menu_bar = self._get_menu_bar()
+    def _find_menu_item_id(self, win):
+        labels = self.get_all_args()
+        menu_bar = self._get_menu_bar(win)
         item_id = self.get_item_id(menu_bar, labels[0], labels[1])
         labels = labels [2:]
         while len(labels) > 0:
@@ -118,8 +96,8 @@ class SelectMenuInstruction(Instruction):
             raise MenuNotFoundException()
         return item_id
         
-    def _get_menu_bar(self):
-        menu_bar = wx.GetActiveWindow().GetMenuBar()
+    def _get_menu_bar(self, win):
+        menu_bar = win.GetMenuBar()
         if menu_bar is None:
             raise MenuNotFoundException()
         return menu_bar
