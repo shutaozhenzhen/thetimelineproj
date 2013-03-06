ded1cd7db74cf0a00f7afb6067da2d0e1c5244e8
Roger Lindberg <roger@rolidata.se>
2013-03-06 19:57:14 +0100

    ap: Remake of CloseFrame Instruction

--- autopilot/autopilotlib/instructions/closeframe.py
+++ autopilot/autopilotlib/instructions/closeframe.py
@@ -24,28 +24,62 @@ from autopilotlib.app.logger import Logger
 
 
 class CloseFrameInstruction(Instruction):
-    
-    def __init__(self, tokens):
-        Instruction.__init__(self, tokens)
+    """
+        0        1          2  3       4
+        command  object  [  (  target  )  ]?
         
+        command ::=  Close
+        object  ::=  Frame
+        target  ::=  STRING | TEXT
+        
+        Closes a frame window. If no target name is given the frame to close
+        is assumed to be the current window.
+        
+        Example 1:   Close Frame
+        Example 2:   Close Frame(Help)
+    """    
+
+    TARGET = 3    
+        
+    def execute(self, manuscript, win=None):
+        Instruction.execute(self, manuscript, win)
+        self._close_frame(win)
+
     def label(self):
         for token in self.tokens:
             if token.id == scanner.ID:
                 return token.lexeme
         return ""
     
-    def execute(self, manuscript, win=None):
-        wx.CallLater(500, manuscript.execute_next_instruction)
-        label = self.label()
-        frame = self.find_frame(label)
-        if frame is not None:
-            Logger.add("   Frame(%s) closed" % label)
-            frame.Destroy()
-        else:
-            Logger.add("ERROR: Frame not found Label='%s'" % label)
-
-    def find_frame(self, label):
+    def _get_name_of_frame(self):
+        return self.arg(CloseFrameInstruction.TARGET)
+    
+    def _close_frame(self, win):
+        frame, frame_name = self._find_frame(win)
+        self._close(frame, frame_name)
+        
+    def _find_frame(self, win):
+        try:
+            frame_name = self._get_name_of_frame()
+            frame = self._find_frame_by_name(frame_name)
+        except:
+            frame_name = win.GetLabel()
+            frame = self._find_frame_from_input(win)
+        return frame, frame_name
+    
+    def _find_frame_by_name(self, frame_name):
         wins = wx.GetTopLevelWindows()
-        for win in wins:
-            if win.ClassName == "wxFrame" and win.GetLabel() == label:
-                return win
+        for frame in wins:
+            if frame.ClassName == "wxFrame" and frame.GetLabel() == frame_name:
+                return frame
+
+    def _find_frame_from_input(self, win):
+        if win.ClassName == "wxFrame":
+            return win
+        
+    def _close(self, frame, frame_name):
+        try:
+            frame.Destroy()
+            Logger.add_result("Frame(%s) closed" % frame_name)
+        except:
+            Logger.add_error("Frame(%s) not found" % frame_name)        
