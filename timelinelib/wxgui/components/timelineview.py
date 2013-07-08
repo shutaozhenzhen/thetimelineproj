02c385007fc1e3277c8cc862ca4c2758eab8fe2b
roger <roger@H20.rolidata.local>
2013-07-08 22:26:30 +0200

    More adjustments for wxpython version 2.9

--- timelinelib/wxgui/components/timelineview.py
+++ timelinelib/wxgui/components/timelineview.py
@@ -196,31 +196,31 @@ class DrawingAreaPanel(wx.Panel):
         self.controller.window_resized()
 
     def _on_left_down(self, evt):
-        self.controller.left_mouse_down(evt.m_x, evt.m_y, evt.m_controlDown,
-                                        evt.m_shiftDown, evt.m_altDown)
+        self.controller.left_mouse_down(evt.GetX(), evt.GetY(), evt.ControlDown(),
+                                        evt.ShiftDown(), evt.AltDown())
         evt.Skip()
 
     def _on_right_down(self, evt):
-        self.controller.right_mouse_down(evt.m_x, evt.m_y, evt.m_altDown)
+        self.controller.right_mouse_down(evt.GetX(), evt.GetY(), evt.AltDown())
 
     def _on_left_dclick(self, evt):
-        self.controller.left_mouse_dclick(evt.m_x, evt.m_y, evt.m_controlDown,
-                                          evt.m_altDown)
+        self.controller.left_mouse_dclick(evt.GetX(), evt.GetY(), evt.ControlDown(),
+                                          evt.AltDown())
 
     def _on_middle_up(self, evt):
-        self.controller.middle_mouse_clicked(evt.m_x)
+        self.controller.middle_mouse_clicked(evt.GetX())
 
     def _on_left_up(self, evt):
         self.controller.left_mouse_up()
 
     def _on_enter(self, evt):
-        self.controller.mouse_enter(evt.m_x, evt.LeftIsDown())
+        self.controller.mouse_enter(evt.GetX(), evt.LeftIsDown())
 
     def _on_motion(self, evt):
-        self.controller.mouse_moved(evt.m_x, evt.m_y, evt.m_altDown)
+        self.controller.mouse_moved(evt.GetX(), evt.GetY(), evt.AltDown())
 
     def _on_mousewheel(self, evt):
-        self.controller.mouse_wheel_moved(evt.m_wheelRotation, evt.ControlDown(), evt.ShiftDown(), evt.GetX())
+        self.controller.mouse_wheel_moved(evt.GetWheelRotation(), evt.ControlDown(), evt.ShiftDown(), evt.GetX())
 
     def _on_key_down(self, evt):
         self.controller.key_down(evt.GetKeyCode(), evt.AltDown())
