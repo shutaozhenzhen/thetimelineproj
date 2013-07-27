743b497e7e7091e1447f1265ff2d719798e618cf
Rickard Lindberg <ricli85@gmail.com>
2013-07-27 13:30:38 +0200

    Script is run inside ifmain.

--- specs/print_julian_days.py
+++ specs/print_julian_days.py
@@ -4,7 +4,8 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 
 import timelinelib.calendar.gregorian as gregorian
 
-for julian_day in range(10000000):
-    (year, month, day) = gregorian.from_julian_day(julian_day)
-    roundtrip_julian_day = gregorian.to_julian_day(year, month, day)
-    print("%d - %d-%d-%d - %d" % (julian_day, year, month, day, roundtrip_julian_day))
+if __name__ == "__main__":
+    for julian_day in range(10000000):
+        (year, month, day) = gregorian.from_julian_day(julian_day)
+        roundtrip_julian_day = gregorian.to_julian_day(year, month, day)
+        print("%d - %d-%d-%d - %d" % (julian_day, year, month, day, roundtrip_julian_day))
