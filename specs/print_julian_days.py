0b6a8b18062aeb488c841779861ce34b882e5559
Rickard Lindberg <ricli85@gmail.com>
2013-07-27 12:55:51 +0200

    Test julian day conversions against different implementation in C.

---
+++ specs/print_julian_days.py
@@ -0,0 +1,10 @@
+import sys
+import os.path
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+
+import timelinelib.calendar.gregorian as gregorian
+
+for julian_day in range(10000000):
+    (year, month, day) = gregorian.from_julian_day(julian_day)
+    roundtrip_julian_day = gregorian.to_julian_day(year, month, day)
+    print("%d - %d-%d-%d - %d" % (julian_day, year, month, day, roundtrip_julian_day))
