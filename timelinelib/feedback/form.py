c1e1ad88f8e00b5e090717df22cfafdbdbd68d14
Rickard Lindberg <ricli85@gmail.com>
2013-07-30 16:25:12 +0200

    Feedback form and dialog.

---
+++ timelinelib/feedback/form.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2009, 2010, 2011  Rickard Lindberg, Roger Lindberg
+#
+# This file is part of Timeline.
+#
+# Timeline is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Timeline is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Timeline.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import urllib
+
+
+class FeedbackForm(object):
+
+    def __init__(self, dialog, webbrowser):
+        self.dialog = dialog
+        self.webbrowser = webbrowser
+
+    def populate(self, info, subject, body):
+        self.dialog.set_info_text(info)
+        self.dialog.set_subject_text(subject)
+        self.dialog.set_body_text(body)
+
+    def send_with_default(self):
+        self.webbrowser.open("mailto:%s?subject=%s&body=%s" % (
+            urllib.quote(self.dialog.get_to_text()),
+            urllib.quote(self.dialog.get_subject_text()),
+            urllib.quote(self.dialog.get_body_text())))
+
+    def send_with_gmail(self):
+        self.webbrowser.open("https://mail.google.com/mail/?compose=1&view=cm&fs=1&to=%s&su=%s&body=%s" % (
+            urllib.quote(self.dialog.get_to_text()),
+            urllib.quote(self.dialog.get_subject_text()),
+            urllib.quote(self.dialog.get_body_text())))
