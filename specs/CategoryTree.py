344017dc84afe4ecd535a15d2838fab5bba8896c
Rickard Lindberg <ricli85@gmail.com>
2013-10-01 22:31:45 +0200

    Push width and height down to model.

--- specs/CategoryTree.py
+++ specs/CategoryTree.py
@@ -27,107 +27,122 @@ from timelinelib.wxgui.components.categorytree import CustomCategoryTreeModel
 
 class CategoryTreeModelSpec(unittest.TestCase):
 
-    def test_has_no_entries_when_no_categories(self):
-        self.model.set_timeline_view(self.timeline_view)
-        self.assert_model_has_entries([])
+    def test_has_no_items_when_no_timeline_view_set(self):
+        self.assert_model_has_itmes_matching([])
 
-    def test_copies_event_properties_when_event_visible(self):
-        self.add_category("Work", (255, 0, 100), True)
+    def test_has_no_items_when_no_categories_available(self):
         self.model.set_timeline_view(self.timeline_view)
-        self.assert_model_has_entries([
+        self.assert_model_has_itmes_matching([])
+
+    def test_has_items_for_categories(self):
+        play_category = self.add_category("Play", (255, 0, 100), False)
+        work_category = self.add_category("Work", (88, 55, 22), True)
+        self.model.set_timeline_view(self.timeline_view)
+        self.assert_model_has_itmes_matching([
             {
-                "id": 1,
+                "id": play_category.id,
+                "name": "Play",
+                "visible": False,
+                "color": (255, 0, 100),
+            },
+            {
+                "id": work_category.id,
                 "name": "Work",
                 "visible": True,
-                "color": (255, 0, 100),
-                "x": 0,
-                "y": 0,
-                "expanded": True,
+                "color": (88, 55, 22),
             },
         ])
 
-    def test_copies_event_properties_when_event_hidden(self):
-        self.add_category("Work", (255, 0, 100), False)
+    def test_includes_bounding_box_information_for_items(self):
+        self.model.ITEM_HEIGHT_PX = 20
+        self.add_category("Play")
+        self.add_category("Work")
         self.model.set_timeline_view(self.timeline_view)
-        self.assert_model_has_entries([
+        self.model.set_view_size(200, 900)
+        self.assert_model_has_itmes_matching([
             {
-                "id": 1,
-                "name": "Work",
-                "visible": False,
-                "color": (255, 0, 100),
+                "name": "Play",
                 "x": 0,
                 "y": 0,
-                "expanded": True,
+                "width": 200,
             },
-        ])
-
-    def test_flattens_category_hierarchy_with_indent_level(self):
-        self.model.HEIGHT = 20
-        self.model.INDENT_PX = 10
-        work_category = self.add_category("Work", (255, 0, 100), False)
-        self.add_category("Reading", (0, 255, 0), False, work_category)
-        self.model.set_timeline_view(self.timeline_view)
-        self.assert_model_has_entries([
             {
-                "id": 1,
                 "name": "Work",
-                "visible": False,
-                "color": (255, 0, 100),
                 "x": 0,
-                "y": 0,
-                "expanded": True,
-            },
-            {
-                "id": 2,
-                "name": "Reading",
-                "visible": False,
-                "color": (0, 255, 0),
-                "x": 10,
                 "y": 20,
-                "expanded": True,
+                "width": 200,
+            },
+        ])
+
+    def test_has_indented_bounding_box_for_child_categories(self):
+        self.model.INDENT_PX = 10
+        work_category = self.add_category("Work")
+        self.add_category("Reading", parent=work_category)
+        self.model.set_view_size(200, 900)
+        self.model.set_timeline_view(self.timeline_view)
+        self.assert_model_has_itmes_matching([
+            {
+                "name": "Work",
+                "x": 0,
+                "width": 200,
+            },
+            {
+                "name": "Reading",
+                "x": 10,
+                "width": 190,
             },
         ])
 
     def test_sorts_categories_at_same_level(self):
-        self.add_category("Work", (255, 0, 100), False)
-        self.add_category("Reading", (0, 255, 0), False)
+        self.add_category("Work")
+        self.add_category("Reading")
         self.model.set_timeline_view(self.timeline_view)
-        self.assert_model_has_names(["Reading", "Work"])
+        self.assert_model_has_item_names(["Reading", "Work"])
 
-    def test_can_set_view_multiple_times_without_entries_duplicating(self):
-        self.add_category("Work", (255, 0, 100), False)
+    def test_can_set_view_multiple_times_without_items_duplicating(self):
+        self.add_category("Work")
         self.model.set_timeline_view(self.timeline_view)
         self.model.set_timeline_view(self.timeline_view)
-        self.assertEqual(len(self.model.entries), 1)
+        self.assertEqual(len(self.model.get_items()), 1)
 
     def test_toggles_expandedness(self):
-        self.model.HEIGHT = 20
-        self.add_category("Work", (255, 0, 100), False)
-        self.add_category("Reading", (0, 255, 0), False)
+        self.model.ITEM_HEIGHT_PX = 20
+        self.add_category("Reading")
+        self.add_category("Work")
         self.model.set_timeline_view(self.timeline_view)
-        self.assertTrue(self.model.entries[1]["expanded"])
+        self.assert_model_has_itmes_matching([
+            { "name": "Reading", "expanded": True, },
+            { "name": "Work",    "expanded": True, },
+        ])
         self.model.toggle_expandedness(25)
-        self.assertFalse(self.model.entries[1]["expanded"])
+        self.assert_model_has_itmes_matching([
+            { "name": "Reading", "expanded": True, },
+            { "name": "Work",    "expanded": False, },
+        ])
         self.model.toggle_expandedness(25)
+        self.assert_model_has_itmes_matching([
+            { "name": "Reading", "expanded": True, },
+            { "name": "Work",    "expanded": True, },
+        ])
 
     def test_does_not_toggle_expandedness_if_doing_it_out_of_range(self):
-        self.model.HEIGHT = 20
-        self.add_category("Work", (255, 0, 100), False)
-        self.add_category("Reading", (0, 255, 0), False)
+        self.model.ITEM_HEIGHT_PX = 20
+        self.add_category("Work")
+        self.add_category("Reading")
         self.model.set_timeline_view(self.timeline_view)
-        before = [x["expanded"] for x in self.model.entries]
+        before = [x["expanded"] for x in self.model.get_items()]
         self.model.toggle_expandedness(50)
-        after = [x["expanded"] for x in self.model.entries]
+        after = [x["expanded"] for x in self.model.get_items()]
         self.assertEqual(before, after)
 
     def test_hides_subtrees_if_parent_not_expanded(self):
-        self.model.HEIGHT = 20
+        self.model.ITEM_HEIGHT_PX = 20
         work_category = self.add_category("Work", (255, 0, 100), False)
         self.add_category("Reading", (0, 255, 0), False, work_category)
         self.model.set_timeline_view(self.timeline_view)
-        self.assert_model_has_names(["Work", "Reading"])
+        self.assert_model_has_item_names(["Work", "Reading"])
         self.model.toggle_expandedness(5)
-        self.assert_model_has_names(["Work"])
+        self.assert_model_has_item_names(["Work"])
 
     def setUp(self):
         self.id_counter = IdCounter()
@@ -151,7 +166,7 @@ class CategoryTreeModelSpec(unittest.TestCase):
 
         self.model = CustomCategoryTreeModel()
 
-    def add_category(self, name, color, visible, parent=None):
+    def add_category(self, name, color=(0, 0, 0), visible=True, parent=None):
         category = Category(name, color, (0, 0, 0), True, parent=parent)
         category.set_id(self.id_counter.get_next())
         if visible:
@@ -159,8 +174,12 @@ class CategoryTreeModelSpec(unittest.TestCase):
         self.categories.append(category)
         return category
 
-    def assert_model_has_entries(self, expected_entries):
-        self.assertEqual(self.model.entries, expected_entries)
+    def assert_model_has_itmes_matching(self, expected_entries):
+        self.assertEqual(len(self.model.get_items()), len(expected_entries))
+        for i in range(len(self.model.get_items())):
+            for key in expected_entries[i]:
+                self.assertEqual(self.model.get_items()[i][key],
+                                 expected_entries[i][key])
 
-    def assert_model_has_names(self, expected_names):
-        self.assertEqual([x["name"] for x in self.model.entries], expected_names)
+    def assert_model_has_item_names(self, expected_names):
+        self.assertEqual([x["name"] for x in self.model.get_items()], expected_names)
