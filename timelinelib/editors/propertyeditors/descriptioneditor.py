734c828ca37931a506973a6bf867f1623ced0796
Roger Lindberg <roger@rolidata.se>
2014-02-02 09:23:24 +0100

    Refactored DescriptionEditor

--- timelinelib/editors/propertyeditors/descriptioneditor.py
+++ timelinelib/editors/propertyeditors/descriptioneditor.py
@@ -21,15 +21,40 @@ import wx
 from timelinelib.editors.propertyeditors.baseeditor import BaseEditor
 
 
-class DescriptionEditor(BaseEditor):
+class DescriptionEditorGuiCreator(object):
+
+    def create_sizer(self):
+        return wx.BoxSizer()
+        
+    def create_controls(self):
+        text = self._create_text_control()
+        return (text,)
+    
+    def put_controls_in_sizer(self, sizer, controls):
+        text, = controls 
+        sizer.Add(text, 1, wx.ALL|wx.EXPAND, 0)
+        
+    def _create_text_control(self):
+        self.data = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.Bind(wx.EVT_CHAR, self._on_char)
+        return self.data
+        
+    def _on_char(self, evt):
+        if self._ctrl_a(evt):
+            self.SelectAll()
+        else: 
+            evt.Skip()
+            
+    def _ctrl_a(self, evt):
+        KEY_CODE_A = 1
+        return evt.ControlDown() and evt.KeyCode == KEY_CODE_A
+
+                    
+class DescriptionEditor(BaseEditor, DescriptionEditorGuiCreator):
 
     def __init__(self, parent, editor):
         BaseEditor.__init__(self, parent, editor)
-        self.data = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-        sizer = wx.BoxSizer()
-        sizer.Add(self.data, 1, wx.ALL|wx.EXPAND, 0)
-        self.Bind(wx.EVT_CHAR, self._on_char)
-        self.SetSizerAndFit(sizer)
+        self.create_gui()
 
     def get_data(self):
         description = self.data.GetValue().strip()
@@ -39,13 +64,3 @@ class DescriptionEditor(BaseEditor):
 
     def clear_data(self):
         self.SetValue("")
-
-    def _on_char(self, evt):
-        if self._ctrl_a(evt):
-            self.SelectAll()
-        else: 
-            evt.Skip()
-        
-    def _ctrl_a(self, evt):
-        KEY_CODE_A = 1
-        return evt.ControlDown() and evt.KeyCode == KEY_CODE_A
