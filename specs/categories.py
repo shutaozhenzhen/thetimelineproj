7b004e0f5dc6c2a76ff7c711660bd8c473f1ff6e
Roger Lindberg <roger@rolidata.se>
2014-07-26 15:00:43 +0200

    Refactored tests for Category

--- specs/categories.py
+++ specs/categories.py
@@ -26,7 +26,24 @@ from timelinelib.drawing.viewproperties import ViewProperties
 from timelinelib.repositories.categories import CategoriesFacade
 
 
-class queries(unittest.TestCase):
+class TestBase(unittest.TestCase):
+
+    def _create_example_tree(self):
+        self.work = self.add_category("Work")
+        self.report = self.add_category("Report", parent=self.work)
+        self.monthly_report = self.add_category("Monthly report", parent=self.report)
+        self.yearly_report = self.add_category("Yearly report", parent=self.report)
+        self.play = self.add_category("Play")
+        self.football = self.add_category("Football", parent=self.play)
+
+    def add_category(self, name, parent=None):
+        category = Category(name, color=(0, 0, 0), font_color=(0, 0, ),
+                            visible=True, parent=parent)
+        self.category_list.append(category)
+        return category
+
+
+class queries(TestBase):
 
     def test_get_all(self):
         self.assertEqual(self.categories.get_all(),
@@ -49,15 +66,6 @@ class queries(unittest.TestCase):
         self.assertEqual(set(self.categories.get_parents_for_checked_childs()),
                          set([self.work, self.report]))
 
-    def test_cloning_returns_new_object(self):
-        cloned_category = self.category_list[0].clone()
-        self.assertTrue(self.category_list[0] != cloned_category)
-        self.assertEqual(cloned_category.name, self.category_list[0].name)
-        self.assertEqual(cloned_category.color, self.category_list[0].color)
-        self.assertEqual(cloned_category.font_color, self.category_list[0].font_color)
-        self.assertEqual(cloned_category.visible, self.category_list[0].visible)
-        self.assertEqual(cloned_category.parent, self.category_list[0].parent)
-
     def setUp(self):
         self.category_list = []
         self.view_properties = Mock(ViewProperties)
@@ -69,17 +77,20 @@ class queries(unittest.TestCase):
 
     def _is_category_visible(self, category):
         return category in self.checked_categories
-        
-    def _create_example_tree(self):
-        self.work = self.add_category("Work")
-        self.report = self.add_category("Report", parent=self.work)
-        self.monthly_report = self.add_category("Monthly report", parent=self.report)
-        self.yearly_report = self.add_category("Yearly report", parent=self.report)
-        self.play = self.add_category("Play")
-        self.football = self.add_category("Football", parent=self.play)
 
-    def add_category(self, name, parent=None):
-        category = Category(name, color=(0, 0, 0), font_color=(0, 0, ),
-                            visible=True, parent=parent)
-        self.category_list.append(category)
-        return category
+    
+class cloning(TestBase):
+    
+    def test_cloning_returns_new_object(self):
+        cloned_category = self.category_list[0].clone()
+        self.assertTrue(self.category_list[0] != cloned_category)
+        self.assertEqual(cloned_category.name, self.category_list[0].name)
+        self.assertEqual(cloned_category.color, self.category_list[0].color)
+        self.assertEqual(cloned_category.font_color, self.category_list[0].font_color)
+        self.assertEqual(cloned_category.visible, self.category_list[0].visible)
+        self.assertEqual(cloned_category.parent, self.category_list[0].parent)
+
+    def setUp(self):
+        self.category_list = []
+        self._create_example_tree()
+    
