3aab0db55082407d2eaff00f5e132af677a3f15f
Rickard Lindberg <ricli85@gmail.com>
2014-08-04 08:30:57 +0200

    Rename export -> export_db_to_timeline_xml.

--- specs/XmlTimeline.py
+++ specs/XmlTimeline.py
@@ -22,7 +22,7 @@ import os.path
 
 from specs.utils import TmpDirTestCase
 from timelinelib.db.backends.xmlfile import XmlTimeline
-from timelinelib.db.exporters.timelinexml import export
+from timelinelib.db.exporters.timelinexml import export_db_to_timeline_xml
 from timelinelib.db import db_open
 from timelinelib.db.objects import Category
 from timelinelib.db.objects import Event
@@ -143,7 +143,7 @@ class XmlTimelineSpec(TmpDirTestCase):
         vp.displayed_period = TimePeriod(db.get_time_type(), start, end)
         vp.set_category_visible(cat3, False)
         db.save_view_properties(vp)
-        export(db, self.tmp_path)
+        export_db_to_timeline_xml(db, self.tmp_path)
 
     def _assert_re_read_db_same(self, db):
         # Assert event correctly loaded
