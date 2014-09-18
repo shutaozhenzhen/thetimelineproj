db0f35c5330c396b05c407fc9f92fcc65a550b92
Roger Lindberg <roger@rolidata.se>
2014-09-18 07:08:57 +0200

    Checkpoint handling removed from db

--- timelinelib/data/db.py
+++ timelinelib/data/db.py
@@ -184,7 +184,6 @@ class MemoryDB(Observable):
 
     def loaded(self):
         self._undo_handler.enable(True)
-        self._undo_handler.set_checkpoint()
         self._undo_handler.save()
         
     def _append_category(self, category):
@@ -274,14 +273,6 @@ class MemoryDB(Observable):
             self._save_if_not_disabled()
             self._notify(STATE_CHANGE_ANY)
             self._undo_handler.enable(True)
-
-    def set_checkpoint(self):
-        self._undo_handler.set_checkpoint()
-
-    def revert_to_checkpoint(self):
-        self._events = self._undo_handler.revert_to_checkpoint()
-        self._save_if_not_disabled()
-        self._notify(STATE_CHANGE_ANY)
 
     def _ensure_no_circular_parent(self, cat):
         parent = cat.parent
