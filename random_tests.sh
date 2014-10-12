759018ea786a117bdb43ac00d0724af1dbed9905
Rickard Lindberg <ricli85@gmail.com>
2014-10-12 17:32:55 +0200

    Add undo test to random test.

--- random_tests.sh
+++ random_tests.sh
@@ -3,5 +3,5 @@
 set -e
 
 while true; do
-    ./execute-specs.py --only test_can_be_compared test_can_be_cloned
+    ./execute-specs.py --only test_can_be_compared test_can_be_cloned specs.Undo.
 done
