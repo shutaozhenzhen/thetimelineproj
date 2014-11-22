21e989ceb856fe4792e24d469b8924497e4f3393
Rickard Lindberg <ricli85@gmail.com>
2014-11-22 11:24:14 +0100

    Remove version numbers from doc.
    The timeline doc is more a website and not tied to a particular version.

--- doc/conf.py
+++ doc/conf.py
@@ -53,13 +53,10 @@ copyright = u'2013, Rickard Lindberg'
 # built documents.
 #
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
-from timelinelib.meta.version import get_version
-
 # The short X.Y version.
-version = get_version()
+version = ""
 # The full version, including alpha/beta/rc tags.
-release = get_version()
+release = ""
 
 def get_latest_version():
     latest_version = "null"
