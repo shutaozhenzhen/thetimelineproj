a453c1e37521b1b29dc5b3bb63ef1ad3517f43c2
Roger Lindberg <roger@rolidata.se>
2015-01-18 13:47:43 +0100

    Pep8 conformance

--- autopilot/autopilotlib/instructions/factory.py
+++ autopilot/autopilotlib/instructions/factory.py
@@ -34,74 +34,74 @@ from autopilotlib.instructions.selectcombobox import SelectComboboxInstruction
 class InstructionSyntaxException():
     pass
 
- 
+
 def create_instruction(text):
     tokens = scanner.scan_instruction(text)
-    
+
     if len(tokens) == 0:
-        raise InstructionSyntaxException()    
-    
+        raise InstructionSyntaxException()
+
     if tokens[0].id != scanner.KEYWORD:
-        raise InstructionSyntaxException()    
-    
+        raise InstructionSyntaxException()
+
     if tokens[0].subid == scanner.ID_INCLUDE:
         syntaxcheck_include_instruction(tokens)
         return IncludeInstruction(tokens)
-    
+
     if tokens[0].subid == scanner.ID_EXIT:
         syntaxcheck_exit_instruction(tokens)
-        return ExitInstruction(tokens) 
+        return ExitInstruction(tokens)
 
     if tokens[0].subid == scanner.ID_COMMENT:
-        return CommentInstruction(tokens) 
+        return CommentInstruction(tokens)
 
     if tokens[0].subid == scanner.ID_SELECT:
         if tokens[1].lexeme.lower() == "menu":
             syntaxcheck_select_menu_instruction(tokens)
-            return SelectMenuInstruction(tokens) 
+            return SelectMenuInstruction(tokens)
         elif tokens[1].lexeme.lower() == "customtreecontrol":
             syntaxcheck_select_customtreecontrol_instruction(tokens)
-            return SelectCustomTreeControlInstruction(tokens) 
+            return SelectCustomTreeControlInstruction(tokens)
         elif tokens[1].lexeme.lower() == "combobox":
             syntaxcheck_select_combobox_instruction(tokens)
-            return SelectComboboxInstruction(tokens) 
+            return SelectComboboxInstruction(tokens)
         else:
             raise InstructionSyntaxException()
 
     if tokens[0].subid == scanner.ID_ENTER:
         if tokens[1].lexeme.lower() == "text":
             syntaxcheck_enter_text_instruction(tokens)
-            return EnterTextInstruction(tokens) 
+            return EnterTextInstruction(tokens)
         else:
             raise InstructionSyntaxException()
-        
+
     if tokens[0].subid == scanner.ID_CLOSE:
         if tokens[1].lexeme.lower() == "frame":
             syntaxcheck_close_frame_instruction(tokens)
-            return CloseFrameInstruction(tokens) 
+            return CloseFrameInstruction(tokens)
         elif tokens[1].lexeme.lower() == "dialog":
             syntaxcheck_close_dialog_instruction(tokens)
-            return CloseDialogInstruction(tokens) 
+            return CloseDialogInstruction(tokens)
         elif tokens[1].lexeme.lower() == "application":
             syntaxcheck_close_application_instruction(tokens)
-            return ExitInstruction(tokens) 
+            return ExitInstruction(tokens)
         else:
             raise InstructionSyntaxException()
-        
+
     if tokens[0].subid == scanner.ID_HIDE:
         if tokens[1].lexeme.lower() == "frame":
             syntaxcheck_hide_frame_instruction(tokens)
-            return HideFrameInstruction(tokens) 
+            return HideFrameInstruction(tokens)
         else:
             raise InstructionSyntaxException()
-        
-    if tokens[0].subid == scanner.ID_CLICK: 
+
+    if tokens[0].subid == scanner.ID_CLICK:
         if tokens[1].lexeme.lower() == "button":
             syntaxcheck_click_button_instruction(tokens)
-            return ClickButtonInstruction(tokens) 
+            return ClickButtonInstruction(tokens)
         if tokens[1].lexeme.lower() == "mouse":
             syntaxcheck_click_mouse_instruction(tokens)
-            return ClickMouseInstruction(tokens) 
+            return ClickMouseInstruction(tokens)
         else:
             raise InstructionSyntaxException()
 
@@ -120,6 +120,7 @@ def syntaxcheck_include_instruction(tokens):
     )
     validate(tokens, STATES)
 
+
 def syntaxcheck_exit_instruction(tokens):
     """
     State   token-id           token-subid        Next State
@@ -130,6 +131,7 @@ def syntaxcheck_exit_instruction(tokens):
       (1,   scanner.KEYWORD,   scanner.ID_APP,    -1),
     )
     validate(tokens, STATES)
+
 
 def syntaxcheck_select_menu_instruction(tokens):
     """
@@ -150,6 +152,7 @@ def syntaxcheck_select_menu_instruction(tokens):
     )
     validate(tokens, STATES)
 
+
 def syntaxcheck_select_customtreecontrol_instruction(tokens):
     """
     State   token-id           token-subid        Next State
@@ -166,6 +169,7 @@ def syntaxcheck_select_customtreecontrol_instruction(tokens):
       (6,   scanner.RP,        None,                     -1),
     )
     validate(tokens, STATES)
+
 
 def syntaxcheck_select_combobox_instruction(tokens):
     """
@@ -184,6 +188,7 @@ def syntaxcheck_select_combobox_instruction(tokens):
     )
     validate(tokens, STATES)
 
+
 def syntaxcheck_enter_text_instruction(tokens):
     """
     State   token-id           token-subid        Next State
@@ -201,6 +206,7 @@ def syntaxcheck_enter_text_instruction(tokens):
     )
     validate(tokens, STATES)
 
+
 def syntaxcheck_close_frame_instruction(tokens):
     """
     State   token-id           token-subid        Next State
@@ -215,7 +221,8 @@ def syntaxcheck_close_frame_instruction(tokens):
       (4,   scanner.RP,        None,              -1),
     )
     validate(tokens, STATES)
-    
+
+
 def syntaxcheck_close_dialog_instruction(tokens):
     """
     State   token-id           token-subid        Next State
@@ -231,6 +238,7 @@ def syntaxcheck_close_dialog_instruction(tokens):
     )
     validate(tokens, STATES)
 
+
 def syntaxcheck_close_application_instruction(tokens):
     """
     State   token-id           token-subid        Next State
@@ -241,6 +249,7 @@ def syntaxcheck_close_application_instruction(tokens):
       (1,   scanner.KEYWORD,   scanner.ID_APP,    -1),
     )
     validate(tokens, STATES)
+
 
 def syntaxcheck_hide_frame_instruction(tokens):
     """
@@ -256,7 +265,8 @@ def syntaxcheck_hide_frame_instruction(tokens):
       (4,   scanner.RP,        None,              -1),
     )
     validate(tokens, STATES)
-   
+
+
 def syntaxcheck_click_button_instruction(tokens):
     """
     State   token-id           token-subid        Next State
@@ -272,7 +282,8 @@ def syntaxcheck_click_button_instruction(tokens):
       (4,   scanner.RP,        None,              -1),
     )
     validate(tokens, STATES)
-    
+
+
 def syntaxcheck_click_mouse_instruction(tokens):
     """
     State   token-id           token-subid        Next State
@@ -288,18 +299,20 @@ def syntaxcheck_click_mouse_instruction(tokens):
       (6,   scanner.RP,        None,                     -1),
     )
     validate(tokens, STATES)
-        
+
+
 def validate(tokens, states):
     state = 0
     i = 0
     while state != -1:
         state = next_state(state, states, tokens[i])
         i += 1
-    
+
+
 def next_state(current_state, states, token):
     transitions = [state for state in states if state[0] == current_state]
     for state, token_id, subid, next_state in transitions:
         if token.id == token_id:
-            if subid == None or subid == token.subid:
+            if subid is None or subid == token.subid:
                 return next_state
-    raise InstructionSyntaxException()    
+    raise InstructionSyntaxException()
