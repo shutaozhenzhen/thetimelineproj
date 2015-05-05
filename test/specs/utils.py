0ba169d71663c61bd2465a5a5be8a47ba3bf81a1
Rickard Lindberg <ricli85@gmail.com>
2015-05-05 23:14:12 +0200

    Problem: UnitTestCase goes by different names.
    This is confusing.
    
    Solution: Use only a single name.

--- test/specs/utils.py
+++ test/specs/utils.py
@@ -32,9 +32,9 @@ from timelinelib.config.arguments import ApplicationArguments
 from timelinelib.config.dotfile import read_config
 from timelinelib.data import Category
 from timelinelib.data import Container
+from timelinelib.data import Era
 from timelinelib.data import Event
 from timelinelib.data import Subevent
-from timelinelib.data import Era
 from timelinelib.data import TimePeriod
 from timelinelib.db import db_open
 from timelinelib.time.gregoriantime import GregorianTimeType
@@ -42,6 +42,7 @@ from timelinelib.time.numtime import NumTimeType
 from timelinelib.time.timeline import delta_from_days
 from timelinelib.time.timeline import TimeDelta
 from timelinelib.wxgui.setup import start_wx_application
+import timelinetest
 
 
 ANY_TIME = "1 Jan 2010"
@@ -173,10 +174,6 @@ def a_numeric_era_with(start=None, end=None, time=ANY_NUM_TIME, name="foo", colo
         start = time
         end = time
     return Era(NumTimeType(), start, end, name, color)
-
-
-def get_random_modifier(modifiers):
-    return random.choice(modifiers)
 
 
 def inc(number):
@@ -313,50 +310,7 @@ TIME_MODIFIERS = [
 ]
 
 
-class TestCase(unittest.TestCase):
-
-    def assertListIsCloneOf(self, cloned_list, original_list):
-        self.assertEqual(cloned_list, original_list)
-        self.assertTrue(cloned_list is not original_list)
-        for i in range(len(cloned_list)):
-            self.assertIsCloneOf(cloned_list[i], original_list[i])
-
-    def assertIsCloneOf(self, clone, original):
-        self.assertEqual(clone, original)
-        self.assertTrue(clone is not original, "%r" % clone)
-
-    def assertInstanceNotIn(self, object_, list_):
-        for element in list_:
-            if element is object_:
-                self.fail("%r was in list" % object_)
-
-    def assertEqNeImplementationIsCorrect(self, create_fn, modifiers):
-        (modification_description, modifier_fn) = get_random_modifier(modifiers)
-        one = modifier_fn(create_fn())
-        other = modifier_fn(create_fn())
-        fail_message_one_other = "%r vs %r (%s)" % (one, other,
-                                                    modification_description)
-        self.assertTrue(type(one) == type(other), fail_message_one_other)
-        self.assertFalse(one is None, fail_message_one_other)
-        self.assertTrue(one is not None, fail_message_one_other)
-        self.assertTrue(one is not other, fail_message_one_other)
-        self.assertFalse(one is other, fail_message_one_other)
-        self.assertTrue(one == other, fail_message_one_other)
-        self.assertFalse(one != other, fail_message_one_other)
-        self.assertTrue(one == one, fail_message_one_other)
-        self.assertFalse(one != one, fail_message_one_other)
-        (modification_description, modifier_fn) = get_random_modifier(modifiers)
-        modified = modifier_fn(other)
-        fail_message_modified_one = "%r vs %r (%s)" % (modified, one,
-                                                       modification_description)
-        self.assertTrue(type(modified) == type(one), fail_message_modified_one)
-        self.assertTrue(modified is not one, fail_message_modified_one)
-        self.assertFalse(modified is one, fail_message_modified_one)
-        self.assertTrue(modified != one, fail_message_modified_one)
-        self.assertFalse(modified == one, fail_message_modified_one)
-
-
-class TmpDirTestCase(TestCase):
+class TmpDirTestCase(timelinetest.UnitTestCase):
 
     def setUp(self):
         self.tmp_dir = tempfile.mkdtemp(prefix="timeline-test")
@@ -368,7 +322,7 @@ class TmpDirTestCase(TestCase):
         return os.path.join(self.tmp_dir, name)
 
 
-class WxComponentTest(TestCase):
+class WxComponentTest(timelinetest.UnitTestCase):
 
     def setUp(self):
         self._app = wx.App(False)
