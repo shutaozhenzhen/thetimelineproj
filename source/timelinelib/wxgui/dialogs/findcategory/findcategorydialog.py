03f53c05b1ffb682bc8d4716b58672c58a853481
Roger Lindberg <roger@rolidata.se>
2015-07-10 09:17:20 +0200

    Pep8 conformance

--- source/timelinelib/wxgui/dialogs/findcategory/findcategorydialog.py
+++ source/timelinelib/wxgui/dialogs/findcategory/findcategorydialog.py
@@ -26,7 +26,7 @@ class FindDialogGuiCreator(wx.Dialog):
 
     def __init__(self, mainframe, timeline):
         wx.Dialog.__init__(self, mainframe, title=_("Category Finder"), name="container_editor",
-                           style=wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER)
+                           style=wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER)
         self.mainframe = mainframe
         self.db = timeline
         self._create_gui()
@@ -43,15 +43,15 @@ class FindDialogGuiCreator(wx.Dialog):
         sizer.AddGrowableCol(0)
         sizer.AddGrowableRow(0)
         return sizer
-        
+
     def _create_controls(self):
         return (self._create_content(), self._create_buttons())
-    
+
     def _layout_controls(self, sizer, controls):
         content, buttons = controls
-        sizer.Add(content, flag=wx.ALL|wx.EXPAND, border=BORDER)
+        sizer.Add(content, flag=wx.ALL | wx.EXPAND, border=BORDER)
         sizer.Add(buttons, flag=wx.ALL, border=BORDER)
-        
+
     def _create_content(self):
         sizer = self._create_content_sizer()
         controls = self._create_content_controls()
@@ -61,27 +61,27 @@ class FindDialogGuiCreator(wx.Dialog):
     def _create_content_controls(self):
         return (self._create_name_textctrl(),
                 self._create_categories_listbox())
-        
+
     def _create_content_sizer(self):
         sizer = wx.FlexGridSizer(2, 1, BORDER, BORDER)
         sizer.AddGrowableCol(0)
         sizer.AddGrowableRow(1)
         return sizer
- 
+
     def _layout_content_controls(self, sizer, controls):
         for control in controls:
             sizer.Add(control, flag=wx.EXPAND)
-           
+
     def _create_name_textctrl(self):
         self.txt_name = wx.TextCtrl(self, wx.ID_ANY, name="name")
         self.Bind(wx.EVT_TEXT, self._on_char, self.txt_name)
         return self.txt_name
-    
+
     def _create_categories_listbox(self):
         self.lst_category = wx.ListBox(self, -1)
         self._fill_categories_listbox()
         return self.lst_category
-    
+
     def _create_buttons(self):
         sizer = wx.BoxSizer(wx.HORIZONTAL)
         btn_check = wx.Button(self, wx.ID_ANY, _("Check"))
@@ -95,7 +95,7 @@ class FindDialogGuiCreator(wx.Dialog):
         sizer.Add(btn_close)
         return sizer
 
-    
+
 class CategoryFindDialog(FindDialogGuiCreator):
 
     def __init__(self, parent, timeline):
@@ -115,10 +115,10 @@ class CategoryFindDialog(FindDialogGuiCreator):
 
     def _btn_uncheck_on_click(self, evt):
         SidebarProxy(self.mainframe).uncheck_categories(self._get_categories())
-        
+
     def _btn_close_on_click(self, evt):
         self.EndModal(wx.OK)
-            
+
     def _get_categories(self):
         names = self.lst_category.GetItems()
         return sorted([category for category in self.db.get_categories()
