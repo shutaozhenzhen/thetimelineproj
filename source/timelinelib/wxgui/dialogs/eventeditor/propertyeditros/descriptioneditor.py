2397cf0a54db7377907c5ed18dae75e6148e323f
Roger Lindberg <roger@rolidata.se>
2015-07-11 19:02:19 +0200

    Pep8 conformance

--- source/timelinelib/wxgui/dialogs/eventeditor/propertyeditros/descriptioneditor.py
+++ source/timelinelib/wxgui/dialogs/eventeditor/propertyeditros/descriptioneditor.py
@@ -22,37 +22,37 @@ from timelinelib.wxgui.dialogs.eventeditor.propertyeditros.baseeditor import Bas
 
 
 class DescriptionEditorGuiCreator(wx.Panel):
-    
+
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
-            
+
     def create_sizer(self):
         return wx.BoxSizer()
-        
+
     def create_controls(self):
         text = self._create_text_control()
         return (text,)
-    
+
     def put_controls_in_sizer(self, sizer, controls):
-        text, = controls 
-        sizer.Add(text, 1, wx.ALL|wx.EXPAND, 0)
-        
+        text, = controls
+        sizer.Add(text, 1, wx.ALL | wx.EXPAND, 0)
+
     def _create_text_control(self):
         self.data = wx.TextCtrl(self, style=wx.TE_MULTILINE)
         self.Bind(wx.EVT_CHAR, self._on_char)
         return self.data
-        
+
     def _on_char(self, evt):
         if self._ctrl_a(evt):
             self.SelectAll()
-        else: 
+        else:
             evt.Skip()
-            
+
     def _ctrl_a(self, evt):
         KEY_CODE_A = 1
         return evt.ControlDown() and evt.KeyCode == KEY_CODE_A
 
-                    
+
 class DescriptionEditor(BaseEditor, DescriptionEditorGuiCreator):
 
     def __init__(self, parent, editor):
