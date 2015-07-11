81359611c4318cb40327384c149108712a943721
Roger Lindberg <roger@rolidata.se>
2015-07-11 08:44:10 +0200

    Pep8 conformance

--- source/timelinelib/dataexport/timelinexml.py
+++ source/timelinelib/dataexport/timelinexml.py
@@ -38,16 +38,16 @@ def export_db_to_timeline_xml(db, path):
 
 def wrap_in_tag(func, name, indent=""):
     def wrapper(*args, **kwargs):
-        file = args[1] # 1st argument is self, 2nd argument is file
-        file.write(indent)
-        file.write("<")
-        file.write(name)
-        file.write(">\n")
+        dbfile = args[1]  # 1st argument is self, 2nd argument is dbfile
+        dbfile.write(indent)
+        dbfile.write("<")
+        dbfile.write(name)
+        dbfile.write(">\n")
         func(*args, **kwargs)
-        file.write(indent)
-        file.write("</")
-        file.write(name)
-        file.write(">\n")
+        dbfile.write(indent)
+        dbfile.write("</")
+        dbfile.write(name)
+        dbfile.write(">\n")
     return wrapper
 
 
@@ -62,93 +62,93 @@ class Exporter(object):
     def _time_string(self, time):
         return self.db.get_time_type().time_string(time)
 
-    def _write_xml_doc(self, file):
-        file.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n")
-        self._write_timeline(file)
+    def _write_xml_doc(self, xmlfile):
+        xmlfile.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n")
+        self._write_timeline(xmlfile)
 
-    def _write_timeline(self, file):
-        write_simple_tag(file, "version", get_version(), INDENT1)
-        write_simple_tag(file, "timetype", self.db.get_time_type().get_name(), INDENT1)
+    def _write_timeline(self, xmlfile):
+        write_simple_tag(xmlfile, "version", get_version(), INDENT1)
+        write_simple_tag(xmlfile, "timetype", self.db.get_time_type().get_name(), INDENT1)
         if len(self.db.get_all_eras()) > 0:
-            self._write_eras(file)
-        self._write_categories(file)
-        self._write_events(file)
-        self._write_view(file)
+            self._write_eras(xmlfile)
+        self._write_categories(xmlfile)
+        self._write_events(xmlfile)
+        self._write_view(xmlfile)
     _write_timeline = wrap_in_tag(_write_timeline, "timeline")
 
-    def _write_categories(self, file):
+    def _write_categories(self, xmlfile):
         def write_with_parent(categories, parent):
             for cat in categories:
                 if cat._get_parent() == parent:
-                    self._write_category(file, cat)
+                    self._write_category(xmlfile, cat)
                     write_with_parent(categories, cat)
         write_with_parent(self.db.get_categories(), None)
     _write_categories = wrap_in_tag(_write_categories, "categories", INDENT1)
 
-    def _write_category(self, file, cat):
-        write_simple_tag(file, "name", cat.get_name(), INDENT3)
-        write_simple_tag(file, "color", color_string(cat.get_color()), INDENT3)
-        write_simple_tag(file, "progress_color", color_string(cat.get_progress_color()), INDENT3)
-        write_simple_tag(file, "done_color", color_string(cat.get_done_color()), INDENT3)
-        write_simple_tag(file, "font_color", color_string(cat.get_font_color()), INDENT3)
+    def _write_category(self, xmlfile, cat):
+        write_simple_tag(xmlfile, "name", cat.get_name(), INDENT3)
+        write_simple_tag(xmlfile, "color", color_string(cat.get_color()), INDENT3)
+        write_simple_tag(xmlfile, "progress_color", color_string(cat.get_progress_color()), INDENT3)
+        write_simple_tag(xmlfile, "done_color", color_string(cat.get_done_color()), INDENT3)
+        write_simple_tag(xmlfile, "font_color", color_string(cat.get_font_color()), INDENT3)
         if cat._get_parent():
-            write_simple_tag(file, "parent", cat._get_parent().get_name(), INDENT3)
+            write_simple_tag(xmlfile, "parent", cat._get_parent().get_name(), INDENT3)
     _write_category = wrap_in_tag(_write_category, "category", INDENT2)
 
-    def _write_events(self, file):
+    def _write_events(self, xmlfile):
         all_events = self.db.get_all_events()
         subevents = [event for event in all_events if event.is_subevent()]
         events = [event for event in all_events if not event.is_subevent()]
         events.extend(subevents)
         for evt in events:
-            self._write_event(file, evt)
+            self._write_event(xmlfile, evt)
     _write_events = wrap_in_tag(_write_events, "events", INDENT1)
 
-    def _write_event(self, file, evt):
-        write_simple_tag(file, "start",
+    def _write_event(self, xmlfile, evt):
+        write_simple_tag(xmlfile, "start",
                          self._time_string(evt.get_time_period().start_time), INDENT3)
-        write_simple_tag(file, "end",
+        write_simple_tag(xmlfile, "end",
                          self._time_string(evt.get_time_period().end_time), INDENT3)
         if evt.is_container():
-            write_simple_tag(file, "text", "[%d]%s" % (evt.cid(), evt.get_text()), INDENT3)
+            write_simple_tag(xmlfile, "text", "[%d]%s" % (evt.cid(), evt.get_text()), INDENT3)
         elif evt.is_subevent():
-            write_simple_tag(file, "text", "(%d)%s" % (evt.cid(), evt.get_text()), INDENT3)
+            write_simple_tag(xmlfile, "text", "(%d)%s" % (evt.cid(), evt.get_text()), INDENT3)
         else:
             text = evt.get_text()
             if self._text_starts_with_container_tag(evt.get_text()):
                 text = self._add_leading_space_to_text(evt.get_text())
-            write_simple_tag(file, "text", text, INDENT3)
+            write_simple_tag(xmlfile, "text", text, INDENT3)
         if evt.get_data("progress") is not None:
-            write_simple_tag(file, "progress", "%s" % evt.get_data("progress"), INDENT3)
-        write_simple_tag(file, "fuzzy", "%s" % evt.get_fuzzy(), INDENT3)
-        write_simple_tag(file, "locked", "%s" % evt.get_locked(), INDENT3)
-        write_simple_tag(file, "ends_today", "%s" % evt.get_ends_today(), INDENT3)
+            write_simple_tag(xmlfile, "progress", "%s" % evt.get_data("progress"), INDENT3)
+        write_simple_tag(xmlfile, "fuzzy", "%s" % evt.get_fuzzy(), INDENT3)
+        write_simple_tag(xmlfile, "locked", "%s" % evt.get_locked(), INDENT3)
+        write_simple_tag(xmlfile, "ends_today", "%s" % evt.get_ends_today(), INDENT3)
         if evt.get_category() is not None:
-            write_simple_tag(file, "category", evt.get_category().get_name(), INDENT3)
+            write_simple_tag(xmlfile, "category", evt.get_category().get_name(), INDENT3)
         if evt.get_data("description") is not None:
-            write_simple_tag(file, "description", evt.get_data("description"), INDENT3)
+            write_simple_tag(xmlfile, "description", evt.get_data("description"), INDENT3)
         alert = evt.get_data("alert")
         if alert is not None:
-            write_simple_tag(file, "alert", alert_string(self.db.get_time_type(), alert),
+            write_simple_tag(xmlfile, "alert", alert_string(self.db.get_time_type(), alert),
                              INDENT3)
         hyperlink = evt.get_data("hyperlink")
         if hyperlink is not None:
-            write_simple_tag(file, "hyperlink", hyperlink, INDENT3)
+            write_simple_tag(xmlfile, "hyperlink", hyperlink, INDENT3)
         if evt.get_data("icon") is not None:
             icon_text = icon_string(evt.get_data("icon"))
-            write_simple_tag(file, "icon", icon_text, INDENT3)
+            write_simple_tag(xmlfile, "icon", icon_text, INDENT3)
     _write_event = wrap_in_tag(_write_event, "event", INDENT2)
 
-    def _write_eras(self, file):
+    def _write_eras(self, xmlfile):
         for era in self.db.get_all_eras():
-            self._write_era(file, era)
+            self._write_era(xmlfile, era)
     _write_eras = wrap_in_tag(_write_eras, "eras", INDENT1)
 
-    def _write_era(self, file, era):
-        write_simple_tag(file, "name", era.get_name(), INDENT3)
-        write_simple_tag(file, "start", self._time_string(era.get_time_period().start_time), INDENT3)
-        write_simple_tag(file, "end", self._time_string(era.get_time_period().end_time), INDENT3)
-        write_simple_tag(file, "color", color_string(era.get_color()), INDENT3)
+    def _write_era(self, xmlfile, era):
+        write_simple_tag(xmlfile, "name", era.get_name(), INDENT3)
+        write_simple_tag(xmlfile, "start", self._time_string(era.get_time_period().start_time), INDENT3)
+        write_simple_tag(xmlfile, "end", self._time_string(era.get_time_period().end_time), INDENT3)
+        write_simple_tag(xmlfile, "color", color_string(era.get_color()), INDENT3)
     _write_era = wrap_in_tag(_write_era, "era", INDENT2)
 
     def _text_starts_with_container_tag(self, text):
@@ -157,37 +157,37 @@ class Exporter(object):
     def _add_leading_space_to_text(self, text):
         return " %s" % text
 
-    def _write_view(self, file):
+    def _write_view(self, xmlfile):
         if self.db.get_displayed_period() is not None:
-            self._write_displayed_period(file)
-        self._write_hidden_categories(file)
+            self._write_displayed_period(xmlfile)
+        self._write_hidden_categories(xmlfile)
     _write_view = wrap_in_tag(_write_view, "view", INDENT1)
 
-    def _write_displayed_period(self, file):
+    def _write_displayed_period(self, xmlfile):
         period = self.db.get_displayed_period()
-        write_simple_tag(file, "start",
+        write_simple_tag(xmlfile, "start",
                          self._time_string(period.start_time), INDENT3)
-        write_simple_tag(file, "end",
+        write_simple_tag(xmlfile, "end",
                          self._time_string(period.end_time), INDENT3)
     _write_displayed_period = wrap_in_tag(_write_displayed_period,
                                           "displayed_period", INDENT2)
 
-    def _write_hidden_categories(self, file):
+    def _write_hidden_categories(self, xmlfile):
         for cat in self.db.get_hidden_categories():
-            write_simple_tag(file, "name", cat.get_name(), INDENT3)
+            write_simple_tag(xmlfile, "name", cat.get_name(), INDENT3)
     _write_hidden_categories = wrap_in_tag(_write_hidden_categories,
                                            "hidden_categories", INDENT2)
 
 
-def write_simple_tag(file, name, content, indent=""):
-    file.write(indent)
-    file.write("<")
-    file.write(name)
-    file.write(">")
-    file.write(xmlescape(content))
-    file.write("</")
-    file.write(name)
-    file.write(">\n")
+def write_simple_tag(xmlfile, name, content, indent=""):
+    xmlfile.write(indent)
+    xmlfile.write("<")
+    xmlfile.write(name)
+    xmlfile.write(">")
+    xmlfile.write(xmlescape(content))
+    xmlfile.write("</")
+    xmlfile.write(name)
+    xmlfile.write(">\n")
 
 
 def color_string(color):
