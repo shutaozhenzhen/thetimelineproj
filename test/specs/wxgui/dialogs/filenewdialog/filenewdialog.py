37ddd48cee02b849b7e16253040cfeea112ff208
Rickard Lindberg <ricli85@gmail.com>
2015-09-15 13:59:05 +0200

    Add template for file new dialog.

---
+++ test/specs/wxgui/dialogs/filenewdialog/filenewdialog.py
@@ -0,0 +1,37 @@
+# Copyright (C) 2009, 2010, 2011, 2012, 2013, 2014, 2015  Rickard Lindberg, Roger Lindberg
+#
+# This file is part of Timeline.
+#
+# Timeline is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Timeline is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Timeline.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from mock import Mock
+
+from timelinetest import UnitTestCase
+from timelinelib.wxgui.dialogs.filenewdialog.filenewdialog import FileNewDialog
+from timelinelib.wxgui.dialogs.filenewdialog.filenewdialogcontroller import FileNewDialogController
+
+
+class FileNewDialogTestCase(UnitTestCase):
+
+    def setUp(self):
+        self.view = Mock(FileNewDialog)
+        self.controller = FileNewDialogController(self.view)
+
+
+class describe_controller_instantiation(FileNewDialogTestCase):
+
+    def test_controller_can_be_instantiated(self):
+        self.assertTrue(self.controller is not None)
+
