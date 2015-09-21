d548ec09253dad1d5d96b23f8c3e2b34c2573e19
Rickard Lindberg <ricli85@gmail.com>
2015-09-21 11:15:38 +0200

    Category choice is used in edit category dialog.

--- source/timelinelib/wxgui/dialogs/editcategorydialog/editcategorydialog.py
+++ source/timelinelib/wxgui/dialogs/editcategorydialog/editcategorydialog.py
@@ -42,7 +42,7 @@ class EditCategoryDialog(Dialog):
             <StaticText align="ALIGN_CENTER_VERTICAL" label="$(font_color_text)" />
             <ColourSelect name="fontcolorpicker" align="ALIGN_CENTER_VERTICAL" width="60" height="30" />
             <StaticText align="ALIGN_CENTER_VERTICAL" label="$(parent_text)" />
-            <Choice name="parentlistbox" align="ALIGN_CENTER_VERTICAL" />
+            <CategoryChoice name="category_choice" timeline="$(db)" align="ALIGN_CENTER_VERTICAL" />
         </FlexGridSizer>
         <DialogButtonsOkCancelSizer
             border="LEFT|BOTTOM|RIGHT"
@@ -53,6 +53,7 @@ class EditCategoryDialog(Dialog):
 
     def __init__(self, parent, title, db, category):
         Dialog.__init__(self, EditCategoryDialogController, parent, {
+            "db": db,
             "name_text": _("Name:"),
             "color_text": _("Color:"),
             "progress_color_text": _("Progress Color:"),
@@ -62,15 +63,9 @@ class EditCategoryDialog(Dialog):
         }, title=title)
         self.controller.on_init(category, DbWrapperCategoryRepository(db))
 
-    def SetCategoryTree(self, tree):
-        def add_tree(tree, indent=""):
-            for (root, subtree) in tree:
-                self.parentlistbox.Append(indent + root.name, root)
-                add_tree(subtree, indent + "    ")
-        self.parentlistbox.Clear()
-        self.parentlistbox.Append("", None)  # No parent
-        add_tree(tree)
-        self.SetSizerAndFit(self.GetSizer())
+    def PopulateCategories(self, exclude):
+        self.category_choice.Populate(exclude=exclude)
+        self.Fit()
 
     def GetName(self):
         return self.txt_name.GetValue().strip()
@@ -103,17 +98,10 @@ class EditCategoryDialog(Dialog):
         self.fontcolorpicker.SetValue(new_color)
 
     def GetParent(self):
-        selection = self.parentlistbox.GetSelection()
-        if selection == wx.NOT_FOUND:
-            return None
-        return self.parentlistbox.GetClientData(selection)
+        return self.category_choice.GetSelectedCategory()
 
     def SetParent(self, parent):
-        no_items = self.parentlistbox.GetCount()
-        for i in range(0, no_items):
-            if self.parentlistbox.GetClientData(i) is parent:
-                self.parentlistbox.SetSelection(i)
-                return
+        return self.category_choice.SetSelectedCategory(parent)
 
     def HandleInvalidName(self, name):
         msg = _("Category name '%s' not valid. Must be non-empty.")
