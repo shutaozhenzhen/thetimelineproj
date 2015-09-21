ddfb79ef2df4ff2b11814f8355bf0d238e403fb5
Rickard Lindberg <ricli85@gmail.com>
2015-09-21 12:44:28 +0200

    Clean up.

--- source/timelinelib/wxgui/dialogs/setcategorydialog/setcategorydialogcontroller.py
+++ source/timelinelib/wxgui/dialogs/setcategorydialog/setcategorydialogcontroller.py
@@ -22,21 +22,21 @@ from timelinelib.wxgui.framework import Controller
 class SetCategoryDialogController(Controller):
 
     def on_init(self, db, selected_event_ids):
-        self.db = db
-        self.selected_event_ids = selected_event_ids
+        self._db = db
+        self._selected_event_ids = selected_event_ids
         self.view.PopulateCategories()
         self._set_title()
 
     def on_ok_clicked(self, event):
         category = self.view.GetSelectedCategory()
-        if not self._category_is_given(category) and self.selected_event_ids == []:
+        if not self._category_is_given(category) and self._selected_event_ids == []:
             self.view.DisplayErrorMessage(_("You must select a category!"))
         else:
             self._save_category_in_events(category)
             self.view.EndModalOk()
 
     def _set_title(self):
-        if self.selected_event_ids == []:
+        if self._selected_event_ids == []:
             self.view.SetTitle(_("Set Category on events without category"))
         else:
             self.view.SetTitle(_("Set Category on selected events"))
@@ -45,23 +45,23 @@ class SetCategoryDialogController(Controller):
         return category is not None
 
     def _save_category_in_events(self, category):
-        if self.selected_event_ids == []:
+        if self._selected_event_ids == []:
             self._save_category_in_events_for_events_without_category(category)
         else:
             self._save_category_in_events_for_selected_events(category)
 
     def _save_category_in_events_for_selected_events(self, category):
-        for event_id in self.selected_event_ids:
-            event = self.db.find_event_with_id(event_id)
+        for event_id in self._selected_event_ids:
+            event = self._db.find_event_with_id(event_id)
             event.set_category(category)
 
     def _save_category_in_events_for_events_without_category(self, category):
-        for event in self.db.get_all_events():
+        for event in self._db.get_all_events():
             if event.get_category() is None:
                 event.set_category(category)
 
     def _events_without_category_exists(self):
-        for event in self.db.get_all_events():
+        for event in self._db.get_all_events():
             if event.category is None:
                 return True
         return False
