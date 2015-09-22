a46e1ebeeabd1571367dc84dfba49fef49794d0c
Roger Lindberg <roger@rolidata.se>
2015-09-22 22:42:07 +0200

    Implemented new logic in Eras Editor Dialog

--- source/timelinelib/wxgui/dialogs/eraseditordialog/eraseditordialogcontroller.py
+++ source/timelinelib/wxgui/dialogs/eraseditordialog/eraseditordialogcontroller.py
@@ -16,10 +16,53 @@
 # along with Timeline.  If not, see <http://www.gnu.org/licenses/>.
 
 
+import time
+import wx
+
 from timelinelib.wxgui.framework import Controller
+from timelinelib.wxgui.dialogs.eraeditordialog.eraeditordialog import EraEditorDialog
+from timelinelib.data.era import Era
 
 
 class ErasEditorDialogController(Controller):
 
-    def on_init(self):
+    def on_init(self, db, config):
+        self.db = db
+        self.config = config
+        self.eras = db.get_all_eras()
+        self.view.SetEras(self.eras)
+
+    def on_edit(self, evt):
         pass
+
+    def on_add(self, evt):
+        era = self._create_era()
+        dlg = EraEditorDialog(self.view, _("Add an Era"), self.db.time_type, self.config, era)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.eras.append(era)
+            self.view.AppendEra(era)
+            self.db.save_era(era)
+        dlg.Destroy()
+
+    def on_remove(self, evt):
+        era = self.view.GetSelectedEra()
+        if era in self.eras:
+            self.eras.remove(era)
+            self.view.RemoveEra(era)
+
+    def on_dclick(self, evt):
+        self._edit(self.view.GetSelectedEra())
+
+    def _edit(self, era):
+        dlg = EraEditorDialog(self.view, _("Edit an Era"), self.db.time_type, self.config, era)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.view.UpdateEra(era)
+        dlg.Destroy()
+
+    def _create_era(self):
+        if self.db.time_type.is_date_time_type():
+            start = self.db.time_type.parse_time("%s 00:00:00" % time.strftime("%Y-%m-%d"))
+        else:
+            start = self.db.time_type.now()
+        end = start
+        return Era(self.db.time_type, start, end, "New Era")
