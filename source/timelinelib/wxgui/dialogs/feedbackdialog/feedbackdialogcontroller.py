5044ff3bb47b11451e5613069af9da660a9fabba
Rickard Lindberg <ricli85@gmail.com>
2015-09-24 13:31:22 +0200

    Port logic from old feedback dialog to new.

--- source/timelinelib/wxgui/dialogs/feedbackdialog/feedbackdialogcontroller.py
+++ source/timelinelib/wxgui/dialogs/feedbackdialog/feedbackdialogcontroller.py
@@ -16,10 +16,46 @@
 # along with Timeline.  If not, see <http://www.gnu.org/licenses/>.
 
 
+import urllib
+
 from timelinelib.wxgui.framework import Controller
 
 
 class FeedbackDialogController(Controller):
 
-    def on_init(self):
-        pass
+    def on_init(self, webbrowser, info, subject, body):
+        self.webbrowser = webbrowser
+        self.view.SetInfoText(info)
+        self.view.SetToText("thetimelineproj-user@lists.sourceforge.net")
+        self.view.SetSubjectText(subject)
+        self.view.SetBodyText(body)
+
+    def on_default_click(self, event):
+        attr = self._get_url_attributes()
+        url = "mailto:%s?subject=%s&body=%s" % (
+            urllib.quote(attr["to"]),
+            urllib.quote(attr["subject"]),
+            urllib.quote(attr["body"]),
+        )
+        self.webbrowser.open(url)
+
+    def on_gmail_click(self, event):
+        attr = self._get_url_attributes()
+        url = "https://mail.google.com/mail/?compose=1&view=cm&fs=1&to=%s&su=%s&body=%s" % (
+            urllib.quote(attr["to"]),
+            urllib.quote(attr["subject"]),
+            urllib.quote(attr["body"]),
+        )
+        self.webbrowser.open(url)
+
+    def on_other_click(self, event):
+        self.view.DisplayInformationMessage(
+            caption=_("Other email client"),
+            message=_("Copy and paste this email into your favorite email client and send it from there."))
+
+    def _get_url_attributes(self):
+        return {
+            "to": self.view.GetToText().encode("utf-8"),
+            "subject": self.view.GetSubjectText().encode("utf-8"),
+            "body": self.view.GetBodyText().encode("utf-8"),
+        }
