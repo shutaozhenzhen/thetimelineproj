06631d9f146d90a996472a077a281c6a01b86ad8
Rickard Lindberg <ricli85@gmail.com>
2015-09-27 12:03:13 +0200

    Layout for new edit event dialog depends on config.

--- test/specs/wxgui/dialogs/editeventdialog/editeventdialog.py
+++ test/specs/wxgui/dialogs/editeventdialog/editeventdialog.py
@@ -107,6 +107,7 @@ class describe_edit_event_dialog(EditEventDialogTestCase):
         config = Mock()
         config.event_editor_show_period = True
         config.event_editor_show_time = False
+        config.event_editor_tab_order = ["0", "1", "2", "3", "4", ":"]
         db = db_open(":tutorial:")
         categories = db.get_categories()
         categories[0].parent = categories[1]
