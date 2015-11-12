f2772594b61f45009f4ba8518435ed36d9b2d8f1
Rickard Lindberg <ricli85@gmail.com>
2015-11-12 10:46:45 +0100

    Problem: Batch script continues even if an individual command fails.

--- tools/jenkins-windows-exe.bat
+++ tools/jenkins-windows-exe.bat
@@ -1,4 +1,1 @@
-c:\python27\python test/execute-specs-repeat.py
-cd release
-cd buildtool
-c:\python27\python build_win32_py25.py
+c:\python27\python test/execute-specs-repeat.py && cd release && cd buildtool && c:\python27\python build_win32_py25.py
