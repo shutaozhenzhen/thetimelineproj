808235bfb063b2d42e93d4e8b6c01848fe37f2b2
Roger Lindberg <roger@rolidata.se>
2015-11-12 18:47:51 +0100

    Build with python 2.7

--- tools/jenkins-windows-exe.bat
+++ tools/jenkins-windows-exe.bat
@@ -1,1 +1,1 @@
-c:\python27\python tools\sanity-check.py && cd release && cd buildtool && c:\python27\python build_win32_py25.py
+c:\python27\python tools\sanity-check.py && cd release && cd buildtool && c:\python27\python build_win32.py
