e098df07fa7fd1068ea6541b9045dca54d43ffcc
Rickard Lindberg <ricli85@gmail.com>
2015-11-12 10:33:18 +0100

    Problem: Jenkins script is not versioned.

--- documentation/upload-to-sf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-USERNAME="rickardlindberg"
-
-HOST="$USERNAME,thetimelineproj@web.sourceforge.net"
-ROOT=/home/project-web/thetimelineproj
-RSYNC_FLAGS="--recursive --del --progress"
-
-make html && rsync $RSYNC_FLAGS _build/html/ $HOST:$ROOT/htdocs
