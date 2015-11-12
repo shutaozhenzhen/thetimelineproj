7beebb71afa7e9ae5be7ab2f87c7dceeddadcefa
Rickard Lindberg <ricli85@gmail.com>
2015-11-12 09:14:55 +0100

    Problem: Not possible to define revision in jenkins script.

--- tools/jenkins-linux-source.sh
+++ tools/jenkins-linux-source.sh
@@ -3,4 +3,4 @@ virtualenv venv -p python2.7 --system-site-packages
 . venv/bin/activate
 pip install -r requirements.txt
 python test/execute-specs-repeat.py
-python tools/package-source.py
+python tools/package-source.py --revision $1
