4acf38729010f3641dc723f73131611c222b883b
Rickard Lindberg <ricli85@gmail.com>
2015-11-23 15:36:41 +0100

    Problem: Debug texts in tip are annoying.
    When running from tip, the debug texts are always shown. This is annyoing.
    They can now be turned on with the --debug flag. Even in a released version.

--- tools/execute-specs.py
+++ tools/execute-specs.py
@@ -38,7 +38,6 @@ def execute_specs(args):
     setup_paths()
     install_gettext_in_builtin_namespace()
     setup_humblewx()
-    disable_monitoring()
     set_halt_gui_flag(args)
     suite = create_suite(create_include_test_function(args))
     all_pass = execute_suite(suite, select_verbosity(args))
@@ -76,11 +75,6 @@ def install_gettext_in_builtin_namespace():
 def setup_humblewx():
     import timelinelib.wxgui.setup
     timelinelib.wxgui.setup.setup_humblewx()
-
-
-def disable_monitoring():
-    from timelinelib.monitoring import monitoring
-    monitoring.IS_ENABLED = False
 
 
 def set_halt_gui_flag(args):
