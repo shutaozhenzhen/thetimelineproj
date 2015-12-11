16665de64a0988965c1c368d1262e66ca40a1505
Rickard Lindberg <ricli85@gmail.com>
2015-12-11 16:42:11 +0100

    Problem: There are no tests for text pattern control controller.

--- test/unit/wxgui/components/textpatterncontrol.py
+++ test/unit/wxgui/components/textpatterncontrol.py
@@ -19,15 +19,36 @@
 import datetime
 
 import humblewx
+from mock import Mock
 
 from timelinelib.test.cases.unit import UnitTestCase
+from timelinelib.wxgui.components.textpatterncontrol import TextPatternControl
+from timelinelib.wxgui.components.textpatterncontrol import TextPatternControlController
 from timelinelib.wxgui.framework import Dialog
 
 
 class describe_text_pattern_control(UnitTestCase):
 
+    def test_it_sets_values_depending_on_separators_and_values(self):
+        self.controller.set_separators(["a", "b"])
+        self.controller.set_values(["1", "2", "3"])
+        self.view.SetValue.assert_called_with("1a2b3")
+
+    def test_it_gets_values_depending_on_separators_and_values(self):
+        self.controller.set_separators(["a", "b"])
+        self.view.GetValue.return_value = "1a2b3"
+        self.assertEqual(self.controller.get_values(), ["1", "2", "3"])
+
     def test_show_manual_test_dialog(self):
         self.show_dialog(TextPatternControlManualTestDialog)
+
+    def setUp(self):
+        UnitTestCase.setUp(self)
+        self.view = Mock(TextPatternControl)
+        self.view.GetValue.return_value = ""
+        self.view.GetSelection.return_value = (0, 0)
+        self.controller = TextPatternControlController(self.view)
+        self.controller.on_init()
 
 
 class TextPatternControlManualTestDialog(Dialog):
