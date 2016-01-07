c795e0890b101c0d2da514f4020feefd420e2935
Rickard Lindberg <ricli85@gmail.com>
2016-01-07 13:58:53 +0100

    Problem: TimelineCanvas depends on never show period events as point events config.

--- source/timelinelib/canvas/appearance.py
+++ source/timelinelib/canvas/appearance.py
@@ -37,6 +37,7 @@ class Appearance(Observable):
         self._build_property("major_strip_font", Font(12, weight=wx.FONTWEIGHT_BOLD).serialize())
         self._build_property("legend_font", Font(8).serialize())
         self._build_property("center_event_texts", False)
+        self._build_property("never_show_period_events_as_point_events", False)
 
     def _build_property(self, name, initial_value):
         def getter():
