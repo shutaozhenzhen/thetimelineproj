471d901c4410d1d41add0c35239208f687fbcc46
Rickard Lindberg <ricli85@gmail.com>
2016-01-07 13:26:26 +0100

    Problem: TimelineCanvas depends on draw periods events to right config.

--- source/timelinelib/canvas/appearance.py
+++ source/timelinelib/canvas/appearance.py
@@ -28,6 +28,7 @@ class Appearance(Observable):
         self._build_property("minor_strip_divider_line_colour", (200, 200, 200))
         self._build_property("major_strip_divider_line_colour", (200, 200, 200))
         self._build_property("now_line_colour", (200, 0, 0))
+        self._build_property("draw_period_events_to_right", False)
 
     def _build_property(self, name, initial_value):
         def getter():
