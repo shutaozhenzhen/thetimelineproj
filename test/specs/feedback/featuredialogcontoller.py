aa8eedc18c7d908b401d2100a262af53147abc3d
Roger Lindberg <roger@rolidata.se>
2016-01-08 10:01:10 +0100

    Tests are missing
    => Add tests

---
+++ test/specs/feedback/featuredialogcontoller.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2009, 2010, 2011, 2012, 2013, 2014, 2015  Rickard Lindberg, Roger Lindberg
+#
+# This file is part of Timeline.
+#
+# Timeline is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Timeline is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Timeline.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from mock import Mock
+
+from timelinelib.test.cases.unit import UnitTestCase
+from timelinelib.feedback.featuredialogcontoller import FeatureDialogController
+from timelinelib.wxgui.dialogs.feature.view import FeatureDialog
+
+
+class FeatureDialogControllerTestCase(UnitTestCase):
+
+    def setUp(self):
+        self.view = Mock(FeatureDialog)
+        self.controller = FeatureDialogController(self.view)
+
+
+class describe_feature_dialog_controller(FeatureDialogControllerTestCase):
+
+    def test_can_be_instantiated(self):
+        self.assertTrue(self.controller.view)
