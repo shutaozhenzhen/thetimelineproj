3fe64e4be1b79adf5fc68f8f179146b042fbbf79
Roger Lindberg <roger@rolidata.se>
2016-01-09 13:42:17 +0100

    Test contains duplications
    => Eliminate duplications

--- test/unit/help/helppagerepository.py
+++ test/unit/help/helppagerepository.py
@@ -24,13 +24,19 @@ from timelinelib.help.helppagerepository import HelpPage
 HOME_PAGE_ID = "contents"
 HELP_RESOURCES_ROOT_DIR = "root_dir"
 PAGE_PREFIX = "page:"
+PAGE_ID = "id"
+PAGE_HEADER = "header"
+PAGE_BODY = "body"
 
 
-class describe_help_page_repoistory(UnitTestCase):
+class HelpPageRepositoryUnitTestCase(UnitTestCase):
 
     def setUp(self):
         UnitTestCase.setUp(self)
-        self.repository = HelpPageRepository(HOME_PAGE_ID, HELP_RESOURCES_ROOT_DIR, "page:")
+        self.repository = HelpPageRepository(HOME_PAGE_ID, HELP_RESOURCES_ROOT_DIR, PAGE_PREFIX)
+
+
+class describe_help_page_repoistory_instansiation(HelpPageRepositoryUnitTestCase):
 
     def test_has_an_empty_page_dictionary_when_instatiated(self):
         self.assertEqual({}, self.repository.help_pages)
@@ -44,6 +50,9 @@ class describe_help_page_repoistory(UnitTestCase):
     def test_is_given_a_page_prefix_when_instatiated(self):
         self.assertEqual(PAGE_PREFIX, self.repository.page_prefix)
 
+
+class describe_help_page_repoistory_page_creation(HelpPageRepositoryUnitTestCase):
+
     def test_a_page_can_be_created_and_saved(self):
         self.repository.install_page(None, None, None, None)
         self.assertEqual(1, len(self.repository.help_pages))
@@ -55,79 +64,88 @@ class describe_help_page_repoistory(UnitTestCase):
         self.assertTrue(isinstance(page, HelpPage))
 
     def test_pages_are_defined_by_id_header_body_and_related_pages(self):
-        self.repository.install_page("id", "header", "body", [])
-
-    def test_pages_can_be_retrieved_by_page_id(self):
-        self.repository.install_page("id", "header", "body", [])
-        page = self.repository.get_page("id")
-        self.assertTrue(page)
+        self.repository.install_page(PAGE_ID, PAGE_HEADER, PAGE_BODY, [])
 
     def test_related_pages_has_default_value(self):
-        self.repository.install_page("id", "header", "body")
-        page = self.repository.get_page("id")
+        self.repository.install_page(PAGE_ID, PAGE_HEADER, PAGE_BODY)
+        page = self.repository.get_page(PAGE_ID)
         self.assertEqual([], page.related_pages)
-
-    def test_pages_not_crated_cannot_be_retrieved(self):
-        page = self.repository.get_page("id")
-        self.assertFalse(page)
 
     def test_pages_has_id_header_body_and_related_pages(self):
-        self.repository.install_page("id", "header", "body", [])
-        page = self.repository.get_page("id")
-        self.assertEqual("id", page.page_id)
-        self.assertEqual("header", page.header)
-        self.assertEqual("body", page.body)
+        self.repository.install_page(PAGE_ID, PAGE_HEADER, PAGE_BODY, [])
+        page = self.repository.get_page(PAGE_ID)
+        self.assertEqual(PAGE_ID, page.page_id)
+        self.assertEqual(PAGE_HEADER, page.header)
+        self.assertEqual(PAGE_BODY, page.body)
         self.assertEqual([], page.related_pages)
 
+
+class describe_help_page_repoistory_page_retrieval(HelpPageRepositoryUnitTestCase):
+
+    def test_pages_can_be_retrieved_by_page_id(self):
+        self.repository.install_page(PAGE_ID, PAGE_HEADER, PAGE_BODY, [])
+        page = self.repository.get_page(PAGE_ID)
+        self.assertTrue(page)
+
+    def test_pages_not_crated_cannot_be_retrieved(self):
+        page = self.repository.get_page(PAGE_ID)
+        self.assertFalse(page)
+
     def test_pages_can_be_found_by_text_search_in_header(self):
-        self.repository.install_page("id", "header", "body", [])
-        pages = self.repository._find_pages("header")
+        self.repository.install_page(PAGE_ID, PAGE_HEADER, PAGE_BODY, [])
+        pages = self.repository._find_pages(PAGE_HEADER)
         self.assertEqual(1, len(pages))
-        self.assertEqual("header", pages[0].header)
+        self.assertEqual(PAGE_HEADER, pages[0].header)
+
+
+class describe_help_page_repoistory_page_search(HelpPageRepositoryUnitTestCase):
 
     def test_pages_can_be_found_by_text_search_in_body(self):
-        self.repository.install_page("id", "header", "body", [])
+        self.repository.install_page(PAGE_ID, PAGE_HEADER, PAGE_BODY, [])
         pages = self.repository._find_pages("body")
         self.assertEqual(1, len(pages))
-        self.assertEqual("header", pages[0].header)
+        self.assertEqual(PAGE_HEADER, pages[0].header)
 
     def test_serach_result_page_rendered_when_no_matches_found(self):
         serach_result_page = self.repository.get_search_results_page("body")
         self.assertEqual("<h1>#Search results for 'body'#</h1><ul></ul>", serach_result_page)
 
     def test_serach_result_page_rendered_when_matches_found(self):
-        self.repository.install_page("id", "header", "body", [])
+        self.repository.install_page(PAGE_ID, PAGE_HEADER, PAGE_BODY, [])
         serach_result_page = self.repository.get_search_results_page("body")
         self.assertEqual('<h1>#Search results for \'body\'#</h1><ul><li><a href="page:id">header</a></li></ul>', serach_result_page)
 
 
-class describe_help_page(UnitTestCase):
+class HelpPageUnitTestCase(UnitTestCase):
 
     def setUp(self):
         UnitTestCase.setUp(self)
-        self.repository = HelpPageRepository(HOME_PAGE_ID, HELP_RESOURCES_ROOT_DIR, "page:")
-        self.help_page = HelpPage(self.repository, "id", "header", "body", [])
+        self.repository = HelpPageRepository(HOME_PAGE_ID, HELP_RESOURCES_ROOT_DIR, PAGE_PREFIX)
+        self.help_page = HelpPage(self.repository, PAGE_ID, PAGE_HEADER, PAGE_BODY, [])
+
+
+class describe_help_page(HelpPageUnitTestCase):
 
     def test_has_ref_to_repository(self):
         self.assertEqual(self.repository, self.help_page.help_page_repository)
 
     def test_has_id_header_body_and_related_pages(self):
-        self.assertEqual("id", self.help_page.page_id)
-        self.assertEqual("header", self.help_page.header)
-        self.assertEqual("body", self.help_page.body)
+        self.assertEqual(PAGE_ID, self.help_page.page_id)
+        self.assertEqual(PAGE_HEADER, self.help_page.header)
+        self.assertEqual(PAGE_BODY, self.help_page.body)
         self.assertEqual([], self.help_page.related_pages)
 
     def test_can_be_rendered_to_html(self):
-        self.assertEqual("<h1>header</h1><p>body</p>", self.help_page.render_to_html())
+        self.assertEqual("<h1>%s</h1><p>%s</p>" % (PAGE_HEADER, PAGE_BODY), self.help_page.render_to_html())
 
     def test_can_be_rendered_to_html_with_related_pages(self):
-        self.repository.install_page("foo", "header", "body", [])
-        self.repository.install_page("bar", "header", "body", [])
-        self.help_page = HelpPage(self.repository, "id", "header", "body", ["foo", "bar"])
+        self.repository.install_page("foo", PAGE_HEADER, PAGE_BODY, [])
+        self.repository.install_page("bar", PAGE_HEADER, PAGE_BODY, [])
+        self.help_page = HelpPage(self.repository, PAGE_ID, PAGE_HEADER, PAGE_BODY, ["foo", "bar"])
         self.assertEqual('<h1>header</h1><p>body</p><h2>#Related pages#</h2><ul><li><a href="page:foo">header</a></li><li><a href="page:bar">header</a></li></ul>', 
                          self.help_page.render_to_html())
 
     def test_can_be_rendered_to_html_with_related_pages_not_found(self):
-        self.help_page = HelpPage(self.repository, "id", "header", "body", ["foo", "bar"])
-        self.assertEqual('<h1>header</h1><p>body</p><h2>#Related pages#</h2><ul></ul>', 
+        self.help_page = HelpPage(self.repository, PAGE_ID, PAGE_HEADER, PAGE_BODY, ["foo", "bar"])
+        self.assertEqual('<h1>%s</h1><p>%s</p><h2>#Related pages#</h2><ul></ul>' % (PAGE_HEADER, PAGE_BODY), 
                          self.help_page.render_to_html())
