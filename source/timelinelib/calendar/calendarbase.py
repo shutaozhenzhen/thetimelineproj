e8a5ca97d3558ba81abde53695f504f6e9ac6295
Roger Lindberg <roger@rolidata.se>
2016-01-09 13:54:12 +0100

    Code not following Pep8
    => Conform to Pep8

--- source/timelinelib/calendar/calendarbase.py
+++ source/timelinelib/calendar/calendarbase.py
@@ -17,13 +17,13 @@
 
 
 class CalendarBase(object):
-    
+
     def replace(self, year=None, month=None):
         raise NotImplementedError("replace not implemented.")
 
     def days_in_month(self):
         raise NotImplementedError("days_in_month not implemented.")
-    
+
     def to_tuple(self):
         raise NotImplementedError("to_tuple not implemented.")
 
@@ -51,36 +51,35 @@ class CalendarUtilsBase(object):
     @classmethod
     def is_valid(self, year, month, day):
         raise NotImplementedError("is_valid not implemented.")
-    
-    @classmethod    
+
+    @classmethod
     def is_valid_time(self, hour, minute, second):
         raise NotImplementedError("is_valid_time not implemented.")
-    
+
     @classmethod
     def days_in_month(self, year, month):
         raise NotImplementedError("days_in_month not implemented.")
-    
+
     @classmethod
     def is_leap_year(year):
         raise NotImplementedError("is_leap_year not implemented.")
-    
+
     @classmethod
     def from_absolute_day(self, absolute_day):
         raise NotImplementedError("from_absolute_day not implemented.")
-    
+
     @classmethod
     def to_absolute_day(self, year, month, day):
         raise NotImplementedError("to_absolute_day not implemented.")
-    
+
     @classmethod
     def calendar_week(self, time):
         raise NotImplementedError("calendar_week not implemented.")
-    
+
     @classmethod
     def from_time(self, time):
         raise NotImplementedError("from_time not implemented.")
-    
+
     @classmethod
     def from_date(self, year, month, day):
         raise NotImplementedError("from_date not implemented")
-    
