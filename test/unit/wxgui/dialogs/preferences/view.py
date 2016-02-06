0aba7589ad534a9bbf63123938f44ffaeda25405
Roger Lindberg <roger@rolidata.se>
2016-02-06 10:10:22 +0100

    Test for view designing is missing
    Create test

---
+++ test/unit/wxgui/dialogs/preferences/view.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2009, 2010, 2011, 2012, 2013, 2014, 2015  Rickard Lindberg, Roger Lindberg
+#
+# This file is part of Timeline.
+#
+# Timeline is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Timeline is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Timeline.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import os
+import wx
+from timelinelib.test.cases.unit import UnitTestCase
+from timelinelib.config.dotfile import Config
+from timelinelib.wxgui.dialogs.preferences.view import PreferencesDialog
+
+
+class _describe_preferences_dialog(UnitTestCase):
+
+    def test_it_can_be_created(self):
+        app = wx.App()
+        path = os.path.join(
+                wx.StandardPaths.Get().GetUserConfigDir(),
+                ".thetimelineproj.cfg")
+        config = Config(path)
+        self.show_dialog(PreferencesDialog, None, config)
