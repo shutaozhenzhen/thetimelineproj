5b60cfcbf02d37c986389a13edad1dc4e6dac01c
Rickard Lindberg <ricli85@gmail.com>
2016-02-14 11:48:56 +0100

    First working version of new gregorian date picker.

---
+++ test/unit/wxgui/components/newgregoriandatepicker/view.py
@@ -0,0 +1,59 @@
+# Copyright (C) 2009, 2010, 2011, 2012, 2013, 2014, 2015  Rickard Lindberg, Roger Lindberg
+#
+# This file is part of Timeline.
+#
+# Timeline is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Timeline is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Timeline.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import datetime
+
+import humblewx
+
+from timelinelib.calendar.newdateformatter import NewDateFormatter
+from timelinelib.test.cases.unit import UnitTestCase
+from timelinelib.wxgui.framework import Dialog
+
+
+class describe_new_gregorian_date_picker_view(UnitTestCase):
+
+    def test_show_manual_test_dialog(self):
+        self.show_dialog(NewGregorianDatePickerTestDialog)
+
+
+class NewGregorianDatePickerTestDialog(Dialog):
+
+    """
+    <BoxSizerVertical>
+        <FlexGridSizer columns="1" border="ALL">
+            <Button label="before" />
+            <NewGregorianDatePicker
+                date_formatter="$(date_formatter)"
+                name="date"
+            />
+            <Button label="after" />
+        </FlexGridSizer>
+    </BoxSizerVertical>
+    """
+
+    def __init__(self):
+        Dialog.__init__(self, humblewx.Controller, None, {
+            "date_formatter": self._create_date_formatter()
+        })
+        self.date.SetGregorianDate((2015, 11, 1))
+
+    def _create_date_formatter(self):
+        formatter = NewDateFormatter()
+        formatter.set_separators("/", " ")
+        formatter.set_region_order(year=2, month=1, day=0)
+        return formatter
