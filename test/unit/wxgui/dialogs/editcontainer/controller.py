840e4f9406dd9bf056e73cad3233514aa10d2474
Roger Lindberg <roger@rolidata.se>
2016-02-23 06:55:31 +0100

    Moved test -> unit

--- test/unit/wxgui/dialogs/editcontainer/controller.py
+++ test/unit/wxgui/dialogs/editcontainer/controller.py
@@ -22,7 +22,6 @@ from timelinelib.canvas.data.db import MemoryDB
 from timelinelib.canvas.data.exceptions import TimelineIOError
 from timelinelib.canvas.data import Container
 from timelinelib.db import db_open
-from timelinelib.repositories.interface import EventRepository
 from timelinelib.test.cases.unit import UnitTestCase
 from timelinelib.test.utils import human_time_to_gregorian
 from timelinelib.time.gregoriantime import GregorianTimeType
