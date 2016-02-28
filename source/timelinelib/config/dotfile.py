7f649d7452a4d34f1f85e2f1323dc37949c547c1
Rickard Lindberg <ricli85@gmail.com>
2016-02-28 15:51:15 +0100

    Remove never_show_period_events_as_point_events property.
    Reason: Properties do not work with new config and they are generally confusing.
    Generally confusing because you expect a variable to just be set, but actually
    a setter function is called instead.

--- source/timelinelib/config/dotfile.py
+++ source/timelinelib/config/dotfile.py
@@ -308,8 +308,6 @@ class Config(Observable):
     def set_never_show_period_events_as_point_events(self, value):
         self.config_parser.set(DEFAULTSECT, NEVER_SHOW_PERIOD_EVENTS_AS_POINT_EVENTS, str(value))
         self._notify()
-    never_show_period_events_as_point_events = property(get_never_show_period_events_as_point_events,
-                                                        set_never_show_period_events_as_point_events)
 
     def get_center_event_texts(self):
         return self.config_parser.getboolean(DEFAULTSECT, CENTER_EVENT_TEXTS)
