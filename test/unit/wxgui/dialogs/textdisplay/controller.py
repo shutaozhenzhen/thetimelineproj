0aea84333b1cd3a259b8d93222ef0949c40b98a3
Roger Lindberg <roger@rolidata.se>
2016-04-28 07:23:57 +0200

    Tests are missing Added tests

--- test/unit/wxgui/dialogs/textdisplay/controller.py
+++ test/unit/wxgui/dialogs/textdisplay/controller.py
@@ -16,6 +16,8 @@
 # along with Timeline.  If not, see <http://www.gnu.org/licenses/>.
 
 
+import wx
+
 from mock import Mock
 
 from timelinelib.wxgui.dialogs.textdisplay.controller import TextDisplayDialogController
@@ -23,15 +25,35 @@ from timelinelib.wxgui.dialogs.textdisplay.view import TextDisplayDialog
 from timelinelib.test.cases.unit import UnitTestCase
 
 
-class describe_text_display_dialog(UnitTestCase):
-
-    def setUp(self):
-        self.view = Mock(TextDisplayDialog)
-        self.controller = TextDisplayDialogController(self.view)
+class describe_text_display_dialog_gui(UnitTestCase):
 
     def test_it_can_be_created(self):
         self.show_dialog(TextDisplayDialog, "title", "text", None)
 
+
+class describe_text_display_dialog(UnitTestCase):
+
     def test_set_text_on_init(self):
         self.controller.on_init("hello world")
         self.view.SetText.assert_called_with("hello world")
+
+    def test_cant_copy_to_clipboard_when_clipboard_open_fails(self):
+        wx.TheClipboard.Open.return_value = False
+        self.controller.on_copy_click(None)
+        self.view.DisplayErrorMessage.assert_called_with(_("Unable to copy to clipboard."))
+
+    def test_can_copy_to_clipboard_when_clipboard_opens(self):
+        wx.TheClipboard.Open.return_value = True
+        self.controller.on_copy_click(None)
+        self.assertEqual(wx.TheClipboard.SetData.call_count, 1)
+        self.assertEqual(wx.TheClipboard.Close.call_count, 1)
+
+    def setUp(self):
+        self.TheClipboard = wx.TheClipboard
+        wx.TheClipboard = Mock()
+        self.view = Mock(TextDisplayDialog)
+        self.view.GetText.return_value = "Test text"
+        self.controller = TextDisplayDialogController(self.view)
+
+    def tearDown(self):
+        wx.TheClipboard = self.TheClipboard
