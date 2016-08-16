0bd258012928521a4cc74b22d71939a922ab3efb
Roger Lindberg <roger@rolidata.se>
2016-08-16 07:38:11 +0200

    Refactored to simplify tests Added tests

--- test/unit/canvas/svg.py
+++ test/unit/canvas/svg.py
@@ -21,6 +21,7 @@ from mock import Mock
 from timelinelib.canvas.svg import SVGDrawingAlgorithm
 from timelinelib.canvas.drawing.scene import TimelineScene
 from timelinelib.canvas.drawing.viewproperties import ViewProperties
+from timelinelib.canvas.data.event import Event
 from timelinelib.test.cases.unit import UnitTestCase
 
 
@@ -34,11 +35,27 @@ class describe_svg_drawing_algorithm(UnitTestCase):
         shape = self.svg._draw_now_line()
         self.assertEqual(shape.getXML(), '<line y1="0" x2="150" style="stroke:darkred; stroke-width:1; " x1="150" y2="200"  />\n')
 
+    def test_can_draw_line_to_selected_non_period_event(self):
+        self.view_properties.is_selected.return_value = True
+        line, circle = self.svg._draw_line_to_non_period_event(self.view_properties, self.point_event, self.point_event_rect)
+        self.assertEqual(line.getXML(), '<line y1="106" x2="200" style="stroke:red; stroke-width:1; " x1="200" y2="200"  />\n')
+        self.assertEqual(circle.getXML(), '<circle cy="200" cx="200" r="2" style="stroke:black; stroke-width:1; fill:none; "  />\n')
+
+    def test_can_draw_line_to_nonselected_non_period_event(self):
+        line, circle = self.svg._draw_line_to_non_period_event(self.view_properties, self.point_event, self.point_event_rect)
+        self.assertEqual(line.getXML(), '<line y1="106" x2="200" style="stroke:black; stroke-width:1; " x1="200" y2="200"  />\n')
+        self.assertEqual(circle.getXML(), '<circle cy="200" cx="200" r="2" style="stroke:black; stroke-width:1; fill:none; "  />\n')
+
     def setUp(self):
         path = Mock()
-        scene = self.setup_scene()
+        self.view_properties = self.setup_view_properties()
+        self.scene = self.setup_scene()
+        self.svg = SVGDrawingAlgorithm(path, self.scene, self.view_properties)
+
+    def setup_view_properties(self):
         view_properties = Mock(ViewProperties)
-        self.svg = SVGDrawingAlgorithm(path, scene, view_properties)
+        view_properties.is_selected.return_value = False
+        return view_properties
 
     def setup_scene(self):
         scene = Mock(TimelineScene)
@@ -46,4 +63,10 @@ class describe_svg_drawing_algorithm(UnitTestCase):
         scene.height = 200
         scene.divider_y = 200
         scene.x_pos_for_now.return_value = 150
+        scene.x_pos_for_time.return_value = 200
+        self.point_event = Mock(Event)
+        self.point_event_rect = Mock()
+        self.point_event_rect.Y = 100
+        self.point_event_rect.Height = 12
+        scene.event_data = [(self.point_event, self.point_event_rect), ]
         return scene
