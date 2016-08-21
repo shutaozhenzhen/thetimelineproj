575799861e0747208df8da5f499546a8a077533d
Roger Lindberg <roger@rolidata.se>
2016-08-21 07:29:28 +0200

    Renamed local variable

--- source/timelinelib/canvas/svg.py
+++ source/timelinelib/canvas/svg.py
@@ -371,10 +371,10 @@ class SVGDrawingAlgorithm(object):
                  (rx, ry + height, rx + width, ry, rx))
         return pathId, p
 
-    def _draw_text(self, myString, rectTuple, myStyle):
-        myString = self._encode_text(myString)
+    def _draw_text(self, my_text, rectTuple, myStyle):
+        my_text = self._encode_text(my_text)
         text_x, text_y, _ = self._calc_text_pos(rectTuple)
-        myText = text(myString, text_x, text_y)
+        myText = text(my_text, text_x, text_y)
         myText.set_style(myStyle.getStyle())
         myText.set_lengthAdjust("spacingAndGlyphs")
         return myText
