05db80e308a36a9f727437d05c1958a5b95b7ed4
Rickard Lindberg <ricli85@gmail.com>
2017-01-02 15:38:55 +0100

    Use TimelineItem in Era.

--- source/timelinelib/canvas/data/era.py
+++ source/timelinelib/canvas/data/era.py
@@ -16,13 +16,13 @@
 # along with Timeline.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from timelinelib.canvas.data.timeperiod import TimePeriod
+from timelinelib.canvas.data.item import TimelineItem
 
 
 DEFAULT_ERA_COLOR = (200, 200, 200)
 
 
-class Era(object):
+class Era(TimelineItem):
     """
     A clearly defined period of time of arbitrary but well-defined length.
 
@@ -47,10 +47,10 @@ class Era(object):
         return not (self == other)
 
     def __gt__(self, other):
-        return self.get_time_period().start_time > other.get_time_period().start_time
+        return self.get_start_time() > other.get_start_time()
 
     def __lt__(self, other):
-        return self.get_time_period().start_time < other.get_time_period().start_time
+        return self.get_start_time() < other.get_start_time()
 
     def ends_today(self):
         return self._ends_today
@@ -59,13 +59,13 @@ class Era(object):
         self._ends_today = value
 
     def update(self, start_time, end_time, name, color=DEFAULT_ERA_COLOR):
-        self.time_period = TimePeriod(start_time, end_time)
+        self.update_period(start_time, end_time)
         self.name = name.strip()
         self.color = color
 
     def clone(self):
-        new_era = Era(self.time_period.start_time,
-                      self.time_period.end_time, self.name, self.color)
+        new_era = Era(self.get_start_time(),
+                      self.get_end_time(), self.name, self.color)
         new_era.set_ends_today(self._ends_today)
         return new_era
 
@@ -78,13 +78,6 @@ class Era(object):
 
     def has_id(self):
         return self.id is not None
-
-    def set_time_period(self, time_period):
-        self.time_period = time_period
-        return self
-
-    def get_time_period(self):
-        return self.time_period
 
     def set_name(self, name):
         self.name = name.strip()
@@ -100,21 +93,18 @@ class Era(object):
     def get_color(self):
         return self.color
 
-    def inside_period(self, time_period):
-        return self.time_period.inside_period(time_period)
-
     def overlapping(self, era):
-        if era.get_time_period().start_time >= self.time_period.end_time:
+        if era.get_start_time() >= self.get_end_time():
             return 0
-        if era.get_time_period().start_time == self.time_period.start_time:
-            if era.get_time_period().end_time == self.time_period.end_time:
+        if era.get_start_time() == self.get_start_time():
+            if era.get_end_time() == self.get_end_time():
                 return 4
-            if era.get_time_period().end_time > self.time_period.end_time:
+            if era.get_end_time() > self.get_end_time():
                 return 2
             else:
                 return 3
-        if era.get_time_period().end_time == self.time_period.end_time:
+        if era.get_end_time() == self.get_end_time():
             return 5
-        if era.get_time_period().end_time > self.time_period.end_time:
+        if era.get_end_time() > self.get_end_time():
             return 1
         return 6
