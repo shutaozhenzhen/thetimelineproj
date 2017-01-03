0459b625d1be0be7204fc5dc1127749bca7cc7dd
Rickard Lindberg <ricli85@gmail.com>
2017-01-03 16:15:23 +0100

    Move bosparanian_weekdaynames module into bosparanian package.

--- source/timelinelib/time/bosparaniantime.py
+++ source/timelinelib/time/bosparaniantime.py
@@ -21,7 +21,7 @@ import re
 from timelinelib.calendar.bosparanian.bosparanian import Bosparanian, BosparanianUtils
 from timelinelib.calendar.bosparanian.monthnames import bosp_abbreviated_name_of_month
 from timelinelib.calendar.bosparanian.monthnames import bosp_name_of_month
-from timelinelib.calendar.bosparanian_weekdaynames import bosp_abbreviated_name_of_weekday
+from timelinelib.calendar.bosparanian.weekdaynames import bosp_abbreviated_name_of_weekday
 from timelinelib.calendar.gregorian.timetype import GregorianTimeType
 from timelinelib.calendar import get_date_formatter
 from timelinelib.canvas.data import TimeOutOfRangeLeftError
