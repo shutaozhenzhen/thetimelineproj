4f2a374f44f51c4c3726ae87efa4c7db809b1e53
Rickard Lindberg <ricli85@gmail.com>
2017-01-03 11:04:53 +0100

    Remove setting of mid-day time that has no effect.

--- test/unit/wxgui/dialogs/timeeditor/controller.py
+++ test/unit/wxgui/dialogs/timeeditor/controller.py
@@ -18,38 +18,23 @@
 
 from mock import Mock
 
-from timelinelib.canvas.data.db import MemoryDB
 from timelinelib.config.dotfile import Config
 from timelinelib.test.cases.unit import UnitTestCase
 from timelinelib.test.utils import human_time_to_gregorian
-from timelinelib.wxgui.dialogs.timeeditor.controller import TimeEditorDialogController
+from timelinelib.time.gregoriantime import GregorianTimeType
 from timelinelib.wxgui.dialogs.timeeditor.view import TimeEditorDialog
 
 
 class describe_time_editor_dialog_for_gregorian_time(UnitTestCase):
 
-    def setUp(self):
-        self.db = MemoryDB()
-        self.view = Mock(TimeEditorDialog)
-        self.controller = TimeEditorDialogController(self.view)
-
     def test_it_can_be_created(self):
-        tm = human_time_to_gregorian("31 Dec 2010 00:00")
         config = Mock(Config)
         config.get_date_format.return_value = "yyyy-mm-dd"
         self.show_dialog(
-            TimeEditorDialog, None, config, self.db.get_time_type(), tm, "Go to Date")
-
-    def test_hours_set_to_midday_if_not_given_by_user(self):
-        self.given_time_not_shown_in_dialog("31 Dec 2010 00:00")
-        self.when_ok_button_clikced()
-        self.view.SetTime.assert_called_with(human_time_to_gregorian("31 Dec 2010 12:00"))
-
-    def given_time_not_shown_in_dialog(self, human_time):
-        tm = human_time_to_gregorian(human_time)
-        self.controller.on_init(self.db.get_time_type(), tm)
-        self.view.GetTime.return_value = tm
-        self.view.ShowTimeIsChecked.return_value = False
-
-    def when_ok_button_clikced(self):
-        self.controller.ok_button_clicked(None)
+            TimeEditorDialog,
+            None,
+            config,
+            GregorianTimeType(),
+            human_time_to_gregorian("31 Dec 2010 00:00"),
+            "Go to Date"
+        )
