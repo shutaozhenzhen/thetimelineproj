a0f254434fbb59192429cc87912090ee56bc18da
Rickard Lindberg <ricli85@gmail.com>
2017-01-05 14:25:54 +0100

    Move timepicker module into datepicker package.

--- source/timelinelib/calendar/gregorian/datepicker/datetime.py
+++ source/timelinelib/calendar/gregorian/datepicker/datetime.py
@@ -20,9 +20,9 @@ import os.path
 
 import wx.calendar
 
+from timelinelib.calendar.gregorian.datepicker.time import GregorianTimePicker
 from timelinelib.calendar.gregorian.datepicker.view import NewGregorianDatePicker
 from timelinelib.calendar.gregorian.gregorian import Gregorian, GregorianUtils
-from timelinelib.calendar.gregorian.timepicker import GregorianTimePicker
 from timelinelib.calendar.gregorian.timetype import GregorianTimeType
 from timelinelib.canvas.data.internaltime import Time
 from timelinelib.config.paths import ICONS_DIR
