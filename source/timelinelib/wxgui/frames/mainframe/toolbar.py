dedff9491f23d3e6b980f16f885ef9313fddbec1
Roger Lindberg <roger@rolidata.se>
2017-07-31 07:20:33 +0200

    Eliminated code duplication

--- source/timelinelib/wxgui/frames/mainframe/toolbar.py
+++ source/timelinelib/wxgui/frames/mainframe/toolbar.py
@@ -40,44 +40,22 @@ class ToolbarCreator(object):
         return self.toolbar
 
     def _add_event_text_alignment(self):
-        left_tool = self._add_radio(_("Left"), "format-justify-left.png")
-        center_tool = self._add_radio(_("Center"), "format-justify-center.png")
-
-        def on_left_click(event):
-            self.config.center_event_texts = False
-
-        def on_center_click(event):
-            self.config.center_event_texts = True
-
-        def check_item_corresponding_to_config():
-            if self.config.center_event_texts:
-                self.toolbar.ToggleTool(center_tool.GetId(), True)
-            else:
-                self.toolbar.ToggleTool(left_tool.GetId(), True)
-        self.parent.Bind(wx.EVT_TOOL, on_left_click, left_tool)
-        self.parent.Bind(wx.EVT_TOOL, on_center_click, center_tool)
-        self.config.listen_for_any(check_item_corresponding_to_config)
-        check_item_corresponding_to_config()
+        spec = {'tool-1-name': _("Left"),
+                'tool-2-name': _("Center"),
+                'tool-1-image': 'format-justify-left.png',
+                'tool-2-image': 'format-justify-center.png',
+                'config-name': 'center_event_texts',
+                }
+        self._toggle_toolbar(spec)
 
     def _add_point_event_alignment(self):
-        left_tool = self._add_radio(_("Left"), "event-line-left.png")
-        center_tool = self._add_radio(_("Center"), "event-line-center.png")
-
-        def on_left_click(event):
-            self.config.draw_point_events_to_right = True
-
-        def on_center_click(event):
-            self.config.draw_point_events_to_right = False
-
-        def check_item_corresponding_to_config():
-            if self.config.draw_point_events_to_right:
-                self.toolbar.ToggleTool(left_tool.GetId(), True)
-            else:
-                self.toolbar.ToggleTool(center_tool.GetId(), True)
-        self.parent.Bind(wx.EVT_TOOL, on_left_click, left_tool)
-        self.parent.Bind(wx.EVT_TOOL, on_center_click, center_tool)
-        self.config.listen_for_any(check_item_corresponding_to_config)
-        check_item_corresponding_to_config()
+        spec = {'tool-1-name': _("Left"),
+                'tool-2-name': _("Center"),
+                'tool-1-image': 'event-line-left.png',
+                'tool-2-image': 'event-line-center.png',
+                'config-name': 'draw_point_events_to_right',
+                }
+        self._toggle_toolbar(spec)
 
     def _add_radio(self, text, icon):
         return self.toolbar.AddRadioLabelTool(
@@ -89,3 +67,24 @@ class ToolbarCreator(object):
     def _set_visibility(self):
         self.toolbar.Show(self.config.show_toolbar)
         self.parent.Layout()
+
+    def _toggle_toolbar(self, spec):
+        left_tool = self._add_radio(spec['tool-1-name'], spec['tool-1-image'])
+        center_tool = self._add_radio(spec['tool-2-name'], spec['tool-2-image'])
+
+        def on_left_click(event):
+            self.config._set(spec['config-name'], True)
+
+        def on_center_click(event):
+            self.config._set(spec['config-name'], False)
+
+        def check_item_corresponding_to_config():
+            if self.config._get(spec['config-name']):
+                self.toolbar.ToggleTool(left_tool.GetId(), True)
+            else:
+                self.toolbar.ToggleTool(center_tool.GetId(), True)
+        self.parent.Bind(wx.EVT_TOOL, on_left_click, left_tool)
+        self.parent.Bind(wx.EVT_TOOL, on_center_click, center_tool)
+        self.config.listen_for_any(check_item_corresponding_to_config)
+        check_item_corresponding_to_config()
+
