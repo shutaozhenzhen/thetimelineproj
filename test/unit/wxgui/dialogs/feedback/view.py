9dfb7d337224137a8608883594226ec0d79033de
Rickard Lindberg <ricli85@gmail.com>
2017-08-04 11:01:48 +0200

    Problem: Unit test for feedback are not correclty split.

---
+++ test/unit/wxgui/dialogs/feedback/view.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017  Rickard Lindberg, Roger Lindberg
+#
+# This file is part of Timeline.
+#
+# Timeline is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Timeline is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Timeline.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from timelinelib.test.cases.unit import UnitTestCase
+from timelinelib.wxgui.dialogs.feedback.view import FeedbackDialog
+
+
+class describe_feedback_dialog(UnitTestCase):
+
+    def test_it_can_be_created(self):
+        self.show_dialog(FeedbackDialog, None, "info", "this was fun", "very fun")
