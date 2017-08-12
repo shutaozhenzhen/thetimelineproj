8a5c0eb6c8143443f08f9b51a72472aa9e71ec20
Roger Lindberg <roger@rolidata.se>
2017-08-12 07:23:02 +0200

    Problem: Naming conventions for class variables is not followed. Solution: Rename class variables

--- source/timelinelib/wxgui/frames/mainframe/mainframecontroller.py
+++ source/timelinelib/wxgui/frames/mainframe/mainframecontroller.py
@@ -32,16 +32,16 @@ class LockedException(Exception):
 class MainFrameController(object):
 
     def __init__(self, main_frame, db_open_fn, config):
-        self.main_frame = main_frame
-        self.db_open_fn = db_open_fn
-        self.config = config
-        self.timeline = None
+        self._main_frame = main_frame
+        self._db_open_fn = db_open_fn
+        self._config = config
+        self._timeline = None
 
     def on_started(self, application_arguments):
         if application_arguments.has_files():
             self.open_or_create_timeline(application_arguments.get_first_file())
-        elif self.config.has_recently_opened_files():
-            self.open_timeline_if_exists(self.config.get_latest_recently_opened_file())
+        elif self._config.has_recently_opened_files():
+            self.open_timeline_if_exists(self._config.get_latest_recently_opened_file())
 
     def open_or_create_timeline(self, path):
         self.open_timeline(path)
@@ -50,46 +50,46 @@ class MainFrameController(object):
         if os.path.exists(path):
             self.open_timeline(path)
         else:
-            display_error_message(_("File '%s' does not exist.") % path, self.main_frame)
+            display_error_message(_("File '%s' does not exist.") % path, self._main_frame)
 
     def open_gregorian_tutorial_timeline(self, *args, **kwargs):
         self.open_timeline(":tutorial:")
 
     def open_timeline(self, path, timetype=None, save_current_data=True):
         if save_current_data:
-            self.main_frame.save_current_timeline_data()
+            self._main_frame.save_current_timeline_data()
         try:
-            self.timeline = self.db_open_fn(path, timetype=timetype)
+            self._timeline = self._db_open_fn(path, timetype=timetype)
         except Exception as e:
-            self.main_frame.DisplayErrorMessage(
+            self._main_frame.DisplayErrorMessage(
                 _("Unable to open timeline '%s'.") % path + "\n\n" + str(e)
             )
         else:
-            self.config.append_recently_opened(path)
-            self.main_frame.update_open_recent_submenu()
-            self.timeline.path = path
-            self.main_frame.display_timeline(self.timeline)
-            self.timelinepath = path
-            self.last_changed = self._get_modification_date()
-            self.main_frame.update_navigation_menu_items()
-            self.main_frame.enable_disable_menus()
+            self._config.append_recently_opened(path)
+            self._main_frame.update_open_recent_submenu()
+            self._timeline.path = path
+            self._main_frame.display_timeline(self._timeline)
+            self._timelinepath = path
+            self._last_changed = self._get_modification_date()
+            self._main_frame.update_navigation_menu_items()
+            self._main_frame.enable_disable_menus()
             if path == ":numtutorial:":
-                self.main_frame._fit_all_events()
+                self._main_frame._fit_all_events()
 
     def set_timeline_in_readonly_mode(self):
         try:
-            self.timeline.set_readonly()
-            self.main_frame.set_timeline_readonly()
+            self._timeline.set_readonly()
+            self._main_frame.set_timeline_readonly()
         except:
             pass
 
     def week_starts_on_monday(self):
-        return self.config.get_week_start() == "monday"
+        return self._config.get_week_start() == "monday"
 
     def ok_to_edit(self):
-        if self.timeline is None:
+        if self._timeline is None:
             return True
-        if self.timeline.is_read_only():
+        if self._timeline.is_read_only():
             return False
         if self._locked():
             display_warning_message("The Timeline is Locked by someone else.\nTry again later")
@@ -98,7 +98,7 @@ class MainFrameController(object):
             self._lock()
             return True
         last_changed = self._get_modification_date()
-        if last_changed > self.last_changed:
+        if last_changed > self._last_changed:
             ack = get_user_ack(_("Someoneelse has changed the Timeline.\nYou have two choices!\n  1. Set Timeline in Read-Only mode.\n  2. Synchronize Timeline.\n\nDo you want to Synchronize?"))
             if ack:
                 self.reload_from_disk()
@@ -110,40 +110,40 @@ class MainFrameController(object):
         return True
 
     def start_slide_show(self, canvas):
-        open_slideshow_dialog(self.timeline, canvas)
+        open_slideshow_dialog(self._timeline, canvas)
 
     def _timeline_path_doesnt_exists_yet(self):
-        return not os.path.exists(self.timelinepath)
+        return not os.path.exists(self._timelinepath)
 
     def edit_ends(self):
         if self._the_lock_is_mine():
-            self.last_changed = self._get_modification_date()
+            self._last_changed = self._get_modification_date()
             self._unlock()
 
     def timeline_is_readonly(self):
-        return self.timeline is not None and self.timeline.is_read_only()
+        return self._timeline is not None and self._timeline.is_read_only()
 
     def _get_modification_date(self):
         try:
-            return os.path.getmtime(self.timelinepath)
+            return os.path.getmtime(self._timelinepath)
         except:
             return 0
 
     def reload_from_disk(self):
-        timeline_canvas = self.main_frame.main_panel.timeline_panel.timeline_canvas
+        timeline_canvas = self._main_frame.main_panel.timeline_panel.timeline_canvas
         vp = timeline_canvas.get_view_properties()
         displayed_period = vp.get_displayed_period()
-        self.open_timeline(self.timelinepath, save_current_data=False)
+        self.open_timeline(self._timelinepath, save_current_data=False)
         vp.set_displayed_period(displayed_period)
         timeline_canvas.redraw_timeline()
 
     def select_all(self):
-        timeline_canvas = self.main_frame.main_panel.timeline_panel.timeline_canvas
+        timeline_canvas = self._main_frame.main_panel.timeline_panel.timeline_canvas
         timeline_canvas.SelectAllEvents()
 
     def _lock(self):
         fp = None
-        if not self.timeline.get_should_lock():
+        if not self._timeline.get_should_lock():
             return
         try:
             ts = self._get_timestamp_string()
@@ -151,19 +151,19 @@ class MainFrameController(object):
             fp = open(path, "w")
             fp.write("%s\n%s\n%s" % (getpass.getuser(), ts, os.getpid()))
         except Exception:
-            msg = _("Unable to take lock on %s\nThis means you can't edit the timeline.\nCheck if you have write access to this directory.") % self.timelinepath
-            display_warning_message(msg, self.main_frame)
+            msg = _("Unable to take lock on %s\nThis means you can't edit the timeline.\nCheck if you have write access to this directory.") % self._timelinepath
+            display_warning_message(msg, self._main_frame)
             raise LockedException()
         finally:
             if fp is not None:
                 fp.close()
 
     def _get_lockpath(self):
-        return "%s.lock" % self.timelinepath
+        return "%s.lock" % self._timelinepath
 
     def _get_timestamp_string(self):
-        now = self.timeline.time_type.now()
-        return self.timeline.time_type.time_string(now)
+        now = self._timeline.time_type.now()
+        return self._timeline.time_type.time_string(now)
 
     def _locked(self):
         lockpath = self._get_lockpath()
