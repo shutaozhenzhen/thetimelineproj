d1b9777b76b632cfaf8bfa35074c53d4c6d0edaf
Roger Lindberg <roger@rolidata.se>
2017-08-12 07:42:04 +0200

    Removed unused variable

--- source/timelinelib/canvas/drawing/scene.py
+++ source/timelinelib/canvas/drawing/scene.py
@@ -336,7 +336,6 @@ class TimelineScene(object):
         minor_strip_data = []  # List of time_period
         self.major_strip, self.minor_strip = self._db.get_time_type().choose_strip(self._metrics, self._appearance)
         if hasattr(self.major_strip, 'set_skip_s_in_decade_text'):
-            skip = self._view_properties.get_skip_s_in_decade_text()
             self.major_strip.set_skip_s_in_decade_text(self._view_properties.get_skip_s_in_decade_text())
         if hasattr(self.minor_strip, 'set_skip_s_in_decade_text'):
             self.minor_strip.set_skip_s_in_decade_text(self._view_properties.get_skip_s_in_decade_text())
