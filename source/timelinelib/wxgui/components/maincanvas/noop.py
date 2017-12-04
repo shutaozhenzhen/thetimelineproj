59da4066fd40b0b7b4a579f2f6bbeeda643147c9
Roger Lindberg <roger@rolidata.se>
2017-12-04 07:34:11 +0100

    Removed unused parameter

--- source/timelinelib/wxgui/components/maincanvas/noop.py
+++ source/timelinelib/wxgui/components/maincanvas/noop.py
@@ -55,7 +55,7 @@ class NoOpInputHandler(InputHandler):
         self.last_hovered_event = self.timeline_canvas.GetEventAt(self._cursor, alt_down)
         self.last_hovered_balloon_event = self.timeline_canvas.GetBalloonAt(self._cursor)
         self._start_balloon_timers()
-        self._display_eventinfo_in_statusbar(self.last_hovered_event, x, y, alt_down)
+        self._display_eventinfo_in_statusbar(self.last_hovered_event, x, y)
         self._select_cursor_shape()
 
     def left_mouse_down(self, x, y, ctrl_down, shift_down, alt_down=False):
@@ -193,7 +193,7 @@ class NoOpInputHandler(InputHandler):
                 else:
                     self._redraw_balloons(None)
 
-    def _display_eventinfo_in_statusbar(self, event, xpixelpos, ypixelpos, alt_down=False):
+    def _display_eventinfo_in_statusbar(self, event, xpixelpos, ypixelpos):
         time_string = self._format_current_pos_datetime_string(xpixelpos)
         if event is None:
             self._status_bar.set_text(time_string)
