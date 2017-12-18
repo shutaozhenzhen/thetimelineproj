57ede23d2b00566fbf9db0c6cccd4644ff41cc2e
Roger Lindberg <roger@rolidata.se>
2017-12-18 10:43:38 +0100

    Added example for balloon drawing with a TimelineCanvas component

---
+++ documentation/timeline_canvas_examples/mouse_move_examples/canvas.py
@@ -0,0 +1,17 @@
+import timelineadaptor
+
+import wx
+
+from timelinelib.canvas import TimelineCanvas
+
+TEST_CASE = 1
+
+
+class Canvas(TimelineCanvas):
+
+    def __init__(self, parent):
+        TimelineCanvas.__init__(self, parent)
+        self.Bind(wx.EVT_MOTION, self._on_motion)
+
+    def _on_motion(self, evt):
+        self.DisplayBalloons(evt)
