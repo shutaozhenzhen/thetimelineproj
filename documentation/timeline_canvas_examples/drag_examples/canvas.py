5d92d7aafe7ab5fa46b1aef99298dbce62788336
Roger Lindberg <roger@rolidata.se>
2017-12-19 20:33:30 +0100

    Added DragScrolling functions to the TimelineCanvas api. Added example code on how to iplement drag-scrolling

---
+++ documentation/timeline_canvas_examples/drag_examples/canvas.py
@@ -0,0 +1,24 @@
+import timelineadaptor
+
+import wx
+
+from timelinelib.canvas import TimelineCanvas
+
+
+class Canvas(TimelineCanvas):
+
+    def __init__(self, parent):
+        TimelineCanvas.__init__(self, parent)
+        self.Bind(wx.EVT_LEFT_DOWN, self._on_left_down)
+        self.Bind(wx.EVT_MOTION, self._on_motion)
+        self.Bind(wx.EVT_LEFT_UP, self._on_left_up)
+        self.InitDragScroll(direction=wx.BOTH)
+
+    def _on_left_down(self, evt):
+        self.StartDragScroll(evt)
+
+    def _on_motion(self, evt):
+        self.DragScroll(evt)
+
+    def _on_left_up(self, evt):
+        self.StopDragScroll()
