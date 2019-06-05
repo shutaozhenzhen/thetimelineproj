9c47da9592321327c508499301bfeec35cf79a6e
Roger Lindberg <roger@rolidata.se>
2019-06-05 22:07:03 +0200

    Cleaning: Renamed private variable

--- source/timelinelib/wxgui/components/searchbar/serachbarview.py
+++ source/timelinelib/wxgui/components/searchbar/serachbarview.py
@@ -25,7 +25,7 @@ from timelinelib.config.paths import ICONS_DIR
 class GuiCreator(object):
 
     def _create_gui(self):
-        self.icon_size = (16, 16)
+        self._icon_size = (16, 16)
         self._create_close_button()
         self._create_search_box()
         self._create_prev_button()
@@ -54,8 +54,7 @@ class GuiCreator(object):
             self._period_label.Show(False)
             
     def _create_search_box(self):
-        self._search = wx.SearchCtrl(self, size=(150, -1),
-                                    style=wx.TE_PROCESS_ENTER)
+        self._search = wx.SearchCtrl(self, size=(150, -1), style=wx.TE_PROCESS_ENTER)
         self.Bind(wx.EVT_SEARCHCTRL_SEARCH_BTN,
                   self._search_on_search_btn, self._search)
         self.Bind(wx.EVT_TEXT_ENTER, self._search_on_text_enter, self._search)
@@ -65,23 +64,22 @@ class GuiCreator(object):
         if 'wxMSW' in wx.PlatformInfo:
             close_bmp = wx.Bitmap(os.path.join(ICONS_DIR, "close.png"))
         else:
-            close_bmp = wx.ArtProvider.GetBitmap(wx.ART_CROSS_MARK, wx.ART_TOOLBAR, self.icon_size)
+            close_bmp = wx.ArtProvider.GetBitmap(wx.ART_CROSS_MARK, wx.ART_TOOLBAR, self._icon_size)
         self.AddLabelTool(wx.ID_CLOSE, "", close_bmp, shortHelp=_("Close"))
         self.Bind(wx.EVT_TOOL, self._btn_close_on_click, id=wx.ID_CLOSE)
 
     def _create_prev_button(self):
-        prev_bmp = wx.ArtProvider.GetBitmap(wx.ART_GO_BACK, wx.ART_TOOLBAR,
-                                            self.icon_size)
+        prev_bmp = wx.ArtProvider.GetBitmap(wx.ART_GO_BACK, wx.ART_TOOLBAR, self._icon_size)
         self.AddLabelTool(wx.ID_BACKWARD, "", prev_bmp, shortHelp=_("Prevoius match"))
         self.Bind(wx.EVT_TOOL, self._btn_prev_on_click, id=wx.ID_BACKWARD)
 
     def _create_next_button(self):
-        next_bmp = wx.ArtProvider.GetBitmap(wx.ART_GO_FORWARD, wx.ART_TOOLBAR, self.icon_size)
+        next_bmp = wx.ArtProvider.GetBitmap(wx.ART_GO_FORWARD, wx.ART_TOOLBAR, self._icon_size)
         self.AddLabelTool(wx.ID_FORWARD, "", next_bmp, shortHelp=_("Next match"))
         self.Bind(wx.EVT_TOOL, self._btn_next_on_click, id=wx.ID_FORWARD)
 
     def _create_list_button(self):
-        list_bmp = wx.ArtProvider.GetBitmap(wx.ART_LIST_VIEW, wx.ART_TOOLBAR, self.icon_size)
+        list_bmp = wx.ArtProvider.GetBitmap(wx.ART_LIST_VIEW, wx.ART_TOOLBAR, self._icon_size)
         self.AddLabelTool(wx.ID_MORE, "", list_bmp, shortHelp=_("List matches"))
         self.Bind(wx.EVT_TOOL, self._btn_list_on_click, id=wx.ID_MORE)
 
