50edfaccedcd47d93d1456ca6c7a38b0b5611d72
Roger Lindberg <roger@rolidata.se>
2019-07-25 14:56:34 +0200

    added doc-string

--- source/timelinelib/config/paths.py
+++ source/timelinelib/config/paths.py
@@ -15,6 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Timeline.  If not, see <http://www.gnu.org/licenses/>.
 
+"""
+Defining paths for resources needed by the application.
+"""
 
 import os.path
 
