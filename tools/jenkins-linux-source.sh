83557b163e41c674ee19717627c300fb5b995b6c
Rickard Lindberg <ricli85@gmail.com>
2019-08-09 14:35:48 +0200

    Install icalendar from pip instead.

--- tools/jenkins-linux-source.sh
+++ tools/jenkins-linux-source.sh
@@ -5,4 +5,5 @@ set -e
 virtualenv venv -p python3 --system-site-packages
 . venv/bin/activate
 pip install git+https://github.com/thetimelineproj/humblewx.git
+pip install icalendar
 python tools/build-source-zip.py --revision $1
