15c79e115d2601572b19b2f7b42c02a18c0211db
Roger Lindberg <roger@rolidata.se>
2019-08-09 12:07:13 +0200

    Removed function

--- source/timeline.py
+++ source/timeline.py
@@ -29,59 +29,54 @@ import platform
 import sys
 
 
-def start():
-    if platform.system() != "Windows":
-        import wxversion
-        wxversion.ensureMinimal('2.8')
+if platform.system() != "Windows":
+    import wxversion
+    wxversion.ensureMinimal('2.8')
 
-    # Make sure that we can import timelinelib
-    sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
-    # Make sure that we can import pysvg
-    sys.path.insert(
-        0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "dependencies", "timelinelib", "pysvg-0.2.1"))
-    # Make sure that we can import pytz which icalendar is dependant on
-    sys.path.insert(
-        0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "dependencies", "timelinelib", "pytz-2012j"))
-    # Make sure that we can import icalendar
-    sys.path.insert(
-        0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "dependencies", "timelinelib", "icalendar-3.2"))
-    # Make sure that we can import markdown
-    sys.path.insert(
-        0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "dependencies", "timelinelib", "markdown-2.0.3"))
-    # Make sure that we can import humblewx
-    sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                    "..", "dependencies", "timelinelib", "humblewx-master", "source"))
-    # Make sure that we can import Pillow
-    sys.path.insert(
-        0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "dependencies", "timelinelib", "Pillow-3.2.0"))
+# Make sure that we can import timelinelib
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
+# Make sure that we can import pysvg
+sys.path.insert(
+    0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "dependencies", "timelinelib", "pysvg-0.2.1"))
+# Make sure that we can import pytz which icalendar is dependant on
+sys.path.insert(
+    0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "dependencies", "timelinelib", "pytz-2012j"))
+# Make sure that we can import icalendar
+sys.path.insert(
+    0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "dependencies", "timelinelib", "icalendar-3.2"))
+# Make sure that we can import markdown
+sys.path.insert(
+    0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "dependencies", "timelinelib", "markdown-2.0.3"))
+# Make sure that we can import humblewx
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                "..", "dependencies", "timelinelib", "humblewx-master", "source"))
+# Make sure that we can import Pillow
+sys.path.insert(
+    0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "dependencies", "timelinelib", "Pillow-3.2.0"))
 
-    from timelinelib.config.paths import LOCALE_DIR
-    from timelinelib.meta.about import APPLICATION_NAME
+from timelinelib.config.paths import LOCALE_DIR
+from timelinelib.meta.about import APPLICATION_NAME
 
-    if platform.system() == "Windows":
-        # The appropriate environment variables are set on other systems
-        language, encoding = locale.getdefaultlocale()
-        os.environ['LANG'] = language
+if platform.system() == "Windows":
+    # The appropriate environment variables are set on other systems
+    language, encoding = locale.getdefaultlocale()
+    os.environ['LANG'] = language
 
-    gettext.install(APPLICATION_NAME.lower(), LOCALE_DIR, unicode=True)
+gettext.install(APPLICATION_NAME.lower(), LOCALE_DIR, unicode=True)
 
-    from timelinelib.config.arguments import ApplicationArguments
+from timelinelib.config.arguments import ApplicationArguments
 
-    application_arguments = ApplicationArguments()
-    application_arguments.parse_from(sys.argv[1:])
+application_arguments = ApplicationArguments()
+application_arguments.parse_from(sys.argv[1:])
 
-    import timelinelib
+import timelinelib
 
-    timelinelib.DEBUG_ENABLED = application_arguments.get_debug_flag()
+timelinelib.DEBUG_ENABLED = application_arguments.get_debug_flag()
 
-    from timelinelib.wxgui.setup import setup_humblewx
+from timelinelib.wxgui.setup import setup_humblewx
 
-    setup_humblewx()
+setup_humblewx()
 
-    from timelinelib.wxgui.setup import start_wx_application
+from timelinelib.wxgui.setup import start_wx_application
 
-    start_wx_application(application_arguments)
-
-
-if __name__ == '__main__':
-    start()
+start_wx_application(application_arguments)
