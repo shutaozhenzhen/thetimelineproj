0ffe0b25c70c6bcb28016b49237cb8a7f8a12dec
Rickard Lindberg <ricli85@gmail.com>
2019-08-09 14:45:47 +0200

    Run 2to3 on timelinecanvascontroller.

--- test/unit/canvas/timelinecanvascontroller.py
+++ test/unit/canvas/timelinecanvascontroller.py
@@ -59,26 +59,26 @@ class describe_navigate(TimelineCanvasControllerTestCase):
     def test_setting_too_narrow_period_gives_error(self):
         self.assertNavigationFails(
             lambda tp: time_period(15, 15),
-            u"⟪Can't zoom deeper than 1⟫"
+            "⟪Can't zoom deeper than 1⟫"
         )
 
     def test_setting_too_left_period_gives_error(self):
         self.assertNavigationFails(
             lambda tp: time_period(0, 2),
-            u"⟪Can't scroll more to the left⟫"
+            "⟪Can't scroll more to the left⟫"
         )
 
     def test_setting_too_right_period_gives_error(self):
         self.assertNavigationFails(
             lambda tp: time_period(20, 21),
-            u"⟪Can't scroll more to the right⟫"
+            "⟪Can't scroll more to the right⟫"
         )
 
     def assertNavigationFails(self, navigate_fn, pattern):
         try:
             self.controller.navigate(navigate_fn)
-        except ValueError, e:
-            self.assertEqual(pattern, unicode(e))
+        except ValueError as e:
+            self.assertEqual(pattern, str(e))
             self.assertEqual(
                 self.controller.get_time_period(),
                 self.original_period
@@ -104,7 +104,7 @@ class ATimeType(TimeType):
         return "%s to %s" % (period.start_time, period.end_time)
 
     def get_min_zoom_delta(self):
-        return (GregorianDelta(1), u"⟪Can't zoom deeper than 1⟫")
+        return (GregorianDelta(1), "⟪Can't zoom deeper than 1⟫")
 
     def now(self):
         return GregorianTime(0, 0)
