b1d5225e34c6fbeab9db7334516f9ea399ec2a9d
Rickard Lindberg <ricli85@gmail.com>
2019-09-08 14:12:58 +0200

    Install all dependencies according to doc in jenkins tests.

--- tools/jenkins-linux-source.sh
+++ tools/jenkins-linux-source.sh
@@ -6,7 +6,8 @@ virtualenv venv -p python3 --system-site-packages
 . venv/bin/activate
 pip install git+https://github.com/thetimelineproj/humblewx.git
 pip install icalendar
-pip install markdown
+pip install Markdown
 pip install pysvg-py3
+pip install Pillow
 python tools/execute-specs-repeat.py --write-testlist testlist.txt
 python tools/build-source-zip.py --revision $1
