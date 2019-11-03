69db1ff5165ac41b69abfe84c417c429945eaad4
Roger Lindberg <roger@rolidata.se>
2019-11-03 22:02:18 +0100

    Fixed error in version creation function Removed tag 2.0.0

--- tools/winbuildtools/buildwinsetup.cmd
+++ tools/winbuildtools/buildwinsetup.cmd
@@ -20,14 +20,18 @@ python generate-mo-files.py
 popd
 
 echo --------------------------------------
+echo Modify version file and iss file
+echo --------------------------------------
+python mod_iss_timeline_version.py . %1
+
+echo --------------------------------------
 echo Building distribution
 echo --------------------------------------
 pyinstaller timeline.spec
 
 echo --------------------------------------
-echo Package distribution
+echo Create distributable
 echo --------------------------------------
-python mod_iss_timeline_version.py . %1
 pushd inno
 iscc.exe timeline2Win32.iss
 popd
