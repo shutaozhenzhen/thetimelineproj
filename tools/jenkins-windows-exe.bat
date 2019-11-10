e982d8854a346d1d7bc8e28f0f963e56624a87a3
Roger Lindberg <roger@rolidata.se>
2019-11-10 12:04:01 +0100

    Trying to fix tests to run when building windows exe

--- tools/jenkins-windows-exe.bat
+++ tools/jenkins-windows-exe.bat
@@ -1,3 +1,10 @@
+virtualenv venv -p python3 --system-site-packages
+. venv/bin/activate
+pip install git+https://github.com/thetimelineproj/humblewx.git
+pip install icalendar
+pip install Markdown
+pip install pysvg-py3
+pip install Pillow
 python tools/execute-specs.py --write-testlist testlist.txt
 cd tools\winbuildtools
 set path=c:\pgm\python37;c:\pgm\python37\Scripts;%PATH%
