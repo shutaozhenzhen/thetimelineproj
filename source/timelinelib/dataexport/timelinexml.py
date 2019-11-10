29fb5bbb9413fb2577c0d830b2da2e632c050224
Roger Lindberg <roger@rolidata.se>
2019-11-10 12:50:00 +0100

    No need for platform specific code

--- source/timelinelib/dataexport/timelinexml.py
+++ source/timelinelib/dataexport/timelinexml.py
@@ -218,25 +218,10 @@ def color_string(color):
 
 
 def icon_string(bitmap):
-    if platform.system() == "Windows":
-        return icon_string_windows(bitmap)
-    else:
-        return icon_string_windows(bitmap)
-        #return icon_string_linux(bitmap)
-
-
-def icon_string_windows(bitmap):
     stream = io.BytesIO()
     image = bitmap.ConvertToImage()
     image.SaveFile(stream, 'image/png')
     return base64.b64encode(stream.getvalue()).decode()
-
-
-def icon_string_linux(bitmap):
-    output = io.StringIO()
-    image = bitmap.ConvertToImage()
-    image.SaveStream(output, wx.BITMAP_TYPE_PNG)
-    return base64.b64encode(output.getvalue())
 
 
 def alert_string(time_type, alert):
