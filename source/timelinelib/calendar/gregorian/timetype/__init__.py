bcabc17404de990bd00498e33086e60927eb90eb
Roger Lindberg <roger@rolidata.se>
2019-11-15 09:13:38 +0100

    Created subdirectory for Gregorian timetype (prepare for refactoring)

---
+++ source/timelinelib/calendar/gregorian/timetype/__init__.py
@@ -0,0 +1,53 @@
+# Copyright (C) 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018  Rickard Lindberg, Roger Lindberg
+#
+# This file is part of Timeline.
+#
+# Timeline is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Timeline is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Timeline.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from timelinelib.calendar.gregorian.timetype.timetype import GregorianTimeType
+from timelinelib.calendar.gregorian.timetype.timetype import DurationFormatter
+from timelinelib.calendar.gregorian.timetype.timetype import YEARS
+from timelinelib.calendar.gregorian.timetype.timetype import DAYS
+from timelinelib.calendar.gregorian.timetype.timetype import HOURS
+from timelinelib.calendar.gregorian.timetype.timetype import MINUTES
+from timelinelib.calendar.gregorian.timetype.timetype import SECONDS
+from timelinelib.calendar.gregorian.timetype.timetype import SECONDS_IN_DAY
+from timelinelib.calendar.gregorian.timetype.timetype import has_nonzero_time
+from timelinelib.calendar.gregorian.timetype.timetype import forward_fn
+from timelinelib.calendar.gregorian.timetype.timetype import format_year
+from timelinelib.calendar.gregorian.timetype.timetype import forward_one_year_fn
+from timelinelib.calendar.gregorian.timetype.timetype import forward_one_month_fn
+from timelinelib.calendar.gregorian.timetype.timetype import forward_one_week_fn
+from timelinelib.calendar.gregorian.timetype.timetype import backward_fn
+from timelinelib.calendar.gregorian.timetype.timetype import backward_one_year_fn
+from timelinelib.calendar.gregorian.timetype.timetype import backward_one_month_fn
+from timelinelib.calendar.gregorian.timetype.timetype import backward_one_week_fn
+from timelinelib.calendar.gregorian.timetype.timetype import fit_millennium_fn
+from timelinelib.calendar.gregorian.timetype.timetype import fit_week_fn
+from timelinelib.calendar.gregorian.timetype.timetype import move_period_num_days
+from timelinelib.calendar.gregorian.timetype.timetype import move_period_num_months
+from timelinelib.calendar.gregorian.timetype.timetype import move_period_num_weeks
+from timelinelib.calendar.gregorian.timetype.timetype import move_period_num_years
+from timelinelib.calendar.gregorian.timetype.timetype import StripCentury
+from timelinelib.calendar.gregorian.timetype.timetype import StripDecade
+from timelinelib.calendar.gregorian.timetype.timetype import StripYear
+from timelinelib.calendar.gregorian.timetype.timetype import StripMonth
+from timelinelib.calendar.gregorian.timetype.timetype import StripWeek
+from timelinelib.calendar.gregorian.timetype.timetype import StripWeekday
+from timelinelib.calendar.gregorian.timetype.timetype import StripDay
+from timelinelib.calendar.gregorian.timetype.timetype import StripHour
+from timelinelib.calendar.gregorian.timetype.timetype import StripMinute
+from timelinelib.calendar.gregorian.timetype.timetype import TimeOutOfRangeLeftError
+from timelinelib.calendar.gregorian.timetype.timetype import TimeOutOfRangeRightError
