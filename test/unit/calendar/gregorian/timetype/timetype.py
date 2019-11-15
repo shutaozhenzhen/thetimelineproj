23ec7eb72980e6a933ac14c7eeab71aa55c46ae6
Roger Lindberg <roger@rolidata.se>
2019-11-15 15:01:08 +0100

    Moved test

--- test/unit/calendar/gregorian/timetype/timetype.py
+++ test/unit/calendar/gregorian/timetype/timetype.py
@@ -38,12 +38,10 @@ from timelinelib.calendar.gregorian.timetype import move_period_num_months
 from timelinelib.calendar.gregorian.timetype import move_period_num_weeks
 from timelinelib.calendar.gregorian.timetype import move_period_num_years
 from timelinelib.calendar.gregorian.timetype import StripDay
-from timelinelib.calendar.gregorian.timetype import StripDecade
 from timelinelib.calendar.gregorian.timetype import StripHour
 from timelinelib.calendar.gregorian.timetype import StripMonth
 from timelinelib.calendar.gregorian.timetype import StripWeek
 from timelinelib.calendar.gregorian.timetype import StripWeekday
-from timelinelib.calendar.gregorian.timetype import StripYear
 from timelinelib.calendar.gregorian.timetype import TimeOutOfRangeLeftError
 from timelinelib.calendar.gregorian.timetype import TimeOutOfRangeRightError
 from timelinelib.canvas.appearance import Appearance
@@ -316,82 +314,6 @@ class describe_gregorian_strip_month(UnitTestCase):
         UnitTestCase.setUp(self)
         self.time_type = GregorianTimeType()
         self.strip = StripMonth()
-
-
-class describe_gregorian_strip_decade(UnitTestCase):
-
-    def test_label(self):
-        for (time, expected_label) in [
-            ("7 Jul -19", "20s ⟪BC⟫"),
-            ("7 Jul -18", "10s ⟪BC⟫"),
-            ("7 Jul -9", "10s ⟪BC⟫"),
-            ("7 Jul -8", "0s ⟪BC⟫"),
-            ("7 Jul 0", "0s ⟪BC⟫"),
-            ("7 Jul 1", "0s"),
-            ("7 Jul 9", "0s"),
-            ("7 Jul 10", "10s"),
-            ("7 Jul 19", "10s"),
-            ("7 Jul 20", "20s"),
-            ("7 Jul 2013", "2010s"),
-        ]:
-            self.assertEqual(
-                self.strip.label(human_time_to_gregorian(time)),
-                expected_label
-            )
-
-    def test_start(self):
-        for (start_year, expected_decade_start_year) in [
-            # 20s BC
-            (-19, -28),
-            # 10s BC
-            (-18, -18),
-            (-9, -18),
-            # 0s BC
-            (-8, -8),
-            (0, -8),
-            # 0s
-            (1, 1),
-            (9, 1),
-            # 10s
-            (10, 10),
-            (19, 10),
-            # 20s
-            (20, 20),
-            # 2010s
-            (2010, 2010),
-            (2013, 2010),
-        ]:
-            self.assertEqual(
-                self.strip.start(human_time_to_gregorian(
-                    "10 Jul {0} 12:33:15".format(start_year)
-                )),
-                human_time_to_gregorian(
-                    "1 Jan {0} 00:00:00".format(expected_decade_start_year)
-                )
-            )
-
-    def test_increment(self):
-        for (start_year, expected_next_start_year) in [
-            (-28, -18),
-            (-18, -8),
-            (-8, 1),
-            (1, 10),
-            (10, 20),
-            (2010, 2020),
-        ]:
-            self.assertEqual(
-                self.strip.increment(human_time_to_gregorian(
-                    "1 Jan {0}".format(start_year)
-                )),
-                human_time_to_gregorian(
-                    "1 Jan {0}".format(expected_next_start_year)
-                )
-            )
-
-    def setUp(self):
-        UnitTestCase.setUp(self)
-        self.time_type = GregorianTimeType()
-        self.strip = StripDecade()
 
 
 class describe_gregorian_time_type_delta_formatting(UnitTestCase):
