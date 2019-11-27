121d7b89f0e831a9e6a05c50262326324d144704
Roger Lindberg <roger@rolidata.se>
2019-11-27 22:41:52 +0100

    Moved function

--- source/timelinelib/wxgui/frames/mainframe/controllerapi.py
+++ source/timelinelib/wxgui/frames/mainframe/controllerapi.py
@@ -61,6 +61,9 @@ class MainFrameApiUsedByController:
     def set_readonly_mode(self):
         self.controller.set_timeline_in_readonly_mode()
 
+    def week_starts_on_monday(self):
+        return self.controller.week_starts_on_monday()
+
     def _set_title(self):
         if self.timeline is None:
             self.SetTitle(APPLICATION_NAME)
