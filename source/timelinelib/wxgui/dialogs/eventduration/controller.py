c59de25bc9c0011c8899e9c24faf02249bf306f5
Roger Lindberg <roger@rolidata.se>
2020-01-13 21:09:32 +0100

    Eliminated unecessary functions

--- source/timelinelib/wxgui/dialogs/eventduration/controller.py
+++ source/timelinelib/wxgui/dialogs/eventduration/controller.py
@@ -111,17 +111,11 @@ class EventsDurationController(Controller):
             return round(duration / divisor, precision)
 
     def _get_event_duration(self, e):
-        start_time = self._get_event_duration_start_time(e.get_start_time())
-        end_time = self._get_event_duration_end_time(e.get_end_time())
+        start_time = max(self.view.GetStartTime() or e.get_start_time(), e.get_start_time())
+        end_time = min(self.view.GetEndTime() or e.get_end_time(), e.get_end_time())
         if end_time < start_time:
             return 0
         return TimePeriod(start_time, end_time).duration().value
-
-    def _get_event_duration_start_time(self, start_time):
-        return max(self.view.GetStartTime() or start_time, start_time)
-
-    def _get_event_duration_end_time(self, end_time):
-        return min(self.view.GetEndTime() or end_time, end_time)
 
     def _autocopy_to_clipboard(self):
         if self.view.GetCopyToClipboard():
