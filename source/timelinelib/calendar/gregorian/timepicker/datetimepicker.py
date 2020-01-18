b51641abc6799f9f6a028d662b7a1891c1f4966b
Roger Lindberg <roger@rolidata.se>
2020-01-18 14:28:17 +0100

    Created a generic datetime picker class

--- source/timelinelib/calendar/gregorian/timepicker/datetimepicker.py
+++ source/timelinelib/calendar/gregorian/timepicker/datetimepicker.py
@@ -26,95 +26,17 @@ from timelinelib.calendar.generic.timepicker.calendarpopup import CalendarPopup
 from timelinelib.calendar.gregorian.timepicker.datetimepickercontroller import GregorianDateTimePickerController
 from timelinelib.calendar.gregorian.timetype import GregorianTimeType
 from timelinelib.config.paths import ICONS_DIR
+from timelinelib.calendar.generic.timepicker.datetimepicker import DateTimePicker
 
 
-class GregorianDateTimePicker(wx.Panel):
+class GregorianDateTimePicker(DateTimePicker):
 
     def __init__(self, parent, show_time=True, config=None, on_change=None):
-        wx.Panel.__init__(self, parent)
-        self._parent = parent
-        self._config = config
-        self._calendar_popup = None
+        DateTimePicker.__init__(self, parent, show_time, config, on_change)
         self._time_type = GregorianTimeType()
         self._date_picker = GregorianDatePicker(self, self._config.get_date_formatter(), on_change=on_change)
         self._time_picker = GregorianTimePicker(self, self._config)
-        self._create_gui()
         self._controller = GregorianDateTimePickerController(self, self._date_picker, self._time_picker,
                                                              GregorianTimeType().now, on_change)
+        self.create_gui()
         self.show_time(show_time)
-
-    def PopupCalendar(self, evt, wx_date):
-        calendar_popup = CalendarPopup(self, wx_date, self._config, self._time_type)
-        calendar_popup.Bind(wx.adv.EVT_CALENDAR_SEL_CHANGED, self._calendar_on_date_changed)
-        calendar_popup.Bind(wx.adv.EVT_CALENDAR, self._calendar_on_date_changed_dclick)
-        btn = evt.GetEventObject()
-        pos = btn.ClientToScreen((0, 0))
-        sz = btn.GetSize()
-        calendar_popup.Position(pos, (0, sz[1]))
-        calendar_popup.Popup()
-        self._calendar_popup = calendar_popup
-
-    def on_return(self):
-        try:
-            self._parent.on_return()
-        except AttributeError:
-            pass
-
-    def on_escape(self):
-        try:
-            self._parent.on_escape()
-        except AttributeError:
-            pass
-
-    def show_time(self, show=True):
-        sizer = wx.BoxSizer(wx.HORIZONTAL)
-        if show:
-            sizer.Add(self._date_picker, proportion=1, flag=wx.ALIGN_CENTER_VERTICAL)
-            sizer.Add(self.date_button, proportion=0, flag=wx.ALIGN_CENTER_VERTICAL)
-            sizer.Add(self._time_picker, proportion=0, flag=wx.ALIGN_CENTER_VERTICAL)
-            self._time_picker.Show()
-        else:
-            sizer.Add(self._date_picker, proportion=1, flag=wx.ALIGN_CENTER_VERTICAL)
-            sizer.Add(self.date_button, proportion=0, flag=wx.ALIGN_CENTER_VERTICAL)
-            self._time_picker.Hide()
-        self.SetSizerAndFit(sizer)
-
-    def get_value(self):
-        try:
-            return self._controller.get_value()
-        except ValueError:
-            pass
-
-    def set_value(self, value):
-        self._controller.set_value(value)
-
-    def _create_gui(self):
-        image = wx.Bitmap(os.path.join(ICONS_DIR, "calendar.png"))
-        self.date_button = wx.BitmapButton(self, bitmap=image)
-        self.Bind(wx.EVT_BUTTON, self._date_button_on_click, self.date_button)
-        # Layout
-        sizer = wx.BoxSizer(wx.HORIZONTAL)
-        sizer.Add(self._date_picker, proportion=1, flag=wx.ALIGN_CENTER_VERTICAL)
-        sizer.Add(self.date_button, proportion=0, flag=wx.ALIGN_CENTER_VERTICAL)
-        sizer.Add(self._time_picker, proportion=0, flag=wx.ALIGN_CENTER_VERTICAL)
-        self.SetSizerAndFit(sizer)
-
-    def _date_button_on_click(self, evt):
-        self._controller.date_button_on_click(evt)
-
-    @staticmethod
-    def _out_of_date_range(wx_date):
-        """It's is a limitation in the wx.adv.CalendarCtrl class
-        that has this date limit."""
-        return str(wx_date) < '1601-01-01 00:00:00'
-
-    def _calendar_on_date_changed(self, evt):
-        wx_date = evt.GetEventObject().GetDate()
-        date = self._controller.wx_date_to_date_tuple(wx_date)
-        self._date_picker.SetDate(date)
-        self._controller.changed()
-
-    def _calendar_on_date_changed_dclick(self, evt):
-        self._time_picker.SetFocus()
-        self._calendar_popup.Dismiss()
-        self._controller.changed()
