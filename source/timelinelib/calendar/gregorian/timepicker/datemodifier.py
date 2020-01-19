8756b7a1fd74fcc47d30a9f6559c5b7aeecd9c9c
Roger Lindberg <roger@rolidata.se>
2020-01-19 08:47:37 +0100

    Created a generic DateModifier

--- source/timelinelib/calendar/gregorian/timepicker/datemodifier.py
+++ source/timelinelib/calendar/gregorian/timepicker/datemodifier.py
@@ -19,63 +19,12 @@
 from timelinelib.calendar.gregorian.gregorian import GregorianDateTime
 from timelinelib.calendar.gregorian.time import GregorianDelta
 from timelinelib.calendar.gregorian.timetype import GregorianTimeType
+import timelinelib.calendar.generic.timepicker.datemodifier as generic
 
 
-class DateModifier:
+class DateModifier(generic.DateModifier):
 
-    def increment_year(self, date):
-        max_year = GregorianDateTime.from_time(GregorianTimeType().get_max_time()).year
-        year, month, day = date
-        if year < max_year - 1:
-            return self._set_valid_day(year + 1, month, day)
-        return date
-
-    def increment_month(self, date):
-        max_year = GregorianDateTime.from_time(GregorianTimeType().get_max_time()).year
-        year, month, day = date
-        if month < 12:
-            return self._set_valid_day(year, month + 1, day)
-        elif year < max_year - 1:
-            return self._set_valid_day(year + 1, 1, day)
-        return date
-
-    def increment_day(self, date):
-        year, month, day = date
-        time = GregorianDateTime.from_ymd(year, month, day).to_time()
-        if time < GregorianTimeType().get_max_time() - GregorianDelta.from_days(1):
-            return GregorianDateTime.from_time(time + GregorianDelta.from_days(1)).to_date_tuple()
-        return date
-
-    def decrement_year(self, date):
-        year, month, day = date
-        if year > GregorianDateTime.from_time(GregorianTimeType().get_min_time()).year:
-            return self._set_valid_day(year - 1, month, day)
-        return date
-
-    def decrement_month(self, date):
-        year, month, day = date
-        if month > 1:
-            return self._set_valid_day(year, month - 1, day)
-        elif year > GregorianDateTime.from_time(GregorianTimeType().get_min_time()).year:
-            return self._set_valid_day(year - 1, 12, day)
-        return date
-
-    def decrement_day(self, date):
-        year, month, day = date
-        if day > 1:
-            return self._set_valid_day(year, month, day - 1)
-        elif month > 1:
-            return self._set_valid_day(year, month - 1, 31)
-        elif year > GregorianDateTime.from_time(GregorianTimeType().get_min_time()).year:
-            return self._set_valid_day(year - 1, 12, 31)
-        return date
-
-    def _set_valid_day(self, new_year, new_month, new_day):
-        done = False
-        while not done:
-            try:
-                date = GregorianDateTime.from_ymd(new_year, new_month, new_day)
-                done = True
-            except Exception:
-                new_day -= 1
-        return date.to_date_tuple()
+    def __init__(self):
+        self._time_type = GregorianTimeType()
+        self._delta = GregorianDelta
+        self._date_time = GregorianDateTime
